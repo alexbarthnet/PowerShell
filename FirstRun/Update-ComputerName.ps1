a0fba6414c0088b9de2b1d004e0231820b8e3e3b
Alex Barth <alexbarth@outlook.com>
2022-02-09 03:48:50 -0600

    Updated logging for startup scripts

--- FirstRun/Update-ComputerName.ps1
+++ FirstRun/Update-ComputerName.ps1
@@ -1,12 +1,12 @@
 $os_path = (Get-CimInstance -Class Win32_OperatingSystem).WindowsDirectory
 $os_name = (Get-CimInstance -Class Win32_ComputerSystem).Name
 $vm_name = (Get-Item 'HKLM:\SOFTWARE\Microsoft\Virtual Machine\Guest\Parameters').GetValue('VirtualMachineName')
-$vm_text = Join-Path -Path $os_path -Child 'VmRename.log'
-Start-Transcript -Path $vm_text -Append
-Write-Output "Found current computer name: $os_name"
-Write-Output "Found hypervisor guest name: $vm_name"
+$vm_file = Join-Path -Path $os_path -Child 'VmRename.log'
 If ($os_name -ne $vm_name) {
+	Start-Transcript -Path $vm_file -Append
+	Write-Output "Found current computer name: $os_name"
+	Write-Output "Found hypervisor guest name: $vm_name"	
 	Write-Output "Renaming computer to: $vm_name"
-	Rename-Computer -NewName $vm_name -Restart -Force
+	Rename-Computer -NewName $vm_file -Restart -Force
+	Stop-Transcript
 }
-Stop-Transcript
--- FirstRun/Update-NetBT.ps1
+++ FirstRun/Update-NetBT.ps1
@@ -1,25 +1,18 @@
 $host_path = (Get-CimInstance -Class Win32_OperatingSystem).WindowsDirectory
-$host_name = (Get-CimInstance -Class Win32_ComputerSystem).Name
 $host_file = Join-Path -Path $host_path -Child 'NetBTDisable.log'
-Start-Transcript -Path $host_file -Append
-Write-Output "Found current computer name: $host_name"
-$nic_change = $false
-Try {
-	# retrieve NICs with NBT enabled
-	$nbt_nics = $null
-	$nbt_nics = Get-ChildItem 'HKLM:SYSTEM\CurrentControlSet\services\NetBT\Parameters\Interfaces' | Get-ItemProperty | Where-Object {$_.NetbiosOptions -eq '0'}
-	# update NICs
-	If ($nbt_nics.Count -gt 0) {
-		$nic_change = $true
-		ForEach ($nbt_nic in $nbt_nics) {
-			Write-Output "Fixing $($nbt_nic.PSChildName)"
-			Set-ItemProperty -Path $nbt_nic.PSPath -Name 'NetbiosOptions' -Value 2
-		}
+$nic_fixed = $false
+$nic_w_nbt = $null
+$nic_w_nbt = Get-ChildItem 'HKLM:SYSTEM\CurrentControlSet\services\NetBT\Parameters\Interfaces' | Get-ItemProperty | Where-Object { $_.NetbiosOptions -eq '0' }
+If ($nic_w_nbt.Count -gt 0) {
+	Start-Transcript -Path $host_file -Append
+	$nic_fixed = $true
+	ForEach ($nic_to_fix in $nic_w_nbt) {
+		Write-Output "Fixing $($nic_to_fix.PSChildName)"
+		Set-ItemProperty -Path $nic_to_fix.PSPath -Name 'NetbiosOptions' -Value 2
 	}
-}
-Finally {
 	Stop-Transcript
-	If ($nic_change) {
-		# Restart-Computer
-	}
+}
+If ($nic_fixed) {
+	Write-Output 'Restarting...'
+	Restart-Computer -Force
 }
