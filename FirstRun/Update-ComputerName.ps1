6fb338a5a445c2532eb033075ecaa45d39d6282b
Alex Barth <alexbarth@outlook.com>
2022-02-09 13:00:14 -0600

    Updated logging for startup scripts

--- FirstRun/Update-ComputerName.ps1
+++ FirstRun/Update-ComputerName.ps1
@@ -1,12 +1,16 @@
-$os_path = (Get-CimInstance -Class Win32_OperatingSystem).WindowsDirectory
+# define logging
+$log_root = (Get-CimInstance -Class Win32_OperatingSystem).WindowsDirectory
+$log_file = $PSCommandPath.Split('\')[-1].Replace('.ps1','.txt')
+$log_path = Join-Path -Path $log_root -Child $log_file
+# define script elements
 $os_name = (Get-CimInstance -Class Win32_ComputerSystem).Name
 $vm_name = (Get-Item 'HKLM:\SOFTWARE\Microsoft\Virtual Machine\Guest\Parameters').GetValue('VirtualMachineName')
-$vm_file = Join-Path -Path $os_path -Child 'VmRename.log'
+# check computer name
 If ($os_name -ne $vm_name) {
-	Start-Transcript -Path $vm_file -Append
+	Start-Transcript -Path $log_path -Append
 	Write-Output "Found current computer name: $os_name"
 	Write-Output "Found hypervisor guest name: $vm_name"	
 	Write-Output "Renaming computer to: $vm_name"
-	Rename-Computer -NewName $vm_file -Restart -Force
+	Rename-Computer -NewName $log_path -Restart -Force
 	Stop-Transcript
 }
--- FirstRun/Update-NetBT.ps1
+++ FirstRun/Update-NetBT.ps1
@@ -1,10 +1,14 @@
-$host_path = (Get-CimInstance -Class Win32_OperatingSystem).WindowsDirectory
-$host_file = Join-Path -Path $host_path -Child 'NetBTDisable.log'
+# define logging
+$log_root = (Get-CimInstance -Class Win32_OperatingSystem).WindowsDirectory
+$log_file = $PSCommandPath.Split('\')[-1].Replace('.ps1','.txt')
+$log_path = Join-Path -Path $log_root -Child $log_file
+# define script elements
 $nic_fixed = $false
 $nic_w_nbt = $null
 $nic_w_nbt = Get-ChildItem 'HKLM:SYSTEM\CurrentControlSet\services\NetBT\Parameters\Interfaces' | Get-ItemProperty | Where-Object { $_.NetbiosOptions -eq '0' }
+# check NICs for NetBIOS transport
 If ($nic_w_nbt.Count -gt 0) {
-	Start-Transcript -Path $host_file -Append
+	Start-Transcript -Path $log_path -Append
 	$nic_fixed = $true
 	ForEach ($nic_to_fix in $nic_w_nbt) {
 		Write-Output "Fixing $($nic_to_fix.PSChildName)"
