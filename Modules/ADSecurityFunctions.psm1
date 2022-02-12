6d7e3b28855b25d95d1d26d5c06dae8b2db1ba84
Alex Barth <alexbarth@outlook.com>
2022-02-11 19:09:05 -0600

    Replaced Get-ADDCDrive with New- and Remove-ADCustomPSDrive

--- Modules/ADSecurityFunctions.psm1
+++ Modules/ADSecurityFunctions.psm1
@@ -1,31 +1,5 @@
 #Requires -Modules ActiveDirectory
 
-Function Get-ADDCDrive {
-	Param(
-		[Parameter(Position = 0)]
-		[string]$Server = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain().PdcRoleOwner.Name,
-		[Parameter(Position = 1)]
-		[switch]$Force
-	)
-
-	Try {
-		# check for PSDrive with correct settings
-		$psdrive = $null
-		$psdrive = Get-PSDrive | Where-Object {($_.Name -eq 'ADDC') -and ($_.Server -eq $Server) -and ($_.Provider -eq 'ActiveDirectory') -and ($_.Root -eq '//RootDSE/')}
-
-		# check for PSdrive with the 'ADDC' name
-		If ($Force -or $null -eq $psdrive) {
-			# remove any PSdrive with the 'ADDC' name
-			Get-PSDrive | Where-Object {$_.Name -eq "ADDC"} | Remove-PSDrive -Force
-			# create PSdrive with correct values
-			$null = New-PSDrive -Name 'ADDC' -Server $Server -PSProvider 'ActiveDirectory' -Root '//RootDSE/' -Scope Global	
-		}
-	}
-	Catch {
-		$_
-	}
-}
-
 Function Get-ADSecurityObjects {
 	[CmdletBinding()]
 	Param(
@@ -35,14 +9,22 @@ Function Get-ADSecurityObjects {
 
 	# try catch for error handling
 	Try {
+		# retrieve servers
+		If ($Force -or $null -eq $ad_dc_pdc) { New-Variable -Force -Scope 'Global' -Option 'ReadOnly' -Name 'ad_dc_pdc' -Value ([System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain().PdcRoleOwner.Name) }
+		If ($Force -or $null -eq $ad_dc_rid) { New-Variable -Force -Scope 'Global' -Option 'ReadOnly' -Name 'ad_dc_rid' -Value ([System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain().RidRoleOwner.Name) }
+		If ($Force -or $null -eq $ad_dc_ism) { New-Variable -Force -Scope 'Global' -Option 'ReadOnly' -Name 'ad_dc_ism' -Value ([System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain().InfrastructureRoleOwner.Name) }
+		If ($Force -or $null -eq $ad_dc_schema) { New-Variable -Force -Scope 'Global' -Option 'ReadOnly' -Name 'ad_dc_schema' -Value ([System.DirectoryServices.ActiveDirectory.Forest]::GetCurrentForest().SchemaRoleOwner.Name) }
+		If ($Force -or $null -eq $ad_dc_naming) { New-Variable -Force -Scope 'Global' -Option 'ReadOnly' -Name 'ad_dc_naming' -Value ([System.DirectoryServices.ActiveDirectory.Forest]::GetCurrentForest().NamingRoleOwner.Name) }
+		If ($Force -or $null -eq $ad_dc_active) { New-Variable -Force -Scope 'Global' -Option 'ReadOnly' -Name 'ad_dc_active' -Value ([System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain().FindDomainController().Name) }
+
 		# retrieve naming contexts
 		If ($Force -or $null -eq $ad_nc_domain) { New-Variable -Force -Scope 'Global' -Option 'ReadOnly' -Name 'ad_nc_domain' -Value ([System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain().GetDirectoryEntry().DistinguishedName) }
 		If ($Force -or $null -eq $ad_nc_schema) { New-Variable -Force -Scope 'Global' -Option 'ReadOnly' -Name 'ad_nc_schema' -Value ([System.DirectoryServices.ActiveDirectory.ActiveDirectorySchema]::GetCurrentSchema().Name) }
 		If ($Force -or $null -eq $ad_nc_config) { New-Variable -Force -Scope 'Global' -Option 'ReadOnly' -Name 'ad_nc_config' -Value $ad_nc_schema.Split(',', 2)[1] }
 
 		# retrieve guids
-		If ($Force -or $null -eq $ad_guids_schema) { New-Variable -Force -Scope 'Private' -Option 'ReadOnly' -Name 'ad_guids_schema' -Value (Get-ADObject -SearchBase $ad_nc_schema -LDAPFilter "(schemaidguid=*)" -Properties 'lDAPDisplayName', 'schemaIDGUID') }
-		If ($Force -or $null -eq $ad_guids_rights) { New-Variable -Force -Scope 'Private' -Option 'ReadOnly' -Name 'ad_guids_rights' -Value (Get-ADObject -SearchBase $ad_nc_config -LDAPFilter "(&(objectclass=controlAccessRight)(rightsguid=*))" -Properties 'displayName', 'rightsGuid') }
+		If ($Force -or $null -eq $ad_guids_schema) { New-Variable -Force -Scope 'Private' -Option 'ReadOnly' -Name 'ad_guids_schema' -Value (Get-ADObject -SearchBase $ad_nc_schema -LDAPFilter '(schemaidguid=*)' -Properties 'lDAPDisplayName', 'schemaIDGUID') }
+		If ($Force -or $null -eq $ad_guids_rights) { New-Variable -Force -Scope 'Private' -Option 'ReadOnly' -Name 'ad_guids_rights' -Value (Get-ADObject -SearchBase $ad_nc_config -LDAPFilter '(&(objectclass=controlAccessRight)(rightsguid=*))' -Properties 'displayName', 'rightsGuid') }
  
 		# create and populate hash tables
 		If ($Force -or $null -eq $ad_map_rights) { 
@@ -106,22 +88,92 @@ Function Get-ADSecurityObjects {
 	}
 }
 
+Function New-ADCustomPSDrive {
+	Param(
+		[Parameter(Position = 0)]
+		[string]$Name = 'ADCustom',
+		[Parameter(Position = 1)]
+		[string]$Server = $ad_dc_pdc,
+		[Parameter(Position = 2)]
+		[switch]$Force,
+		[Parameter(Position = 3)]
+		[switch]$Passthru
+	)
+
+	Try {
+		# check for PSDrive with with matching Name
+		$ad_custom_psdrive = $null
+		$ad_custom_psdrive = Get-PSDrive -Scope 'Global' | Where-Object { $_.Name -eq $Name }
+
+		# set Force if found PSDrive has incorrect values
+		If ($ad_custom_psdrive.Server -ne $Server -or $ad_custom_psdrive.Root -ne '//RootDSE/' -or -not $ad_custom_psdrive.Provider -match 'ActiveDirectory$') {
+			$Force = $true
+		}
+
+		# remove found PSDrive if Force set
+		If ($Force) {
+			$ad_custom_psdrive | Remove-PSDrive -Force -ErrorAction 'SilentlyContinue' -Scope 'Global'
+			$ad_custom_psdrive = $null
+		}
+
+		# create PSdrive with correct values
+		If ($null -eq $ad_custom_psdrive) {
+			$ad_custom_psdrive = New-PSDrive -Name $Name -Server $Server -PSProvider 'ActiveDirectory' -Root '//RootDSE/' -Scope 'Global'
+		}
+
+		# return object if requested
+		If ($Passthru) {
+			$ad_custom_psdrive
+		}
+	}
+	Catch {
+		$_
+	}
+}
+
+Function Remove-ADCustomPSDrive {
+	Param(
+		[Parameter(Position = 0)]
+		[string]$Name = 'ADCustom'
+	)
+
+	Try {
+		# check for PSDrive with with matching Name
+		$ad_custom_psdrive = $null
+		$ad_custom_psdrive = Get-PSDrive -Scope 'Global' | Where-Object { $_.Name -eq $Name }
+
+		# check for PSdrive with matching Name
+		If ($ad_custom_psdrive) {
+			# remove any PSdrive with with matching Name
+			$ad_custom_psdrive | Remove-PSDrive -Force -Scope 'Global'
+		}
+	}
+	Catch {
+		$_
+	}
+}
+
 Function Reset-ADSecurity {
 	[CmdletBinding()]
 	param (
 		[Parameter(Position = 0, Mandatory = $true)]
 		[string]$Identity,
 		[Parameter(Position = 1)]
-		[string]$Server = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain().PdcRoleOwner.Name
+		[string]$Server = $ad_dc_pdc
 	)
-	
-	# retrieve PSdrive
-	Try {
-		Get-ADDCDrive -Server $Server
+
+	# create custom PSdrive if requested
+	If ($null -ne $Server) {
+		Try {
+			$ad_psdrive = (New-ADCustomPSDrive -Server $Server -Passthru).Name
+		}
+		Catch {
+			Write-Host "ERROR: creating 'ADCustom' PSDrive for server: '$Server'"
+			Break
+		}	
 	}
-	Catch {
-		Write-Host "ERROR: creating 'ADDC' PSDrive for server: '$Server'"
-		Break
+	Else {
+		$ad_psdrive = 'AD'
 	}
 
 	# verify the AD object
@@ -138,7 +190,7 @@ Function Reset-ADSecurity {
 		$ad_sddl = (Get-ADObject -Filter "Name -eq '$ad_class'" -SearchBase $ad_nc_schema -Properties 'defaultSecurityDescriptor').defaultSecurityDescriptor
 
 		# get the ACL for the object
-		$ad_path = ("ADDC:\$Identity")
+		$ad_path = ($ad_psdrive + ':\' + $Identity)
 		$ad_acl = Get-Acl -Path $ad_path
 		$ad_acl.SetAccessRuleProtection($true, $false)
 		$ad_acl.Access | ForEach-Object { $ad_acl.RemoveAccessRule($_) } | Out-Null
@@ -154,23 +206,28 @@ Function Update-ADSecurity {
 		[object[]]$Objects,
 		[Parameter(Position = 1)]
 		[object[]]$Permissions,
-		[Parameter(Position = 1)][ValidateScript({$_ -is [System.Security.Principal.SecurityIdentifier]})]
+		[Parameter(Position = 2)][ValidateScript({ $_ -is [System.Security.Principal.SecurityIdentifier] })]
 		[object]$SID,
-		[Parameter(Position = 2)][ValidateSet("Enable", "Disable", "Remove")]
+		[Parameter(Position = 3)][ValidateSet('Enable', 'Disable', 'Remove')]
 		[string]$Inheritance,
-		[Parameter(Position = 3)]
-		[string]$Server = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain().PdcRoleOwner.Name,
 		[Parameter(Position = 4)]
+		[string]$Server = $ad_dc_pdc,
+		[Parameter(Position = 5)]
 		[switch]$Reset
 	)
 
-	# retrieve PSdrive
-	Try {
-		Get-ADDCDrive -Server $Server
+	# create custom PSdrive if requested
+	If ($null -ne $Server) {
+		Try {
+			$ad_psdrive = (New-ADCustomPSDrive -Server $Server -Passthru).Name
+		}
+		Catch {
+			Write-Host "ERROR: creating 'ADCustom' PSDrive for server: '$Server'"
+			Break
+		}	
 	}
-	Catch {
-		Write-Host "ERROR: creating 'ADDC' PSDrive for server: '$Server'"
-		Break
+	Else {
+		$ad_psdrive = 'AD'
 	}
 
 	# create empty array
@@ -197,7 +254,7 @@ Function Update-ADSecurity {
 	# process DNs
 	ForEach ($ad_object_dn in $ad_object_fqdns) {
 		# define path for AD filesystem provider
-		$ad_object_path = ("ADDC:\" + $ad_object_dn)
+		$ad_object_path = ($ad_psdrive + ':\' + $ad_object_dn)
 		# validate the AD file system provider can retrieve the object
 		If (Test-Path -Path $ad_object_path) {
 			# retrieve ACL
@@ -211,15 +268,16 @@ Function Update-ADSecurity {
 			}
 			# check inheritance settings
 			switch ($Inheritance) {
-				"Enable" {
+				'Enable' {
 					# enable inheritance
 					$ad_object_acl.SetAccessRuleProtection($false, $false) 
 				}
-				"Disable" {
+				'Disable' {
 					# disable inheritance and copy inherited permissions
 					$ad_object_acl.SetAccessRuleProtection($true, $true) 
 				}
-				"Remove" { 
+				'Remove' { 
+					# remove inheritance if possible, disable otherwise
 					If ($Permissions.Count -ge 1) {
 						# disable inheritance and do not copy inherited permissions
 						$ad_object_acl.SetAccessRuleProtection($true, $false)
@@ -227,18 +285,21 @@ Function Update-ADSecurity {
 					Else {
 						# disable inheritance and copy inherited permissions
 						$ad_object_acl.SetAccessRuleProtection($true, $true) 
-						Write-Host "WARNING: inherited access cannot be removed without replacement ACEs, inheritance disabled instead"
+						Write-Host 'WARNING: inherited access cannot be removed without replacement ACEs, inheritance disabled instead'
 					}
 				}
+				Default {
+					# leave inheritance as is
+				}
 			}
-			# remove existing ACEs where IdentityReference found in provided ACEs
+			# remove existing ACEs for any IdentityReference found in provided ACEs
 			If ($Reset -and $Permissions.Count -gt 0) {
 				ForEach ($ad_object_ace in $Permissions) {
 					$ad_object_acl.PurgeAccessRules($ad_object_ace.IdentityReference)
 				}
 				Write-Host "Removed existing access for principals in requested ACEs on object: '$ad_object_dn'"
 			}
-			# remove existing ACEs where IdentityReference found in provided ACEs
+			# remove existing ACEs for any provided SID
 			If ($Reset -and $SID -is [System.Security.Principal.SecurityIdentifier]) {
 				ForEach ($ad_object_ace in $Permissions) {
 					$ad_object_acl.PurgeAccessRules($SID)
@@ -250,7 +311,7 @@ Function Update-ADSecurity {
 				$ad_object_acl.AddAccessRule($ad_object_ace)
 			}
 			# update ACL
-			Set-ACL -ACLObject $ad_object_acl -Path $ad_object_path
+			Set-Acl -AclObject $ad_object_acl -Path $ad_object_path
 			Write-Host "Updated ACL on object: '$ad_object_dn'"
 		}
 		Else {
@@ -259,10 +320,14 @@ Function Update-ADSecurity {
 	}
 }
 
+# load the AD security objects on module load
+Get-ADSecurityObjects -Force
+
 # define functions to export
 $functions_to_export = @()
-$functions_to_export += 'Get-ADDCDrive'
 $functions_to_export += 'Get-ADSecurityObjects'
+$functions_to_export += 'New-ADCustomPSDrive'
+$functions_to_export += 'Remove-ADCustomPSDrive'
 $functions_to_export += 'Reset-ADSecurity'
 $functions_to_export += 'Update-ADSecurity'
 
