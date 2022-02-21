1b13a66de947047f1b31a457cdbd050f78da0d0f
Alex Barth <alexbarth@outlook.com>
2022-02-21 14:21:31 -0600

    Added primary function, replaced XML with JSON

--- OfflineVMs/Import-OfflineFiles.ps1
+++ OfflineVMs/Import-OfflineFiles.ps1
@@ -1,3 +1,5 @@
+#Requires -Modules CmsCredentials
+
 [CmdletBinding(DefaultParameterSetName = 'Default')]
 Param(
 	[Parameter(Mandatory = $True, ParameterSetName = 'Import')]
@@ -22,127 +24,148 @@ Param(
 	[switch]$Purge
 )
 
-# build configuration file path from script
-$xml_path = $PSCommandPath.Replace('.ps1', '.xml')
-$xml_test = Test-Path -Path $xml_path
+Function Import-OfflineFilesToVM {
+	[CmdletBinding()]
+	param (
+		[string]$VMName,
+		[string]$Source,
+		[string]$Target,
+		[boolean]$Purge
+	)
 
-# clear required objects then check file
-$xml = @()
-If ($xml_test) {
-	# retrieve XML file name
-	$xml_name = (Get-Item -Path $xml_path).Name
-	# create object from XML file
-	$xml += Import-Clixml -Path $xml_path
-} 
-Else {
-	# define expected XML file name
-	$xml_name = $xml_path.Split('\')[-1]
-}
-
-switch ($true) {
-	$Reset { 
-		Write-Output "Resetting '$xml_name'"
-		If ($xml_test) { Remove-Item -Path $xml_path -Force }
-	}
-	$Clear {
-		Write-Output "Clearing '$xml_name'"
-		@() | Export-Clixml -Path $xml_path
-	}
-	$Remove { 
-		# remove matching entries from object
-		$xml = $xml | Where-Object { $_.VMName -ne $VMName }
-		$xml | Export-Clixml -Path $xml_path
-		# declare changes then show current state
-		Write-Output "Updating '$xml_name' to remove '$VMName'"
-		$xml | Select-Object VMName, Source, Target, Purge
-	}
-	$Add { 
-		# create custom object from parameters and add to object
-		$xml += [pscustomobject]@{ VMName = $VMName ; Purge = $Purge ; Source = $Source ; Target = $Target }
-		$xml | Export-Clixml -Path $xml_path
-		# declare changes then show current state
-		Write-Output "Updating '$xml_name' to add '$VMName'"
-		$xml | Select-Object VMName, Source, Target, Purge
-	}
-	$Import {
-		# define and start transcript
-		$log_path = $PSCommandPath.Replace('.ps1', '.txt')
-		Start-Transcript -Path $log_path -Force
-		# declare start then process entries
-		Write-Output "Importing with '$xml_name'"
-		If ($xml.Count) {
-			$xml | ForEach-Object {
-				# process entry
-				$vm_name = $_.VMName
-				$file_purge = $_.Purge
-				$file_source = $_.Source
-				$file_target = $_.Target
-				# check for VM on local system
-				$vm_check = $null
-				$vm_check = Get-VM | Where-Object { $_.Name -eq $vm_name }
-				If ($vm_check) {
-					# retrieve VM credentials
-					Set-Location -Path $PSScriptRoot
-					$global:Credential = $null
-					.\Unprotect-CmsCredentials.ps1 -Target $vm_name
-					If ($global:Credential) {
-						# connect to VM
-						$vm_direct = $null
-						$vm_direct = New-PSSession -VMName $vm_name -Credential $global:Credential
-						If ($vm_direct) {
-							# verify source
-							If (Test-Path -Path $file_source) { 
-								# retrieve files from source
-								$file_list = $null
-								$file_list = Get-ChildItem -Path $file_source
-								If ($file_list) {
-									# verify target on VM
-									$target_check = $null
-									$target_check = Invoke-Command -Session $vm_direct -ScriptBlock { If ( Test-Path -Path $using:file_target ) { Get-Item -Path $using:file_target } Else { New-Item -ItemType Directory -Path $using:file_target } }
-									If ($target_check) {
-										# determine if target should be cleaned before writing files
-										If ($file_purge) {
-											Write-Output "Clearing '$file_target' before copy"
-											Invoke-Command -Session $vm_direct -ScriptBlock { Get-ChildItem -Path $using:file_target | Remove-Item -Force } 
-										}
-										# copy files from source to VM
-										$file_list.FullName | Copy-Item -ToSession $vm_direct -Destination $file_target -Force -Verbose
-									}
-									Else {
-										Write-Output "Could not find or create '$file_target' on VM"
-									}
-								}
-								Else {
-									Write-Output "Could not retrieve files in '$file_source' on host"
-								}
+	# check for VM on local system
+	$vm_check = $null
+	$vm_check = Get-VM | Where-Object { $_.Name -eq $VMName }
+	If ($vm_check) {
+		# retrieve VM credentials
+		Set-Location -Path $PSScriptRoot
+		$global:Credential = $null
+		Unprotect-CmsCredentials -Target $VMName
+		If ($global:Credential) {
+			# connect to VM
+			$vm_direct = $null
+			$vm_direct = New-PSSession -VMName $VMName -Credential $global:Credential
+			If ($vm_direct) {
+				# verify source
+				If (Test-Path -Path $Source) { 
+					# retrieve files from source
+					$file_list = $null
+					$file_list = Get-ChildItem -Path $Source
+					If ($file_list) {
+						# verify target on VM
+						$target_check = $null
+						$target_check = Invoke-Command -Session $vm_direct -ScriptBlock { If ( Test-Path -Path $using:Target ) { Get-Item -Path $using:Target } Else { New-Item -ItemType Directory -Path $using:Target } }
+						If ($target_check) {
+							# determine if target should be cleaned before writing files
+							If ($Purge) {
+								Write-Output "Clearing '$Target' before copy"
+								Invoke-Command -Session $vm_direct -ScriptBlock { Get-ChildItem -Path $using:Target | Remove-Item -Force } 
 							}
-							Else {
-								Write-Output "Could not find '$file_source' on host"
-							}
-							# disconnect from VM
-							$vm_direct | Remove-PSSession
+							# copy files from source to VM
+							$file_list.FullName | Copy-Item -ToSession $vm_direct -Destination $Target -Force -Verbose
 						}
 						Else {
-							Write-Output "Could not create PowerShell Direct session for VM: '$vm_name'"
+							Write-Output "Could not find or create '$Target' on VM"
 						}
 					}
 					Else {
-						Write-Output "Could not locate credentials for VM: '$vm_name'"
+						Write-Output "Could not retrieve files in '$Source' on host"
 					}
 				}
 				Else {
-					Write-Output "Could not locate VM: '$vm_name'"
+					Write-Output "Could not find '$Source' on host"
 				}
+				# disconnect from VM
+				$vm_direct | Remove-PSSession
+			}
+			Else {
+				Write-Output "Could not create PowerShell Direct session for VM: '$VMName'"
 			}
 		}
 		Else {
-			Write-Output "XML file is empty: '$xml_name'"
+			Write-Output "Could not locate credentials for VM: '$VMName'"
 		}
-		# stop transcript
-		Stop-Transcript
+	}
+	Else {
+		Write-Output "Could not locate VM: '$VMName'"
+	}
+}
+
+# define configuration file from script path then verify path
+$json_path = $PSCommandPath.Replace('.ps1', '.json')
+$json_test = Test-Path -Path $json_path
+
+# clear required objects then check file
+$json_data = @()
+If ($json_test) {
+	# retrieve JSON file name
+	$json_name = (Get-Item -Path $json_path).Name
+	# create object from JSON file
+	$json_data += Get-Content -Path $json_path | ConvertFrom-Json
+} 
+Else {
+	# define expected JSON file name
+	$json_name = Split-Path -Path $json_path -Leaf
+}
+
+# evaluate parameters
+switch ($true) {
+	$Clear { 
+		Write-Output "`nClearing '$json_name'`n"
+		If ($json_test) { Remove-Item -Path $json_path -Force }
+	}
+	$Remove {
+		# remove matching entries from object
+		$json_data = $json_data | Where-Object { $_.VMName -ne $VMName }
+		$json_data | ConvertTo-Json | Set-Content -Path $json_path
+		# declare changes then show current state
+		Write-Output "`nUpdated '$json_name' to remove '$VMName':"
+		$json_data | Select-Object VMName, Source, Target, Purge
+	}
+	$Add {
+		# create custom object from parameters then add to object
+		$json_data += [pscustomobject]@{ 
+			VMName = $VMName
+			Purge  = $Purge
+			Source = $Source 
+			Target = $Target 
+		}
+		$json_data | ConvertTo-Json | Set-Content -Path $json_path
+		# declare changes then show current state
+		Write-Output "`nUpdated '$json_name' to add '$VMName':"
+		$json_data | Select-Object VMName, Source, Target, Purge
+	}
+	$Import {
+		Try {
+			# define transcript file from script path and start transcript
+			Start-Transcript -Path $PSCommandPath.Replace('.ps1', '.txt') -Force
+
+			# # start logging
+			# Start-LogToMultiple -ScriptPath $PSCommandPath
+
+			# check entry count in configuration file
+			If ($json_data.Count -eq 0) {
+				Write-Host "ERROR: no entries found in configuration file: $json_name"
+				Return
+			}
+			
+			# process configuration file
+			ForEach ($json_datum in $json_data) {
+				If ([string]::IsNullOrEmpty($json_datum.VMName) -or [string]::IsNullOrEmpty($json_datum.Source) -or [string]::IsNullOrEmpty($json_datum.Target)) {
+					Write-Host "ERROR: invalid entry found in configuration file: $json_name"
+				}
+				Else {
+					Import-OfflineFilesToVM -VMName $json_datum.VMName -Source $json_datum.Source -Target $json_datum.Target -Purge $json_datum.Purge
+				}
+			}
+		}
+		Finally {
+			Write-Host ([string]::Empty)
+			Stop-Transcript
+		}
 	}
 	Default {
-		Write-Output "Displaying '$xml_name'"
-		$xml | Select-Object VMName, Source, Target, Purge
+		Write-Output "`nDisplaying '$json_name':"
+		$json_data | Select-Object VMName, Source, Target, Purge
 	}
 }
