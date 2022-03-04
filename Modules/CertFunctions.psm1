fe7226fc641037b84fac3a3a48dd2b1b764069dd
Alex Barth <alexbarth@outlook.com>
2022-03-03 21:51:00 -0600

    Fixed whitespace

--- Modules/CertFunctions.psm1
+++ Modules/CertFunctions.psm1
@@ -1,38 +1,38 @@
 Function ConvertTo-X509Certificate {
-    [CmdletBinding()]
-    Param (
-        [Parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $true)]
-        [object]$Input
-    )
+	[CmdletBinding()]
+	Param (
+		[Parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $true)]
+		[object]$Input
+	)
 
-    # create empty certificate object
-    $x509_cert = New-Object -TypeName 'System.Security.Cryptography.X509Certificates.X509Certificate2'
+	# create empty certificate object
+	$x509_cert = New-Object -TypeName 'System.Security.Cryptography.X509Certificates.X509Certificate2'
 
-    # import the certificate data into the object
-    switch ($true) {
-        { $Input[0] -is [byte[]] } { $x509_cert.Import($Input[0]) }
-        { $Input -is [byte[]] } { $x509_cert.Import($Input) }
-        Default { $x509_cert.Import([byte[]]$Input) }
-    }
-    
-    # return populated certificate object
-    $x509_cert 
+	# import the certificate data into the object
+	switch ($true) {
+		{ $Input[0] -is [byte[]] } { $x509_cert.Import($Input[0]) }
+		{ $Input -is [byte[]] } { $x509_cert.Import($Input) }
+		Default { $x509_cert.Import([byte[]]$Input) }
+	}
+
+	# return populated certificate object
+	$x509_cert
 }
 
 Function Test-Thumbprint {
-    [CmdletBinding()] 
-    Param(
-        [Parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $true)]
-        [string]$Thumbprint
-    )
+	[CmdletBinding()]
+	Param(
+		[Parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $true)]
+		[string]$Thumbprint
+	)
 
-    # validate input is 40 hexidecimal characters
-    If ($Thumbprint -match '^[\dA-Fa-f]{40}$') {
-        Return $true
-    }
-    Else {
-        Return $false
-    }
+	# validate input is 40 hexidecimal characters
+	If ($Thumbprint -match '^[\dA-Fa-f]{40}$') {
+		Return $true
+	}
+	Else {
+		Return $false
+	}
 }
 
 # define functions to export
