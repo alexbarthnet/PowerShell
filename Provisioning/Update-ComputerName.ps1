9b6463620221e1ab8fa333dfe69a50fbeaf53a78
Alex Barth <alexbarth@outlook.com>
2022-03-20 14:11:30 -0500

    Minor text cleanup

--- Provisioning/Update-ComputerName.ps1
+++ Provisioning/Update-ComputerName.ps1
@@ -1,15 +1,15 @@
 # define logging
 $log_root = (Get-CimInstance -Class Win32_OperatingSystem).WindowsDirectory
-$log_file = $PSCommandPath.Split('\')[-1].Replace('.ps1','.txt')
+$log_file = $PSCommandPath.Split('\')[-1].Replace('.ps1', '.txt')
 $log_path = Join-Path -Path $log_root -Child $log_file
-# define script elements
-$os_name = (Get-CimInstance -Class Win32_ComputerSystem).Name
+# retrieve computer and virtual machine names
+$os_name = (Get-CimInstance -Class 'Win32_ComputerSystem').Name
 $vm_name = (Get-Item 'HKLM:\SOFTWARE\Microsoft\Virtual Machine\Guest\Parameters').GetValue('VirtualMachineName')
 # check computer name
 If ($os_name -ne $vm_name) {
 	Start-Transcript -Path $log_path -Append
-	Write-Output "Found current computer name: $os_name"
-	Write-Output "Found hypervisor guest name: $vm_name"	
+	Write-Output "Found active computer name: $os_name"
+	Write-Output "Found virtual machine name: $vm_name"	
 	Write-Output "Renaming computer to: $vm_name"
 	Rename-Computer -NewName $vm_name -Restart -Force
 	Stop-Transcript
