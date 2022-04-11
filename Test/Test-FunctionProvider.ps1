37d23bca90fb091d3148616a93e3514becf33319
Alex Barth <alexbarth@outlook.com>
2022-04-11 13:16:27 -0500

    Validate nested functions

--- Test/Test-FunctionProvider.ps1
+++ Test/Test-FunctionProvider.ps1
@@ -4,6 +4,14 @@ param (
 	[string]$ComputerName
 )
 
+Function Invoke-CimInstanceFunction {
+	[CmdletBinding()]
+	param (
+		[Parameter()]
+		[string]$CimClassName
+	)
+	Get-CimInstanceFunction -CimClassName $CimClassName
+}
 Function Get-CimInstanceFunction {
 	[CmdletBinding()]
 	param (
@@ -13,8 +21,10 @@ Function Get-CimInstanceFunction {
 	Get-CimInstance -ClassName $CimClassName | Format-List
 }
 
+
 # define objects
-$RemoteFunction = "function Get-CimInstanceFunction {${function:Get-CimInstanceFunction}}"
+$RemoteFunction1 = "function Get-CimInstanceFunction {${function:Get-CimInstanceFunction}}"
+$RemoteFunction2 = "function Invoke-CimInstanceFunction {${function:Invoke-CimInstanceFunction}}"
 $RemoteParameters = @{
 	CimClassName = 'Win32_ComputerSystem'
 }
@@ -22,8 +32,9 @@ $RemoteParameters = @{
 # try to run function
 Try {
 	Invoke-Command -ComputerName $ComputerName -ScriptBlock {
-		. ([ScriptBlock]::Create($using:RemoteFunction))
-		Get-CimInstanceFunction @using:RemoteParameters
+		. ([ScriptBlock]::Create($using:RemoteFunction1))
+		. ([ScriptBlock]::Create($using:RemoteFunction2))
+		Invoke-CimInstanceFunction @using:RemoteParameters
 	}
 }
 Catch {
