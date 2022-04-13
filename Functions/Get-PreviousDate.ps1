34c63cdc27c4885be1eeb96f100bd9daf4767ad1
Alex Barth <alexbarth@outlook.com>
2022-04-13 14:32:35 -0500

    Simple function to return a previous relative date

---
+++ Functions/Get-PreviousDate.ps1
@@ -0,0 +1,16 @@
+Function Get-PreviousDate {
+	Param (
+		[Parameter(Mandatory = $true, Position = 0)][ValidateRange(1, 65535)]
+		[uint16]$OlderThanUnits,
+		[Parameter(Mandatory = $true, Position = 1)][ValidateSet('Seconds', 'Minutes', 'Hours', 'Days', 'Weeks', 'Months', 'Years')]
+		[string]$OlderThanType
+	)
+	Switch ($OlderThanType) {
+		'Minutes' { Return (Get-Date).AddMinutes(-1 * $OlderThanUnits) }
+		'Hours' { Return (Get-Date).AddHours(-1 * $OlderThanUnits) }
+		'Days' { Return (Get-Date).AddDays(-1 * $OlderThanUnits) }
+		'Weeks' { Return (Get-Date).AddWeeks(-1 * $OlderThanUnits) }
+		'Months' { Return (Get-Date).AddMonths(-1 * $OlderThanUnits) }
+		'Years' { Return (Get-Date).AddYears(-1 * $OlderThanUnits) }
+	}
+}
