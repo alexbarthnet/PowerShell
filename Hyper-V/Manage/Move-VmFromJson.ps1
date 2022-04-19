a1d2698291607660da0933c0684b80966469d483
Alex Barth <alexbarth@outlook.com>
2022-04-19 14:57:59 -0500

    Updated object names and reverse handling

--- Hyper-V/Manage/Move-VmFromJson.ps1
+++ Hyper-V/Manage/Move-VmFromJson.ps1
@@ -45,24 +45,22 @@ ForEach ($VmParams in $vm_list) {
 	Write-Host ("$Hostname - validating move for VM: " + $vm_name)
 
 	# clear variables
-	$vm_host = $null
-	$vm_host_dest = $null
-	$vm_path_dest = $null
+	$vm_host_source = $null
+	$vm_host_target = $null
+	$vm_path_target = $null
 
 	# set host information
 	If ($Reverse) {
-		$vm_host = $DestinationHost
-		$vm_host_dest = $VmParams.Host
-		$vm_path_dest = $VmParams.Path
+		$vm_host_source = $DestinationHost
+		$vm_host_target = $VmParams.Host
 	}
 	Else {
-		$vm_host = $VmParams.Host
-		$vm_host_dest = $DestinationHost
-		$vm_path_dest = $DestinationPath
+		$vm_host_source = $VmParams.Host
+		$vm_host_target = $DestinationHost
 	}
 
 	# check source host
-	switch ($vm_host) {
+	switch ($vm_host_source) {
 		'cloud' {
 			$vm_in_the_cloud = $true
 		}
@@ -72,18 +70,18 @@ ForEach ($VmParams in $vm_list) {
 		}
 		Default {
 			Try {
-				$null = Test-WSMan -ComputerName $vm_host -Authentication 'Default'
-				Write-Host ("$Hostname - connected to source host: $vm_host")
+				$null = Test-WSMan -ComputerName $vm_host_source -Authentication 'Default'
+				Write-Host ("$Hostname - connected to source host: $vm_host_source")
 			}
 			Catch {
-				Write-Host ("$Hostname - ERROR: could not connect to source host: $vm_host")
+				Write-Host ("$Hostname - ERROR: could not connect to source host: $vm_host_source")
 				Return
 			}
 		}
 	}
 
 	# check destination host
-	switch ($vm_host_dest) {
+	switch ($vm_host_target) {
 		'cloud' {
 			$vm_in_the_cloud = $true
 		}
@@ -93,11 +91,11 @@ ForEach ($VmParams in $vm_list) {
 		}
 		Default {
 			Try {
-				$null = Test-WSMan -ComputerName $vm_host_dest -Authentication 'Default'
-				Write-Host ("$Hostname - connected to destination host: $vm_host_dest")
+				$null = Test-WSMan -ComputerName $vm_host_target -Authentication 'Default'
+				Write-Host ("$Hostname - connected to destination host: $vm_host_target")
 			}
 			Catch {
-				Write-Host ("$Hostname - ERROR: could not connect to destination host: $vm_host_dest")
+				Write-Host ("$Hostname - ERROR: could not connect to destination host: $vm_host_target")
 				Return
 			}
 		}
@@ -109,33 +107,41 @@ ForEach ($VmParams in $vm_list) {
 		Return
 	}
 
+	# set path information
+	If ($Reverse) {
+		$vm_path_target = $VmParams.Path
+	}
+	Else {
+		$vm_path_target = $DestinationPath
+	}
+
 	# set destination path
-	If (!($vm_path_dest)) {
-		$vm_path_dest = (Get-VMHost -ComputerName $vm_host_dest).VirtualMachinePath
-		Write-Host ("$Hostname - ...using default VM path: $vm_path_dest")
+	If (!($vm_path_target)) {
+		$vm_path_target = (Get-VMHost -ComputerName $vm_host_target).VirtualMachinePath
+		Write-Host ("$Hostname - ...using default VM path: $vm_path_target")
 	}
 
 	# create the VM specific path
-	$vm_path_vm = Invoke-Command -ComputerName $vm_host_dest -ScriptBlock { Join-Path -Path $using:vm_path_dest -ChildPath $using:vm_name }
+	$vm_path_vm = Invoke-Command -ComputerName $vm_host_target -ScriptBlock { Join-Path -Path $using:vm_path_target -ChildPath $using:vm_name }
 
 	# check if dest is clustered
-	$vm_dest_cl = $null
-	$vm_dest_cl = Get-Service -ComputerName $vm_host_dest | Where-Object { $_.Name -eq 'ClusSvc' -and $_.StartType -eq 'Automatic' -and $_.Status -eq 'Running' }
-	If ($vm_dest_cl) {
+	$vm_host_target_cl = $null
+	$vm_host_target_cl = Get-Service -ComputerName $vm_host_target | Where-Object { $_.Name -eq 'ClusSvc' -and $_.StartType -eq 'Automatic' -and $_.Status -eq 'Running' }
+	If ($vm_host_target_cl) {
 		# check for VM on cluster
-		$vm_dest_cluster = Invoke-Command -ComputerName $vm_host_dest { (Get-Cluster).Name }
-		$vm_to_cl = Get-ClusterGroup -Cluster $vm_dest_cluster | Where-Object { $_.Name -eq $vm_name -and $_.GroupType -eq 'VirtualMachine' }
+		$vm_host_target_cluster = Invoke-Command -ComputerName $vm_host_target { (Get-Cluster).Name }
+		$vm_to_cl = Get-ClusterGroup -Cluster $vm_host_target_cluster | Where-Object { $_.Name -eq $vm_name -and $_.GroupType -eq 'VirtualMachine' }
 		If ($vm_to_cl) {
-			Write-Host ("$Hostname - ...cluster resource for VM found on destination cluster: $vm_dest_cluster")
+			Write-Host ("$Hostname - ...cluster resource for VM found on destination cluster: $vm_host_target_cluster")
 			Write-Host ("$Hostname - ...skipping!")
 			Return
 		}
 	}
 
 	# check for VM on destination host
-	$vm_on_dest = Get-VM -ComputerName $vm_host_dest | Where-Object { $_.Name -eq $vm_name }
-	If ($vm_on_dest) {
-		Write-Host ("$Hostname - ....VM found on destination: $vm_host_dest")
+	$vm_on_target = Get-VM -ComputerName $vm_host_target | Where-Object { $_.Name -eq $vm_name }
+	If ($vm_on_target) {
+		Write-Host ("$Hostname - ....VM found on destination: $vm_host_target")
 		Write-Host ("$Hostname - ...skipping!")
 		Return
 	}
@@ -145,11 +151,12 @@ ForEach ($VmParams in $vm_list) {
 	$vm_host_cl = Get-Service -ComputerName $vm_host | Where-Object { $_.Name -eq 'ClusSvc' -and $_.StartType -eq 'Automatic' -and $_.Status -eq 'Running' }
 	If ($vm_host_cl) {
 		# check for VM on cluster
-		$vm_cluster = Invoke-Command -ComputerName $vm_host { (Get-Cluster).Name }
-		$vm_on_cl = Get-ClusterGroup -Cluster $vm_cluster | Where-Object { $_.Name -eq $vm_name -and $_.GroupType -eq 'VirtualMachine' }
-		If ($vm_on_cl) {
+		$vm_host_cluster = Invoke-Command -ComputerName $vm_host { (Get-Cluster).Name }
+		$vm_on_cluster = Get-ClusterGroup -Cluster $vm_host_cluster | Where-Object { $_.Name -eq $vm_name -and $_.GroupType -eq 'VirtualMachine' }
+		If ($vm_on_cluster) {
+			Write-Host ("$Hostname - ...cluster resource for VM found on source cluster: $vm_host_cluster")
 			# verify the resource group is on the local node
-			$vm_node = $vm_on_cl.OwnerNode.NodeName
+			$vm_node = $vm_on_cluster.OwnerNode.NodeName
 			If ($vm_host -ne $vm_node) {
 				Write-Host ("$Hostname - ...cluster resource for VM found on different host, changing host to: $vm_node")
 				$vm_host = $vm_node
@@ -167,23 +174,23 @@ ForEach ($VmParams in $vm_list) {
 	}
 
 	# remove VM from source cluster
-	If ($vm_on_cl) {
+	If ($vm_on_cluster) {
 		# remove resource group from the cluster
-		Write-Host ("$Hostname - ...removing cluster resource on source: " + $vm_cluster)
-		$vm_on_cl | Remove-ClusterGroup -RemoveResources -Force
+		Write-Host ("$Hostname - ...removing cluster resource on source: " + $vm_host_cluster)
+		$vm_on_cluster | Remove-ClusterGroup -RemoveResources -Force
 	}
 
 	# move VM
-	If ($vm_dest_cl) {
+	If ($vm_host_target_cl) {
 		Write-Host ("$Hostname - moving VM ...")
-		Write-Host ("$Hostname - ...to cluster member: " + $vm_host_dest)
+		Write-Host ("$Hostname - ...to cluster member: " + $vm_host_target)
 	}
 	Else {
 		Write-Host ("$Hostname - moving VM ...")
-		Write-Host ("$Hostname - ...to Hyper-V server: " + $vm_host_dest)
+		Write-Host ("$Hostname - ...to Hyper-V server: " + $vm_host_target)
 	}
 	try {
-		Move-VM -ComputerName $vm_host -Name $vm_name -DestinationHost $vm_host_dest -IncludeStorage -DestinationStoragePath $vm_path_vm
+		Move-VM -ComputerName $vm_host -Name $vm_name -DestinationHost $vm_host_target -IncludeStorage -DestinationStoragePath $vm_path_vm
 		Write-Host ("$Hostname - ...move complete!")
 	}
 	catch {
@@ -191,9 +198,9 @@ ForEach ($VmParams in $vm_list) {
 	}
 
 	# add VM to cluster
-	If ($vm_dest_cl) {
-		Write-Host ("$Hostname - ...adding to cluster: " + $vm_dest_cluster)
-		Add-ClusterVirtualMachineRole -Cluster $vm_dest_cluster -VMName $vm_name | Out-Null
+	If ($vm_host_target_cl) {
+		Write-Host ("$Hostname - ...adding to cluster: " + $vm_host_target_cluster)
+		Add-ClusterVirtualMachineRole -Cluster $vm_host_target_cluster -VMName $vm_name | Out-Null
 	}
 }
 
