0a85b10961b9587a6341cadd7319dd8c251ae6f5
alexbarthnet <81780226+alexbarthnet@users.noreply.github.com>
2022-09-15 13:51:19 -0500

    updated restore process and variable names

--- OneDrive/Unmount-OneDrive.ps1
+++ OneDrive/Unmount-OneDrive.ps1
@@ -1,17 +1,23 @@
 [CmdletBinding(DefaultParameterSetName = 'Default')]
 Param(
 	[string]$Identity,
-	[switch]$SkipExcludedFoldersCheck,
-	[switch]$ExcludeFolders,
-	[string[]]$ExcludedFolders,
 	[switch]$RestoreFolders,
 	[string[]]$FoldersToRestore
 )
 
+# define the default OneDrive excluded folders list
+$FoldersToRestore_default = @()
+$FoldersToRestore_default += 'Desktop'
+$FoldersToRestore_default += 'Documents'
+$FoldersToRestore_default += 'Downloads'
+$FoldersToRestore_default += 'Music'
+$FoldersToRestore_default += 'Pictures'
+$FoldersToRestore_default += 'Videos'
+
 # buffer output
 Write-Output "`n"
 
-# get the junctioned path(s)
+# get the OneDrive path(s) so we can filter which path(s) to restore
 $onedrive_directory = $null
 switch ($Identity) {
 	$null {
@@ -53,22 +59,30 @@ Else {
 Write-Output "`n"
 
 # define OneDrive directories that will *NOT* be un-junctioned
-If ($ExcludeFolders) {
+If ($RestoreFolders) {
 	Write-Output ('Checking the excluded folders...')
-	If ($ExcludedFolders.Count -ge 1) {
-		If (-not $SkipExcludedFoldersCheck) {
-			ForEach ($ExcludedFolder in $ExcludedFolders) {
-				$ExcludedFolder_path = Join-Path -Path $onedrive_directory -ChildPath $ExcludedFolder
-				If (Test-Path $ExcludedFolder_path) {
-					Write-Output ("`t Located: $ExcludedFolder_path")
-				}
-				Else {
-					Write-Output ("`t Missing: $ExcludedFolder_path")
-					Write-Output ('WARNING: the folder above was defined in the ExcludedFolders parameter but not found in OneDrive, exiting!')
-					Return
-				}
+	If ($FoldersToRestore.Count -ge 1) {
+		ForEach ($FolderToRestore in $FoldersToRestore) {
+			$FolderToRestore_path = Join-Path -Path $onedrive_directory -ChildPath $FolderToRestore
+			If (Test-Path $FolderToRestore_path) {
+				Write-Output ("`t Located: $FolderToRestore_path")
+			}
+			Else {
+				Write-Output ("`t Missing: $FolderToRestore_path")
+				Write-Output ('WARNING: the folder above was defined in the FolderToExcludes parameter but not found in OneDrive, exiting!')
+				Return
 			}
 		}
+	}
+	Else {
+		Write-Output ('NOTICE: no restore folders were explicitly defined')
+		Write-Output ('...the following default folders will be restored from junctions:')
+		$FoldersToRestore = $FoldersToRestore_default
+		ForEach ($FolderToRestore in $FoldersToRestore) {
+			$FolderToRestore_path = Join-Path -Path $onedrive_directory -ChildPath $FolderToRestore
+			Write-Output ("`t $FolderToRestore_path")
+		}
+		# insert warning here!
 	}
 }
 
@@ -78,32 +92,39 @@ Write-Output ('-----')
 
 # loop through directories inside profile
 Get-ChildItem -Path $env:USERPROFILE | Where-Object { $_.PSIsContainer -and $_.LinkType -eq 'Junction' -and $_.Target -match $onedrive_directory.Name } | ForEach-Object {
+	# define variables and declare folder
 	$folder_basename = ($_.BaseName)
 	$folder_fullname = ($_.FullName)
 	$folder_target = ($_.Target)
 	Write-Output (' ')
 	Write-Output ("Found OneDrive folder: '" + $folder_fullname + "'")
-	# check if current folder matching the block list
-	If ($ExcludedFolders -contains $folder_basename) {
-		Write-Output ("...'" + $folder_basename + "' explicitly blocked, skipping!")
+
+	# remove junction
+	Try {
+		# $_.Delete()
+		Invoke-Expression -Command "fsutil reparsepoint delete $folder_fullname"
+		Write-Output ("...'" + $folder_basename + "' removing junction!")
 	}
-	Else {
-		# remove junction
-		Try {
-			# $_.Delete()
-			Invoke-Expression "fsutil reparsepoint delete $folder_fullname"
-			Write-Output ("...'" + $folder_basename + "' removing junction!")
-		}
-		Catch {
-			Write-Output ('ERROR: the junction above could not be removed, exiting!')
-		}
-		# export junctioned contents into restored folders
-		If ($RestoreFolders) {
-			If ($FoldersToRestore -contains $folder_basename ) {
-				Get-ChildItem -Path $folder_target -Recurse -Force | ForEach-Object {
-					$path_old = $_.FullName
-					$path_new = $_.FullName.Replace($folder_target,$folder_fullname)
-					Move-Item -Path $path_old -Destination $path_new
+	Catch {
+		Write-Output ('ERROR: the junction above could not be removed, exiting!')
+		Return
+	}
+
+	# restore contents of junctions to original folders
+	If ($RestoreFolders) {
+		If ($FoldersToRestore -contains $folder_basename ) {
+			Write-Output ("...'" + $folder_basename + "' restoring contents...")
+			$path_items = Get-ChildItem -Path $folder_target -Recurse -Force
+			$path_items | ForEach-Object {
+				$path_old = $_.FullName
+				$path_new = $_.FullName.Replace($folder_target, $folder_fullname)
+				Try {
+					Write-Output ("... - restoring '$path_new'...")
+					Copy-Item -Path $path_old -Destination $path_new -Force
+				}
+				Catch {
+					Write-Output ('ERROR: could restore previous file or folder, exiting!')
+					Return
 				}
 			}
 		}
