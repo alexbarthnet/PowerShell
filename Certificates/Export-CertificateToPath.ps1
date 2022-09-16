ddaa52042e66ec036c7140a02daf88c710cc0a09
alexbarthnet <81780226+alexbarthnet@users.noreply.github.com>
2022-09-16 17:12:18 -0500

    added BPE blocks and hostname to transcript file

--- Certificates/Export-CertificateToPath.ps1
+++ Certificates/Export-CertificateToPath.ps1
@@ -16,197 +16,205 @@ Param(
 	[Parameter(Position = 3, Mandatory = $True, ParameterSetName = 'Add')][ValidatePattern('^[^\*]+$')]
 	[string[]]$Principals,
 	[Parameter()]
-	[string]$Json = $PSCommandPath.Replace((Get-Item -Path $PSCommandPath).Extension, '.json')
+	[string]$Json = $PSCommandPath.Replace((Get-Item -Path $PSCommandPath).Extension, '.json'),
+	# local hostname
+	[Parameter(DontShow)]
+	[string]$HostName = ([System.Environment]::MachineName.ToLowerInvariant())
+
 )
 
-Function Export-CertificateChainFiles {
-	[CmdletBinding()]
-	Param(
-		[Parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $true)]
-		[object]$Certificate,
-		[Parameter(Position = 1, Mandatory = $true)][ValidateScript({ Test-Path -Path $_ })]
-		[string]$Path,
-		[Parameter(Position = 2)]
-		[string]$Prefix = [string]::Empty
-	)
-
-	# build prefix if not provided
-	If ([string]::IsNullOrEmpty($Prefix)) {
-		# retrieve subject and NotBefore from input certificate
-		$cert_file_head = $Certificate.Subject.Split(',', 2)[0].Split('=', 2)[-1]
-		$cert_file_date = Get-Date -Date $Certificate.NotBefore -Format 'FileDateTimeUniversal'
-		# define prefix for exported certificates from input certificate
-		$Prefix = $cert_file_head, $cert_file_date -join '_'
+Begin {
+	# if result exists...
+	If ($null -ne $Result) {
+		# ...define transcript file from script path and start transcript 
+		Start-Transcript -Path $PSCommandPath.Replace((Get-Item -Path $PSCommandPath).Extension, "_$Hostname.txt") -Force
 	}
-
-	# create certificate chain object
-	$cert_chain = New-Object System.Security.Cryptography.X509Certificates.X509Chain
-	$cert_chain.ChainPolicy.RevocationMode = 'NoCheck'
-
-	# build certificate chain
-	If ($cert_chain.Build($Certificate)) {
-		# start certificate chain counter
-		$cert_counter = 0
-
-		# define format for certificate chain counter string
-		$cert_counter_string_format = "d$($cert_chain.ChainElements.Certificate.Count.ToString().Length)"
-
-		# export certificate chain to storage
-		ForEach ($cert_element in $cert_chain.ChainElements.Certificate) {
-			switch ($cert_element.Subject) {
-				$Certificate.Subject {
-					# export input certificate
-					$cert_chain_tail = 'cert.cer'
-					$cert_chain_path = Join-Path -Path $Path -ChildPath ($Prefix, $cert_chain_tail -join '_')
-					Write-Host " - exporting public key cert  : $cert_chain_path"
-					$null = $cert_element | Export-Certificate -FilePath $cert_chain_path
-				}
-				$cert_element.Issuer {
-					# export root certificate
-					$cert_chain_tail = 'root.cer'
-					$cert_chain_path = Join-Path -Path $Path -ChildPath ($Prefix, $cert_chain_tail -join '_')
-					Write-Host " - exporting CA cert of root  : $cert_chain_path"
-					$null = $cert_element | Export-Certificate -FilePath $cert_chain_path
-				}
-				Default {
-					# increment counter for intermediate chain certificates
-					$cert_counter++
-					# export intermediate chain certificate
-					$cert_chain_tail = 'chain' + $cert_counter.ToString($cert_counter_string_format) + '.cer'
-					$cert_chain_path = Join-Path -Path $Path -ChildPath ($Prefix, $cert_chain_tail -join '_')
-					Write-Host " - exporting CA cert in chain : $cert_chain_path"
-					$null = $cert_element | Export-Certificate -FilePath $cert_chain_path
+	Function Export-CertificateChainFiles {
+		[CmdletBinding()]
+		Param(
+			[Parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $true)]
+			[object]$Certificate,
+			[Parameter(Position = 1, Mandatory = $true)][ValidateScript({ Test-Path -Path $_ })]
+			[string]$Path,
+			[Parameter(Position = 2)]
+			[string]$Prefix = [string]::Empty
+		)
+	
+		# build prefix if not provided
+		If ([string]::IsNullOrEmpty($Prefix)) {
+			# retrieve subject and NotBefore from input certificate
+			$cert_file_head = $Certificate.Subject.Split(',', 2)[0].Split('=', 2)[-1]
+			$cert_file_date = Get-Date -Date $Certificate.NotBefore -Format 'FileDateTimeUniversal'
+			# define prefix for exported certificates from input certificate
+			$Prefix = $cert_file_head, $cert_file_date -join '_'
+		}
+	
+		# create certificate chain object
+		$cert_chain = New-Object System.Security.Cryptography.X509Certificates.X509Chain
+		$cert_chain.ChainPolicy.RevocationMode = 'NoCheck'
+	
+		# build certificate chain
+		If ($cert_chain.Build($Certificate)) {
+			# start certificate chain counter
+			$cert_counter = 0
+	
+			# define format for certificate chain counter string
+			$cert_counter_string_format = "d$($cert_chain.ChainElements.Certificate.Count.ToString().Length)"
+	
+			# export certificate chain to storage
+			ForEach ($cert_element in $cert_chain.ChainElements.Certificate) {
+				switch ($cert_element.Subject) {
+					$Certificate.Subject {
+						# export input certificate
+						$cert_chain_tail = 'cert.cer'
+						$cert_chain_path = Join-Path -Path $Path -ChildPath ($Prefix, $cert_chain_tail -join '_')
+						Write-Host " - exporting public key cert  : $cert_chain_path"
+						$null = $cert_element | Export-Certificate -FilePath $cert_chain_path
+					}
+					$cert_element.Issuer {
+						# export root certificate
+						$cert_chain_tail = 'root.cer'
+						$cert_chain_path = Join-Path -Path $Path -ChildPath ($Prefix, $cert_chain_tail -join '_')
+						Write-Host " - exporting CA cert of root  : $cert_chain_path"
+						$null = $cert_element | Export-Certificate -FilePath $cert_chain_path
+					}
+					Default {
+						# increment counter for intermediate chain certificates
+						$cert_counter++
+						# export intermediate chain certificate
+						$cert_chain_tail = 'chain' + $cert_counter.ToString($cert_counter_string_format) + '.cer'
+						$cert_chain_path = Join-Path -Path $Path -ChildPath ($Prefix, $cert_chain_tail -join '_')
+						Write-Host " - exporting CA cert in chain : $cert_chain_path"
+						$null = $cert_element | Export-Certificate -FilePath $cert_chain_path
+					}
 				}
 			}
 		}
-	}
-	Else {
-		Write-Host "ERROR: building certificate chain for '$($Certificate.Subject)'"
-	}
-}
-
-Function Export-PfxCertificateToPrincipals {
-	[CmdletBinding()]
-	Param(
-		[Parameter(Position = 0, Mandatory = $true)]
-		[datetime]$Date,
-		[Parameter(Position = 1, Mandatory = $true)]
-		[string]$Hash,
-		[Parameter(Position = 2, Mandatory = $true)][ValidatePattern('^[^\*]+$')]
-		[string]$Name,
-		[Parameter(Position = 3, Mandatory = $true)][ValidateScript({ Test-Path -Path $_ })]
-		[string]$Storage,
-		[Parameter(Position = 4, Mandatory = $true)]
-		[string[]]$Principals
-	)
-
-	# declare start
-	Write-Host "`nExporting private key and certificate chain"
-
-	# retrieve certificate
-	$cert_object = $null
-	$cert_object = Get-ChildItem -Path 'Cert:\LocalMachine\My' | Where-Object { $_.Thumbprint -eq $Hash } | Sort-Object NotBefore | Select-Object -Last 1
-
-	# export certificate and certificate chain to storage
-	If ($cert_object) {
-		# define full path to pfx
-		$cert_file_head = $cert_object.Subject.Split(',', 2)[0].Split('=', 2)[-1]
-		$cert_file_date = Get-Date -Date $cert_object.NotBefore -Format 'FileDateTimeUniversal'
-		$cert_file_tail = 'cert.pfx'
-		$cert_file_name = $cert_file_head, $cert_file_date, $cert_file_tail -join '_'
-		$cert_file_path = Join-Path -Path $Storage -ChildPath $cert_file_name
-
-		# export PFX to storage
-		Write-Host " - exporting keypair to PFX   : $cert_file_path"
-		$null = $cert_object | Export-PfxCertificate -FilePath $cert_file_path -ProtectTo $Principals -ChainOption EndEntityCertOnly -CryptoAlgorithmOption AES256_SHA256
-
-		# export certificate chain to storage
-		$cert_object | Export-CertificateChainFiles -Path $Storage
-	}
-	Else {
-		Write-Host "ERROR: certificate not found with hash: '$Hash'"
-	}
-}
-
-# verify JSON file
-If (-not (Test-Path -Path $Json)) {
-	If ($Add) {
-		Try {
-			$null = New-Item -ItemType 'File' -Path $Json
+		Else {
+			Write-Host "ERROR: building certificate chain for '$($Certificate.Subject)'"
 		}
-		Catch {
-			Write-Output "`nERROR: could not create configuration file:"
+	}
+	
+	Function Export-PfxCertificateToPrincipals {
+		[CmdletBinding()]
+		Param(
+			[Parameter(Position = 0, Mandatory = $true)]
+			[datetime]$Date,
+			[Parameter(Position = 1, Mandatory = $true)]
+			[string]$Hash,
+			[Parameter(Position = 2, Mandatory = $true)][ValidatePattern('^[^\*]+$')]
+			[string]$Name,
+			[Parameter(Position = 3, Mandatory = $true)][ValidateScript({ Test-Path -Path $_ })]
+			[string]$Storage,
+			[Parameter(Position = 4, Mandatory = $true)]
+			[string[]]$Principals
+		)
+	
+		# declare start
+		Write-Host "`nExporting private key and certificate chain"
+	
+		# retrieve certificate
+		$cert_object = $null
+		$cert_object = Get-ChildItem -Path 'Cert:\LocalMachine\My' | Where-Object { $_.Thumbprint -eq $Hash } | Sort-Object NotBefore | Select-Object -Last 1
+	
+		# export certificate and certificate chain to storage
+		If ($cert_object) {
+			# define full path to pfx
+			$cert_file_head = $cert_object.Subject.Split(',', 2)[0].Split('=', 2)[-1]
+			$cert_file_date = Get-Date -Date $cert_object.NotBefore -Format 'FileDateTimeUniversal'
+			$cert_file_tail = 'cert.pfx'
+			$cert_file_name = $cert_file_head, $cert_file_date, $cert_file_tail -join '_'
+			$cert_file_path = Join-Path -Path $Storage -ChildPath $cert_file_name
+	
+			# export PFX to storage
+			Write-Host " - exporting keypair to PFX   : $cert_file_path"
+			$null = $cert_object | Export-PfxCertificate -FilePath $cert_file_path -ProtectTo $Principals -ChainOption EndEntityCertOnly -CryptoAlgorithmOption AES256_SHA256
+	
+			# export certificate chain to storage
+			$cert_object | Export-CertificateChainFiles -Path $Storage
+		}
+		Else {
+			Write-Host "ERROR: certificate not found with hash: '$Hash'"
+		}
+	}	
+}
+
+Process {
+	# verify JSON file
+	If (-not (Test-Path -Path $Json)) {
+		If ($Add) {
+			Try {
+				$null = New-Item -ItemType 'File' -Path $Json
+			}
+			Catch {
+				Write-Output "`nERROR: could not create configuration file:"
+				Write-Output "$Json`n"
+				Return
+			}
+		}
+		Else {
+			Write-Output "`nERROR: could not find configuration file:"
 			Write-Output "$Json`n"
 			Return
 		}
 	}
-	Else {
-		Write-Output "`nERROR: could not find configuration file:"
-		Write-Output "$Json`n"
-		Return
-	}
-}
 
-# import JSON data
-$json_data = @()
-$json_data += Get-Content -Path $Json | ConvertFrom-Json
+	# import JSON data
+	$json_data = @()
+	$json_data += Get-Content -Path $Json | ConvertFrom-Json
 
-# evaluate parameters
-switch ($true) {
-	$Clear {
-		# remove configuration file
-		If (Test-Path -Path $Json) {
+	# evaluate parameters
+	switch ($true) {
+		$Clear {
+			# remove configuration file
+			If (Test-Path -Path $Json) {
+				Try {
+					Remove-Item -Path $Json -Force
+					Write-Output "`nCleared configuration file: '$Json'"
+				}
+				Catch {
+					Write-Output "`nERROR: could not clear configuration file: '$Json'"
+				}
+			}
+		}
+		$Remove {
+			# remove matching entries from object
 			Try {
-				Remove-Item -Path $Json -Force
-				Write-Output "`nCleared configuration file: '$Json'"
+				$json_data = $json_data | Where-Object {
+					$_.Subject -ne $Subject
+				}
+				If ($null -eq $json_data) {
+					[string]::Empty | Set-Content -Path $Json
+					Write-Output "`nRemoved '$Subject' from configuration file: '$Json'"
+				}
+				Else {
+					$json_data | ConvertTo-Json | Set-Content -Path $Json
+					Write-Output "`nRemoved '$Subject' from configuration file: '$Json'"
+				}
+				$json_data | Select-Object Subject, Storage, Principals, Updated
 			}
 			Catch {
-				Write-Output "`nERROR: could not clear configuration file: '$Json'"
+				Write-Output "`nERROR: could not update configuration file: '$Json'"
 			}
 		}
-	}
-	$Remove {
-		# remove matching entries from object
-		Try {
-			$json_data = $json_data | Where-Object {
-				$_.Subject -ne $Subject
-			}
-			If ($null -eq $json_data) {
-				[string]::Empty | Set-Content -Path $Json
-				Write-Output "`nRemoved '$Subject' from configuration file: '$Json'"
-			}
-			Else {
+		$Add {
+			# create custom object from parameters then add to object
+			Try {
+				$json_data += [pscustomobject]@{
+					Subject    = [string]$Subject 
+					Storage    = [string]$Storage
+					Principals = [string[]]$Principals
+					Updated    = (Get-Date -Format FileDateTimeUniversal)
+				}
 				$json_data | ConvertTo-Json | Set-Content -Path $Json
-				Write-Output "`nRemoved '$Subject' from configuration file: '$Json'"
+				Write-Output "`nAdded '$Subject' to configuration file: '$Json'"
+				$json_data | Select-Object Subject, Storage, Principals, Updated
 			}
-			$json_data | Select-Object Subject, Storage, Principals, Updated
-		}
-		Catch {
-			Write-Output "`nERROR: could not update configuration file: '$Json'"
-		}
-	}
-	$Add {
-		# create custom object from parameters then add to object
-		Try {
-			$json_data += [pscustomobject]@{
-				Subject    = [string]$Subject 
-				Storage    = [string]$Storage
-				Principals = [string[]]$Principals
-				Updated    = (Get-Date -Format FileDateTimeUniversal)
+			Catch {
+				Write-Output "`nERROR: could not update configuration file: '$Json'"
 			}
-			$json_data | ConvertTo-Json | Set-Content -Path $Json
-			Write-Output "`nAdded '$Subject' to configuration file: '$Json'"
-			$json_data | Select-Object Subject, Storage, Principals, Updated
 		}
-		Catch {
-			Write-Output "`nERROR: could not update configuration file: '$Json'"
-		}
-	}
-	{ $null -ne $Result } {
-		Try {
-			# define transcript file from script path and start transcript
-			Start-Transcript -Path $PSCommandPath.Replace((Get-Item -Path $PSCommandPath).Extension, '.txt') -Force
-
+		{ $null -ne $Result } {
 			# retrieve values from $Result
 			$cert_name = $Result.ManagedItem.Name
 			$cert_date = $Result.ManagedItem.DateStart
@@ -271,13 +279,17 @@ switch ($true) {
 				Write-Host "ERROR: unable to locate matching subject or '_default' entry with valid storage and principals"
 			}
 		}
-		Finally {
-			Write-Host ([string]::Empty)
-			Stop-Transcript
+		Default {
+			Write-Output "`nDisplaying '$Json'`n"
+			$json_data | Select-Object Subject, Storage, Principals, Updated
 		}
 	}
-	Default {
-		Write-Output "`nDisplaying '$Json'`n"
-		$json_data | Select-Object Subject, Storage, Principals, Updated
+}
+
+End {
+	# if result exists...
+	If ($null -ne $Result) {
+		# ...stop transcript
+		Stop-Transcript
 	}
 }
