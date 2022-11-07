2b8085d1fb359a049950a8e485f0575438e0e242
alexbarthnet <81780226+alexbarthnet@users.noreply.github.com>
2022-11-07 08:30:58 -0600

    now tries 5 times, now with more reporting!

--- Provisioning/Update-ComputerName.ps1
+++ Provisioning/Update-ComputerName.ps1
@@ -9,8 +9,30 @@ $vm_name = (Get-Item 'HKLM:\SOFTWARE\Microsoft\Virtual Machine\Guest\Parameters'
 If ($os_name -ne $vm_name -and -not [string]::IsNullOrEmpty($vm_name)) {
 	Start-Transcript -Path $log_path -Append
 	Write-Output "Found active computer name: $os_name"
-	Write-Output "Found virtual machine name: $vm_name"	
+	Write-Output "Found virtual machine name: $vm_name"
 	Write-Output "Renaming computer to: $vm_name"
-	Rename-Computer -NewName $vm_name -Restart -Force
+	# define variables for loop
+	$vm_renamed = $false
+	$loop_count = 1
+	# make 5 attempsts at renaming computer
+	Do {
+		Try {
+			# sleep to allow for domain replication of new computer objects
+			Start-Sleep -Seconds 5
+			# get boolean from returned object
+			$vm_renamed = Rename-Computer -NewName $vm_name -Force -PassThru | Select-Object 'HasSucceeded' -ExpandProperty 'HasSucceeded'
+		}
+		Catch {
+			Write-Output "...error renaming computer on try #$($loop_count): $($_.ToString())"
+		}
+		Finally {
+			$loop_count++
+		}
+	}
+	Until ($vm_renamed -or $loop_count -gt 5)
+	If ($vm_renamed) {
+		Write-Output "...renamed computer on try #$($loop_count)"
+		Restart-Computer -Force
+	}
 	Stop-Transcript
 }
