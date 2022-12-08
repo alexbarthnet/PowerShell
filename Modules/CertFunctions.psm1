51fa6b9ab6d91778c04f89bf7e27ff44c8e821aa
alexbarthnet <81780226+alexbarthnet@users.noreply.github.com>
2022-12-07 23:44:28 -0600

    add CBH and cleanup object names

--- Modules/CertFunctions.psm1
+++ Modules/CertFunctions.psm1
@@ -1,25 +1,63 @@
 Function ConvertTo-X509Certificate {
+	<#
+	.SYNOPSIS
+	Convert an input into an X.509 certificate object.
+
+	.DESCRIPTION
+	Convert an input into an X.509 certificate object. The input can be a byte array, a collection where the first element is a byte array, or the string representation of a byte array.
+
+	.PARAMETER InputObject
+	Specifies the input that represents an X.509 certificate
+
+	.INPUTS
+	None.
+
+	.OUTPUTS
+	None.
+
+	#>
 	[CmdletBinding()]
 	Param (
 		[Parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $true)]
-		[object]$Input
+		[object]$InputObject
 	)
 
 	# create empty certificate object
-	$x509_cert = New-Object -TypeName 'System.Security.Cryptography.X509Certificates.X509Certificate2'
+	$Certificate = New-Object -TypeName 'System.Security.Cryptography.X509Certificates.X509Certificate2'
 
-	# import the certificate data into the object
-	switch ($true) {
-		{ $Input[0] -is [byte[]] } { $x509_cert.Import($Input[0]) }
-		{ $Input -is [byte[]] } { $x509_cert.Import($Input) }
-		Default { $x509_cert.Import([byte[]]$Input) }
+	# import byte array into certificate object
+	If ( $InputObject[0] -is [byte[]] ) {
+		$Certificate.Import($InputObject[0])
+	}
+	ElseIf ( $InputObject -is [byte[]] ) {
+		$Certificate.Import($InputObject)
+	}
+	Else {
+		$Certificate.Import([byte[]]$InputObject)
 	}
 
 	# return populated certificate object
-	$x509_cert
+	$Certificate
 }
 
 Function Test-Thumbprint {
+	<#
+	.SYNOPSIS
+	Tests if a string is a certificate thumbprint.
+
+	.DESCRIPTION
+	Tests if string is exactly 40 characters long and only contains hexadecimal characters. The function returns true if the string meets the criteria and false otherwise.
+
+	.PARAMETER Thumbprint
+	Specifies the string to be tested.
+
+	.INPUTS
+	System.String
+
+	.OUTPUTS
+	System.Boolean
+
+	#>
 	[CmdletBinding()]
 	Param(
 		[Parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $true)]
