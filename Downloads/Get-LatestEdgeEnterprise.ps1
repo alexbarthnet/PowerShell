8b2f91aae739657c8003e94a43df36d9b0be891c
alexbarthnet <github@alexbarth.net>
2023-07-27 09:24:26 -0500

    fixed parameter name

--- Downloads/Get-LatestEdgeEnterprise.ps1
+++ Downloads/Get-LatestEdgeEnterprise.ps1
@@ -16,14 +16,14 @@ $edge_json = Invoke-RestMethod -Uri "https://edgeupdates.microsoft.com/api/produ
 # get and set browser information
 $edge_msi_down = $false
 $edge_msi_file = Join-Path -Path $Destination -ChildPath "MicrosoftEdgeEnterpriseX64.msi"
-$edge_msi_size = ($edge_json | Where-Object {$_.Product -eq "Stable"} | Select-Object -ExpandProperty 'Releases' | Where-Object {$_.Architecture -eq "x64" -and $_.Platform -eq "Windows" -and $_.Artifacts -ne $null} | Sort-Object -Proerty 'ProductVersion' -Descending | Select-Object -First 1 | Select-Object -ExpandProperty 'Artifacts' | Where-Object {$_.ArtifactName -eq 'msi'}).SizeInBytes
-$edge_msi_path = ($edge_json | Where-Object {$_.Product -eq "Stable"} | Select-Object -ExpandProperty 'Releases' | Where-Object {$_.Architecture -eq "x64" -and $_.Platform -eq "Windows" -and $_.Artifacts -ne $null} | Sort-Object -Proerty 'ProductVersion' -Descending | Select-Object -First 1 | Select-Object -ExpandProperty 'Artifacts' | Where-Object {$_.ArtifactName -eq 'msi'}).Location
+$edge_msi_size = ($edge_json | Where-Object {$_.Product -eq "Stable"} | Select-Object -ExpandProperty 'Releases' | Where-Object {$_.Architecture -eq "x64" -and $_.Platform -eq "Windows" -and $_.Artifacts -ne $null} | Sort-Object -Property 'ProductVersion' -Descending | Select-Object -First 1 | Select-Object -ExpandProperty 'Artifacts' | Where-Object {$_.ArtifactName -eq 'msi'}).SizeInBytes
+$edge_msi_path = ($edge_json | Where-Object {$_.Product -eq "Stable"} | Select-Object -ExpandProperty 'Releases' | Where-Object {$_.Architecture -eq "x64" -and $_.Platform -eq "Windows" -and $_.Artifacts -ne $null} | Sort-Object -Property 'ProductVersion' -Descending | Select-Object -First 1 | Select-Object -ExpandProperty 'Artifacts' | Where-Object {$_.ArtifactName -eq 'msi'}).Location
 
 # get and set policy information
 $edge_pol_down = $false
 $edge_pol_file = Join-Path -Path $Destination -ChildPath "MicrosoftEdgePolicyTemplates.zip"
-$edge_pol_size = ($edge_json | Where-Object {$_.Product -eq "Policy"} | Select-Object -ExpandProperty 'Releases' | Where-Object {$_.Architecture -eq "any" -and $_.Platform -eq "Any" -and $_.Artifacts -ne $null} | Sort-Object -Proerty 'ProductVersion' -Descending | Select-Object -First 1 | Select-Object -ExpandProperty 'Artifacts' | Where-Object {$_.ArtifactName -eq 'zip'}).SizeInBytes
-$edge_pol_path = ($edge_json | Where-Object {$_.Product -eq "Policy"} | Select-Object -ExpandProperty 'Releases' | Where-Object {$_.Architecture -eq "any" -and $_.Platform -eq "Any" -and $_.Artifacts -ne $null} | Sort-Object -Proerty 'ProductVersion' -Descending | Select-Object -First 1 | Select-Object -ExpandProperty 'Artifacts' | Where-Object {$_.ArtifactName -eq 'zip'}).Location
+$edge_pol_size = ($edge_json | Where-Object {$_.Product -eq "Policy"} | Select-Object -ExpandProperty 'Releases' | Where-Object {$_.Architecture -eq "any" -and $_.Platform -eq "Any" -and $_.Artifacts -ne $null} | Sort-Object -Property 'ProductVersion' -Descending | Select-Object -First 1 | Select-Object -ExpandProperty 'Artifacts' | Where-Object {$_.ArtifactName -eq 'zip'}).SizeInBytes
+$edge_pol_path = ($edge_json | Where-Object {$_.Product -eq "Policy"} | Select-Object -ExpandProperty 'Releases' | Where-Object {$_.Architecture -eq "any" -and $_.Platform -eq "Any" -and $_.Artifacts -ne $null} | Sort-Object -Property 'ProductVersion' -Descending | Select-Object -First 1 | Select-Object -ExpandProperty 'Artifacts' | Where-Object {$_.ArtifactName -eq 'zip'}).Location
 
 # do we have edge already and, if so, is it the same as current?
 If (Test-Path $edge_msi_file) {
