85925d81e8b2006234b8119e056dcadf071a2022
alexbarthnet <alexbarth@austin.utexas.edu>
2024-01-09 14:35:54 -0600

    re-work to use lists and speed up processing

--- Modules/LdapQuery/LdapQuery.psm1
+++ Modules/LdapQuery/LdapQuery.psm1
@@ -1,61 +1,97 @@
-Function ConvertFrom-LdapEntry {
+Function Format-LdapAttribute {
+	[CmdletBinding(DefaultParameterSetName = 'Default')]
 	Param(
-		[Parameter(Mandatory = $True)]
-		[object]$Entry,
-		[Parameter(Mandatory = $True)]
-		[string]$Attribute
+		[Parameter(Mandatory = $true)]
+		[string]$LdapDisplayName,
+		[Parameter(Mandatory = $true)]
+		[System.DirectoryServices.Protocols.DirectoryAttribute]$DirectoryAttribute,
+		[Parameter(DontShow)]
+		[System.Globalization.CultureInfo]$Culture = (Get-Culture)
 	)
 
-	# create empty array for values in attribute
-	$values = @()
-
-	# process values in attribute
-	For ($index = 0; $index -lt $Entry.Attributes[$Attribute].Count; $index++) {
-		# retrieve value from attribute
-		$value = $entry.Attributes[$Attribute][$index]
-
-		# check for known attributes
-		switch ($Attribute) {
-			'objectSid' {
-				# create SID object with value
-				$values += New-Object -TypeName 'System.Security.Principal.SecurityIdentifier' -ArgumentList @($value, 0)
-			}
-			'userCertificate' {
-				# create certificate object with value after casting to byte array
-				$values += New-Object -TypeName 'System.Security.Cryptography.X509Certificates.X509Certificate2' -ArgumentList @([byte[]]$value, $null)
-			}
-			'userParameters' {
-				# encode value as bytes
-				$values += [System.Text.Encoding]::Default.GetBytes($value)
-			}
-			Default {
-				# convert 16 character byte arrays to GUID
-				If ($value -is [Byte[]] -and $value.Length -eq 16) {
-					$values += [guid]$value
-				}
-				# return all other attribute values as is
-				Else {
-					$values += $value
-				}
-			}
-		}
+	# map ldapdisplayname to attribute type
+	$AttributeMap = @{
+		accountExpires     = 'FileTime'
+		badPasswordTime    = 'FileTime'
+		lastLogon          = 'FileTime'
+		lastLogonTimestamp = 'FileTime'
+		objectSid          = 'SecurityIdentifier'
+		pwdlastset         = 'FileTime'
+		userCertificate    = 'X509Certificate2'
+		userParameters     = 'ByteArray'
+		whenChanged        = 'GeneralizedTime'
+		whenCreated        = 'GeneralizedTime'
 	}
 
-	# check count of values
-	switch ($values.Count) {
-		# return null for no values
+	# create a generic list to contain values
+	$List = [System.Collections.Generic.List[object]]::new()
+
+	# process each value in directory attribute by index
+	For ($Index = 0; $Index -lt $DirectoryAttribute.Count; $Index++) {
+		# retrieve value from directory attribute by index
+		$Value = $DirectoryAttribute[$Index]
+
+		# retrieve attribute type from attribute map
+		$AttributeType = $AttributeMap[$LdapDisplayName]
+
+		# if attribute type not found...
+		If ([string]::IsNullOrEmpty($AttributeType)) {
+			# if value is 16 character byte array...
+			If ($Value -is [Byte[]] -and $Value.Length -eq 16) {
+				# ...convert to GUID
+				$Value = [guid]$Value
+			}
+		}
+		# if attribute type found in map...
+		Else {
+			# transform attribute based upon type
+			switch ($AttributeType) {
+				'SecurityIdentifier' {
+					# create SID from value
+					$Value = [System.Security.Principal.SecurityIdentifier]::new($Value, 0)
+				}
+				'X509Certificate2' {
+					# create certificate object from value
+					$Value = [System.Security.Cryptography.X509Certificates.X509Certificate2]::new($Value)
+				}
+				'ByteArray' {
+					# encode value as bytes
+					$Value = [System.Text.Encoding]::Default.GetBytes($Value)
+				}
+				'FileTime' {
+					# if value is less than or equal to maximum datetime value...
+					If ($Value -le [System.DateTime]::MaxValue.ToFileTimeUtc()) {
+						# create datetime from filetime
+						$Value = [System.DateTime]::FromFileTimeUtc($Value)
+					}
+				}
+				'GeneralizedTime' {
+					# create datetime from generalized time
+					$Value = [System.Datetime]::ParseExact($Value, 'yyyyMMddHHmmss.fZ', $Culture)
+				}
+			}
+		}
+
+		# add value to list
+		$List.Add($Value)
+	}
+
+	# return value based upon count
+	switch ($List.Count) {
+		# when no values found...
 		0 {
+			# ...return null
 			Return $null
 		}
-
-		# return first value for single valued arrays
+		# when one value found...
 		1 {
-			Return $values[0]
+			# ...return first element of list
+			Return $List[0]
 		}
-
-		# return values in array
+		# when multiple values found...
 		Default {
-			Return $values
+			# ...return list
+			Return $List
 		}
 	}
 }
@@ -81,138 +117,131 @@ Function Invoke-LdapQuery {
 		[int]$PageSize = [int]1000,
 		[Parameter(Position = 8)]
 		[boolean]$SSL = $true,
+		[Parameter(Position = 9, Mandatory = $True, ParameterSetName = 'Cert', ValueFromPipeline = $true)]
+		[System.Security.Cryptography.X509Certificates.X509Certificate2]$Certificate,
 		[Parameter(Position = 9, Mandatory = $True, ParameterSetName = 'Cred', ValueFromPipeline = $true)]
-		[pscredential]$Cred,
+		[pscredential]$Credential,
 		[Parameter(Position = 9, Mandatory = $True, ParameterSetName = 'Kerb')]
-		[switch]$Kerberos,
-		[Parameter(Position = 9, Mandatory = $True, ParameterSetName = 'Pass')]
-		[string]$Username,
-		[Parameter(Position = 10, Mandatory = $True, ParameterSetName = 'Pass')]
-		[securestring]$Password
+		[switch]$Kerberos
 	)
 
-	# verify .net assemblies
-	$assembly = [string]::Empty
-	$assembly = [System.AppDomain]::CurrentDomain.GetAssemblies().Location -match 'System\.DirectoryServices\.Protocols'
-	If ([string]::IsNullOrEmpty($assembly)) { $null = [System.Reflection.Assembly]::LoadWithPartialName('System.DirectoryServices.Protocols') }
+	Begin {
+		# define required assemblies
+		$Assemblies = @('System.DirectoryServices.Protocols')
 
-	# define LDAP server
-	$identifier = New-Object 'System.DirectoryServices.Protocols.LdapDirectoryIdentifier' -ArgumentList $Server, $Port
+		# retrieve loaded assemblies
+		$AssembliesFound = [System.AppDomain]::CurrentDomain.GetAssemblies().GetName().Name
 
-	# create LDAP connection
-	$ldap = New-Object 'System.DirectoryServices.Protocols.LdapConnection' -ArgumentList $identifier
-	$ldap.SessionOptions.ReferralChasing = 'None'
-	$ldap.SessionOptions.ProtocolVersion = 3
-	$ldap.SessionOptions.SecureSocketLayer = $SSL
+		# verify assemblies
+		ForEach ($Assembly in $Assemblies) { If ($Assembly -notin $AssembliesFound) { $null = [System.Reflection.Assembly]::LoadWithPartialName($Assembly) } }
 
-	# define credentials
-	switch ($PSCmdlet.ParameterSetName) {
-		'Cred' {
-			$ldap.AuthType = 'Basic'
-			$ldap.Credential = $Cred
+		# define LDAP server
+		$LdapDirectoryIdentifier = [System.DirectoryServices.Protocols.LdapDirectoryIdentifier]::new($Server, $Port)
+
+		# create LDAP connection
+		$LdapConnection = [System.DirectoryServices.Protocols.LdapConnection]::new($LdapDirectoryIdentifier)
+		$LdapConnection.SessionOptions.ReferralChasing = 'None'
+		$LdapConnection.SessionOptions.ProtocolVersion = 3
+
+		# define credentials
+		switch ($PSCmdlet.ParameterSetName) {
+			'Cert' {
+				$LdapConnection.AuthType = 'Anonymous'
+				$LdapConnection.SessionOptions.QueryClientCertificate = { $Cert }
+				$LdapConnection.SessionOptions.SecureSocketLayer = $SSL
+			}
+			'Cred' {
+				$LdapConnection.AuthType = 'Basic'
+				$LdapConnection.Credential = $Cred
+				$LdapConnection.SessionOptions.SecureSocketLayer = $SSL
+			}
+			'Kerb' {
+				$LdapConnection.AuthType = 'Kerberos'
+			}
+			Default {
+				$LdapConnection.AuthType = 'Negotiate'
+			}
 		}
-		'Pass' {
-			$ldap.AuthType = 'Basic'
-			$ldap.Credential = New-Object 'System.Management.Automation.PSCredential' -ArgumentList $Username, $Password 
-		}
-		'Kerb' {
-			$ldap.AuthType = 'Kerberos'
-		}
-		Default {
-			$ldap.AuthType = 'Negotiate'
-		}
+
+		# define paging configuration
+		$PageResultRequestControl = [System.DirectoryServices.Protocols.PageResultRequestControl]::new($PageSize)
+		$SearchOptionsControl = [System.DirectoryServices.Protocols.SearchOptionsControl]::new('DomainScope')
+
+		# define LDAP search request
+		$SearchRequest = [System.DirectoryServices.Protocols.SearchRequest]::new($BaseDN, $Filter, $Scope, $Attributes)
+		$SearchRequest.SizeLimit = $SizeLimit
+
+		# add controls to LDAP search requested
+		$null = $SearchRequest.Controls.Add($PageResultRequestControl)
+		$null = $SearchRequest.Controls.Add($SearchOptionsControl)
 	}
 
-	# bind to LDAP server
-	Try {
-		$ldap.Bind()
-		If ($VerbosePreference -ne 'SilentlyContinue') {
-			# translate to unenumerated protcols
-			switch ($ldap.SessionOptions.SslInformation.Protocol) {
-				2048 { $ldap_protocol = 'TLS 1.2' }
-				Default { $ldap_protocol = $ldap.SessionOptions.SslInformation.CipherStrength }
+	Process {
+		# if authentication is not with a certificate...
+		If ($PSCmdlet.ParameterSetName -ne 'Cert') {
+			# bind to LDAP server
+			Try {
+				$LdapConnection.Bind()
+			}
+			Catch {
+				Write-Host 'ERROR: failure during bind'
+				Return $_
+			}
+		}
+
+		# execute paged query
+		Do {
+			# clear search response
+			$SearchResponse = $null
+
+			# submit LDAP query
+			Try {
+				$SearchResponse = [System.DirectoryServices.Protocols.SearchResponse]($LdapConnection.SendRequest($SearchRequest))
+			}
+			Catch [System.DirectoryServices.Protocols.DirectoryOperationException] {
+				Write-Host 'WARNING: caught DirectoryOperationException'
+			}
+			Catch {
+				Write-Host "ERROR: caught unknown error: '$($Error[0].Exception)'"
 			}
 
-			# report connection
-			Write-Verbose 'Connected to server...'
-			Write-Verbose " - server    : $($ldap.Directory.Servers):$($ldap.Directory.Port)"
-			Write-Verbose " - protocol  : $ldap_protocol"
-			Write-Verbose " - algorithm : $ldap.SessionOptions.SslInformation.AlgorithmIdentifier"
-			Write-Verbose " - hash      : $ldap.SessionOptions.SslInformation.Hash"
+			# process each entry in the search response
+			ForEach ($Entry in $SearchResponse.Entries) {
+				# create sorted list for attributes
+				$SortedList = [System.Collections.Generic.SortedList[string, object]]::new()
+
+				# retrieve unsorted attributes from entry
+				ForEach ($Attribute in $Entry.Attributes.GetEnumerator()) {
+					# format attribute add to sorted list
+					$SortedList[$Attribute.Key] = Format-LdapAttribute -LdapDisplayName $Attribute.Key -DirectoryAttribute $Attribute.Value
+				}
+
+				# return processed entry
+				$SortedList
+			}
+
+			# retrieve current state of page response control
+			$PageResultResponseControl = $SearchResponse.Controls | Where-Object { $_ -is [System.DirectoryServices.Protocols.PageResultResponseControl] }
+
+			# copy token (aka "the cookie") from page response control into token for page request control to set page position for next search request
+			$PageResultRequestControl.Cookie = $PageResultResponseControl.Cookie
 		}
+		While (
+			# process paged queries while pages remain
+			$PageResultRequestControl.Cookie.Length -gt 0
+		)
 	}
-	Catch {
-		Write-Host 'ERROR: failure during bind'
-		Return $_
+
+	End {
+		# close LDAP connection
+		$LdapConnection.Dispose()
 	}
-
-	# define paging configuration
-	$pageRequestControl = New-Object 'System.DirectoryServices.Protocols.PageResultRequestControl' -ArgumentList $PageSize
-	$searchScopeControl = New-Object 'System.DirectoryServices.Protocols.SearchOptionsControl' -ArgumentList 'DomainScope'
-
-	# define LDAP search request
-	$request = New-Object 'System.DirectoryServices.Protocols.SearchRequest' -ArgumentList $BaseDn, $Filter, $Scope, $Attributes
-	$request.SizeLimit = $SizeLimit
-
-	# add controls to LDAP search requested
-	$null = $request.Controls.Add($pageRequestControl)
-	$null = $request.Controls.Add($searchScopeControl)
-
-	# execute paged query
-	Do {
-		# clear search response
-		$response = $null
-
-		# submit LDAP query
-		Try {
-			$response = [System.DirectoryServices.Protocols.SearchResponse]($ldap.SendRequest($request))
-		}
-		Catch [System.DirectoryServices.Protocols.DirectoryOperationException] {
-			Write-Host 'WARNING: caught DirectoryOperationException'
-		}
-		Catch {
-			Write-Host "ERROR: caught unknown error: '$($Error[0].Exception)'"
-		}
-		
-		# process each entry in response
-		ForEach ($entry in $response.Entries) {
-			# create empty array for unsorted attributes
-			$attributes_unsorted = @()
-
-			# retrieve unsorted attributes from entry
-			ForEach ($attribute in $entry.Attributes.Keys) { $attributes_unsorted += $attribute }
-
-			# sort attributes
-			$attributes_sorted = [array][Linq.Enumerable]::OrderBy($attributes_unsorted, [Func[object, string]] { $args[0] })
-
-			# create empty hashtable for processed attributes
-			$processed = [ordered]@{}
-
-			# process each attribute in sorted order
-			ForEach ($attribute in $attributes_sorted) { $processed[$attribute] = ConvertFrom-LdapEntry -Entry $entry -Attribute $attribute }
-
-			# return processed entry
-			$processed
-		}
-
-		# retrieve current state of page response control
-		$pageResponseControl = $response.Controls | Where-Object { $_ -is [System.DirectoryServices.Protocols.PageResultResponseControl] }
-
-		# copy token (aka "the cookie") from page response control into token for page request control to set page position for next search request
-		$pageRequestControl.Cookie = $pageResponseControl.Cookie
-	}
-	While (
-		# process paged queries while pages remain
-		$pageRequestControl.Cookie.Length -gt 0
-	)
-
-	# close LDAP connection
-	$ldap.Dispose()
 }
 
 # define functions to export
-$functions_to_export = @()
-$functions_to_export += 'Invoke-LdapQuery'
+$FunctionsToExport = @(
+	'Invoke-LdapQuery'
+)
 
 # export module members
-Export-ModuleMember -Function $functions_to_export
+Export-ModuleMember -Function $FunctionsToExport
