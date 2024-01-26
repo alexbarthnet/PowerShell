7a9341b94ef0d1aec51efd09fd2615a096ed5676
alexbarthnet <alexbarth@austin.utexas.edu>
2024-01-25 21:24:34 -0600

    add transcript functions

--- PKI/Copy-CertificatesToWeb.ps1
+++ PKI/Copy-CertificatesToWeb.ps1
@@ -1,32 +1,203 @@
+[CmdletBinding(DefaultParameterSetName = 'Default')]
 Param(
 	[Parameter(DontShow)]
 	[string]$DomainFqdn = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain().Name,
+	# local host name
 	[Parameter(DontShow)]
-	[string]$HostName = ([System.Environment]::MachineName.ToLowerInvariant())
+	[string]$HostName = [System.Net.NetworkInformation.IPGlobalProperties]::GetIPGlobalProperties().HostName.ToLowerInvariant(),
+	# local domain name
+	[Parameter(DontShow)]
+	[string]$DomainName = [System.Net.NetworkInformation.IPGlobalProperties]::GetIPGlobalProperties().DomainName.ToLowerInvariant(),
+	# local DNS hostname
+	[Parameter(DontShow)]
+	[string]$DnsHostName = ($HostName, $DomainName -join '.').TrimEnd('.')
 )
 
-# define transcript file from script path and start transcript
-Start-Transcript -Path $PSCommandPath.Replace((Get-Item -Path $PSCommandPath).Extension, "_$HostName.txt") -Force
+Begin {
+	Function Start-TranscriptWithHostAndDate {
+		Param(
+			# name for transcript file
+			[Parameter()]
+			[string]$TranscriptName,
+			# path for transcript file
+			[Parameter()]
+			[string]$TranscriptPath,
+			# log start time
+			[Parameter(DontShow)]
+			[string]$TranscriptTime = ([datetime]::Now.ToString('yyyyMMddHHmmss')),
+			# local hostname
+			[Parameter(DontShow)]
+			[string]$TranscriptHost = ([System.Environment]::MachineName)
+		)
 
-# define paths
-$dir_array = @()
-$dir_array += $pki_certs = 'C:\Content\pki\certsrv'
-$dir_array += $pki_pages = 'C:\Content\pki\wwwroot'
-$dir_array += $iis_pages = 'C:\inetpub\wwwroot'
-$dir_array += $iis_certs = 'C:\inetpub\wwwroot\pki'
- 
-# verify paths exist
-$dir_array | ForEach-Object { If ((Test-Path $_) -eq $false) { New-Item -Type Directory -Path $_ } }
- 
-# copy DFS files to IIS
-If (Test-Path -Path $pki_certs) { Get-ChildItem -Path $pki_certs | Copy-Item -Destination $iis_certs -Force -Verbose }
-If (Test-Path -Path $pki_pages) { Get-ChildItem -Path $pki_pages | Copy-Item -Destination $iis_pages -Force -Verbose }
- 
-# define path for root certificates
-$ad_path = ('\\' + $DomainFqdn + '\sysvol\' + $DomainFqdn + '\certificates\root')
- 
-# copy AD files to IIS
-If (Test-Path -Path $ad_path) { Get-ChildItem -Path $ad_path | Copy-Item -Destination $iis_certs -Force -Verbose }
+		# define default transcript name as basename of running script
+		If (!$PSBoundParameters.ContainsKey('TranscriptName')) {
+			$TranscriptName = (Get-Item -Path $PSCommandPath).BaseName
+		}
 
-# stop transcript
-Stop-Transcript
+		# define default transcript path as named folder under transcripts folder in common application data folder
+		If (!$PSBoundParameters.ContainsKey('TranscriptPath')) {
+			$TranscriptPath = [System.Environment]::GetFolderPath('CommonApplicationData'), 'PowerShell_transcript', $TranscriptName -join '\'
+		}
+
+		# verify transcript path
+		If (!(Test-Path -Path $TranscriptPath -PathType 'Container')) {
+			# define parameters for New-Item
+			$NewItem = @{
+				Path        = $TranscriptPath
+				ItemType    = 'Directory'
+				ErrorAction = [System.Management.Automation.ActionPreference]::Stop
+			}
+
+			# create transcript path
+			Try {
+				$null = New-Item @NewItem
+			}
+			Catch {
+				Throw $_
+			}
+		}
+
+		# build transcript file name with defined prefix, hostname, transcript name and current datetime
+		$TranscriptFile = "PowerShell_transcript.$TranscriptHost.$TranscriptName.$TranscriptTime.txt"
+
+		# define parameters for Start-Transcript
+		$StartTranscript = @{
+			Path        = Join-Path -Path $TranscriptPath -ChildPath $TranscriptFile
+			Force       = $true
+			ErrorAction = [System.Management.Automation.ActionPreference]::Stop
+		}
+
+		# start transcript
+		Try	{
+			$null = Start-Transcript @StartTranscript
+		}
+		Catch {
+			Throw $_
+		}
+	}
+
+	Function Stop-TranscriptWithHostAndDate {
+		Param(
+			# name for transcript file
+			[Parameter()]
+			[string]$TranscriptName,
+			# path of transcript files
+			[Parameter()]
+			[string]$TranscriptPath,
+			# minimum number of transcript files for removal
+			[Parameter(DontShow)]
+			[uint16]$TranscriptCount = 7,
+			# minimum age of transcript files for removal
+			[Parameter(DontShow)]
+			[double]$TranscriptDays = 7,
+			# datetime for transcript files for removal
+			[Parameter(DontShow)]
+			[datetime]$TranscriptDate = ([datetime]::Now.AddDays(-$TranscriptDays)),
+			# local hostname
+			[Parameter(DontShow)]
+			[string]$TranscriptHost = ([System.Environment]::MachineName)
+		)
+
+		# define default transcript name as basename of running script
+		If (!$PSBoundParameters.ContainsKey('TranscriptName')) {
+			$TranscriptName = (Get-Item -Path $PSCommandPath).BaseName
+		}
+
+		# define default transcript path as named folder under transcripts folder in common application data folder
+		If (!$PSBoundParameters.ContainsKey('TranscriptPath')) {
+			$TranscriptPath = [System.Environment]::GetFolderPath('CommonApplicationData'), 'PowerShell_transcript', $TranscriptName -join '\'
+			# LEGACY: re-define default transcript path as string array containing current path and original path in common application data folder
+			[string[]]$TranscriptPath = @([System.Environment]::GetFolderPath('CommonApplicationData'), $TranscriptPath)
+		}
+
+		# define filter using default transcript prefix, hostname, and script name
+		$TranscriptFilter = "PowerShell_transcript.$TranscriptHost.$TranscriptName*"
+
+		# get transcript files matching filter
+		$TranscriptFiles = Get-ChildItem -Path $TranscriptPath -Filter $TranscriptFilter -ErrorAction 'SilentlyContinue'
+
+		# split transcript files on transcript date
+		$NewFiles, $OldFiles = $TranscriptFiles.Where({ $_.LastWriteTime -ge $TranscriptDate }, [System.Management.Automation.WhereOperatorSelectionMode]::Split)
+		
+		# if count of files after transcript date is less than to cleanup threshold...
+		If ($NewFiles.Count -lt $TranscriptCount) {
+			# declare skip
+			Write-Verbose -Message "Skipping transcript file cleanup; count of transcripts ($($NewFiles.Count)) would be below minimum transcript count ($TranscriptCount)" -Verbose
+		}
+		Else {
+			# declare cleanup
+			Write-Verbose -Message "Removing any transcript files matching '$TranscriptFilter' that are older than '$TranscriptDays' days from: $TranscriptPath" -Verbose
+			# remove old logs
+			ForEach ($OldFile in ($OldFiles | Sort-Object -Property FullName)) {
+				Try {
+					Remove-Item -Path $OldFile.FullName -Force -Verbose -ErrorAction Stop
+				}
+				Catch {
+					$_
+				}
+			}
+		}
+
+		# stop transcript
+		Try {
+			$null = Stop-Transcript
+		}
+		Catch {
+			Throw $_
+		}
+	}
+
+	# if running...
+	If ($PSCmdlet.ParameterSetName -eq 'Default') {
+		# define hashtable for transcript functions
+		$TranscriptWithHostAndDate = @{}
+		# define parameters for transcript functions
+		If ($PSBoundParameters.ContainsKey('TranscriptName')) { $TranscriptWithHostAndDate['TranscriptName'] = $PSBoundParameters['TranscriptName'] }
+		If ($PSBoundParameters.ContainsKey('TranscriptPath')) { $TranscriptWithHostAndDate['TranscriptPath'] = $PSBoundParameters['TranscriptPath'] }
+		# start transcript with parameters
+		Try {
+			Start-TranscriptWithHostAndDate @TranscriptWithHostAndDate
+		}
+		Catch {
+			Throw $_
+		}
+	}
+}
+
+Process {
+
+	# define paths
+	$dir_array = @()
+	$dir_array += $pki_certs = 'C:\Content\pki\certsrv'
+	$dir_array += $pki_pages = 'C:\Content\pki\wwwroot'
+	$dir_array += $iis_pages = 'C:\inetpub\wwwroot'
+	$dir_array += $iis_certs = 'C:\inetpub\wwwroot\pki'
+ 
+	# verify paths exist
+	$dir_array | ForEach-Object { If ((Test-Path $_) -eq $false) { New-Item -Type Directory -Path $_ } }
+ 
+	# copy DFS files to IIS
+	If (Test-Path -Path $pki_certs) { Get-ChildItem -Path $pki_certs | Copy-Item -Destination $iis_certs -Force -Verbose }
+	If (Test-Path -Path $pki_pages) { Get-ChildItem -Path $pki_pages | Copy-Item -Destination $iis_pages -Force -Verbose }
+ 
+	# define path for root certificates
+	$ad_path = ('\\' + $DomainFqdn + '\sysvol\' + $DomainFqdn + '\certificates\root')
+ 
+	# copy AD files to IIS
+	If (Test-Path -Path $ad_path) { Get-ChildItem -Path $ad_path | Copy-Item -Destination $iis_certs -Force -Verbose }
+
+}
+
+End {
+	# if running...
+	If ($PSCmdlet.ParameterSetName -eq 'Default') {
+		# stop transcript with parameters
+		Try {
+			Stop-TranscriptWithHostAndDate @TranscriptWithHostAndDate
+		}
+		Catch {
+			Throw $_
+		}
+	}
+}
