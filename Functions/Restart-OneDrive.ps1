12b5eba6359a2795b8b38dd48e257b810ba6735f
alexbarthnet <alexbarth@austin.utexas.edu>
2024-02-18 17:59:59 -0600

    added process checks

--- Functions/Restart-OneDrive.ps1
+++ Functions/Restart-OneDrive.ps1
@@ -1,12 +1,19 @@
 Function Restart-OneDrive {
 	Param(
 		[string]$OneDrive = (Join-Path -Path ([System.Environment]::GetFolderPath('LocalApplicationData')) -ChildPath 'Microsoft\OneDrive\OneDrive.exe'),
-		[string]$RunAsExe = (Join-Path -Path ([System.Environment]::GetFolderPath('System')) -ChildPath 'runas.exe')
+		[string]$RunAsExe = (Join-Path -Path ([System.Environment]::GetFolderPath('System')) -ChildPath 'runas.exe'),
+		[int32]$SessionId = ([System.Diagnostics.Process]::GetCurrentProcess().SI)
 	)
 
-	# shutdown OneDrive and wait for the shtudown to complete
-	Start-Process -WindowStyle Hidden -FilePath $OneDrive -ArgumentList '/shutdown' -Wait
+	# check for OneDrive process before shutdown
+	$OneDriveProcess = Get-Process | Where-Object { $_.Name -eq 'OneDrive' -and $_.SessionId -eq $SessionId }
+
+	# shutdown OneDrive and wait for the shutdown to complete
+	If ($OneDriveProcess) { Start-Process -WindowStyle Hidden -FilePath $RunAsExe -ArgumentList "/trustlevel:0x20000 `"$OneDrive /shutdown`"" }
+
+	# wait for OneDrive process to exit
+	Do { $OneDriveProcess = Get-Process | Where-Object { $_.Name -eq 'OneDrive' -and $_.SessionId -eq $SessionId } } Until (!$OneDriveProcess)
 
 	# start OneDrive in the background via RunAs with the basic user trust level
-	Start-Process -WindowStyle Hidden -FilePath $RunAsExe -ArgumentList "/trustlevel:0x20000 `"$OneDrive /background`""
+	If (!$OneDriveProcess) { Start-Process -WindowStyle Hidden -FilePath $RunAsExe -ArgumentList "/trustlevel:0x20000 `"$OneDrive /background`"" }
 }
