eea92e41f9902e0441e3ee574e804f489952cade
alexbarthnet <alexbarth@austin.utexas.edu>
2024-04-16 23:48:16 -0500

    swap logging to transcript, massive cleanup

--- DNS/Clear-DnsRecordsByAge.ps1
+++ DNS/Clear-DnsRecordsByAge.ps1
@@ -1,139 +1,183 @@
-#Requires -Modules LogToMultiple
+#Requires -Modules TranscriptWithHostAndDate
 
 [CmdletBinding(SupportsShouldProcess)]
 Param (
-	[Parameter(Position = 0)][ValidateRange(1, 65535)]
+	[Parameter(Position = 0)]
+	[string]$ZoneName = '*',
+	[Parameter(Position = 1)][ValidateRange(1, 65535)]
 	[uint16]$OlderThanUnits = 30,
-	[Parameter(Position = 1)][ValidateSet('Seconds', 'Minutes', 'Hours', 'Days', 'Weeks', 'Months', 'Years')]
+	[Parameter(Position = 2)][ValidateSet('Seconds', 'Minutes', 'Hours', 'Days', 'Weeks', 'Months', 'Years')]
 	[string]$OlderThanType = 'Days',
-	[Parameter(Position = 3)]
-	[string]$Domain = '*',
-	[Parameter(Position = 4)]
-	[string]$Server = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain().PdcRoleOwner.Name,
-	[Parameter(Position = 5)]
-	[string]$HostCheckUri,
+	# PDC emulator of domain
 	[Parameter(DontShow)]
-	[string]$Hostname = [System.Environment]::MachineName.ToLowerInvariant()
+	[string]$PdcRoleOwner = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain().PdcRoleOwner.Name,
+	# check if current host is permitted to run scripts
+	[Parameter(DontShow)]
+	[switch]$HostCheck,
+	# full path to host check files
+	[Parameter(DontShow)]
+	[string]$HostCheckPath = 'C:\Content\adfs\host',
+	# switch to skip transcript logging
+	[Parameter(DontShow)]
+	[switch]$SkipTranscript,
+	# local host name
+	[Parameter(DontShow)]
+	[string]$HostName = [System.Net.NetworkInformation.IPGlobalProperties]::GetIPGlobalProperties().HostName.ToLowerInvariant(),
+	# local domain name
+	[Parameter(DontShow)]
+	[string]$DomainName = [System.Net.NetworkInformation.IPGlobalProperties]::GetIPGlobalProperties().DomainName.ToLowerInvariant(),
+	# local DNS hostname
+	[Parameter(DontShow)]
+	[string]$DnsHostName = ($HostName, $DomainName -join '.').TrimEnd('.')
 )
 
-Function Get-PreviousDate {
-	Param (
-		[Parameter(Mandatory = $true, Position = 0)][ValidateRange(1, 65535)]
-		[uint16]$OlderThanUnits,
-		[Parameter(Mandatory = $true, Position = 1)][ValidateSet('Seconds', 'Minutes', 'Hours', 'Days', 'Weeks', 'Months', 'Years')]
-		[string]$OlderThanType
-	)
-	Switch ($OlderThanType) {
-		'Seconds' { Return (Get-Date).AddSeconds(-1 * $OlderThanUnits) }
-		'Minutes' { Return (Get-Date).AddMinutes(-1 * $OlderThanUnits) }
-		'Hours' { Return (Get-Date).AddHours(-1 * $OlderThanUnits) }
-		'Days' { Return (Get-Date).AddDays(-1 * $OlderThanUnits) }
-		'Weeks' { Return (Get-Date).AddWeeks(-1 * $OlderThanUnits) }
-		'Months' { Return (Get-Date).AddMonths(-1 * $OlderThanUnits) }
-		'Years' { Return (Get-Date).AddYears(-1 * $OlderThanUnits) }
+Begin {
+	Function Get-PreviousDate {
+		Param (
+			[Parameter(Mandatory = $true, Position = 0)][ValidateRange(1, 65535)]
+			[uint16]$OlderThanUnits,
+			[Parameter(Mandatory = $true, Position = 1)][ValidateSet('Seconds', 'Minutes', 'Hours', 'Days', 'Weeks', 'Months', 'Years')]
+			[string]$OlderThanType
+		)
+		Switch ($OlderThanType) {
+			'Seconds' { Return (Get-Date).AddSeconds(-1 * $OlderThanUnits) }
+			'Minutes' { Return (Get-Date).AddMinutes(-1 * $OlderThanUnits) }
+			'Hours' { Return (Get-Date).AddHours(-1 * $OlderThanUnits) }
+			'Days' { Return (Get-Date).AddDays(-1 * $OlderThanUnits) }
+			'Weeks' { Return (Get-Date).AddWeeks(-1 * $OlderThanUnits) }
+			'Months' { Return (Get-Date).AddMonths(-1 * $OlderThanUnits) }
+			'Years' { Return (Get-Date).AddYears(-1 * $OlderThanUnits) }
+		}
 	}
-}
-
-# start log file
-Try {
-	Start-LogToMultiple -ScriptPath $PSCommandPath
-}
-Catch {
-	Write-Host 'ERROR: could not start logging'
-	Return $_
-}
-
-# check active host
-If ($HostCheckUri) {
-	# define url to check and declare start
-	Write-LogToMultiple "host-check-uri: $HostCheckUri"
-	# retrieving value from host check URI
-	Try {
-		$host_check_txt = (Invoke-WebRequest -Uri $HostCheckUri -UseBasicParsing -ErrorAction 'SilentlyContinue').Content
-	}
-	Catch {
-		Write-LogToMultiple 'host-check-failed-connection'
-		Return
-	}
-	# check value against hostname
-	If ($host_check_txt -match $Hostname) {
-		Write-LogToMultiple 'host-check-passed-hostname'
-	}
-	Else {
-		Write-LogToMultiple 'host-check-failed-hostname'
-		Return
-	}
-}
-
-# creat empty objects
-$dns_zones = @()
-$dns_removed_count = 0
-$dns_removed_error = 0
-$dns_records_total = 0
-
-# get date from inputs
-$dns_time = Get-PreviousDate -OlderThanUnits $OlderThanUnits -OlderThanType $OlderThanType
-Write-LogToMultiple -LogText "Checking for records older than $OlderThanUnits $OlderThanType ($($dns_time.ToString()))"
-
-# get DNS zones
-Try {
-	$dns_zones += Get-DnsServerZone -ComputerName $Server | Where-Object { $_.ZoneName -like $Domain -and $_.ZoneName -notlike '_msdcs.*' -and $_.ZoneType -eq 'Primary' -and $_.DynamicUpdate -eq 'Secure' -and $_.IsDsIntegrated -and -not $_.IsReverseLookupZone }
-}
-Catch {
-	Write-LogToMultiple 'ERROR: could not retrieve DNS zones'
-	Return $_
-}
-
-# process each DNS zone
-ForEach ($dns_zone in $dns_zones) {
-	Write-LogToMultiple -LogText "Checking for records in '$($dns_zone.ZoneName)'"
 	
-	# clear objects
-	$dns_records = @()
+	# if skip transcript not requested...
+	If ($SkipTranscript -ne $true) {
+		# start transcript with parameters
+		Try {
+			Start-TranscriptWithHostAndDate
+		}
+		Catch {
+			Throw $_
+		}
+	}
+}
 
-	# get DNS records
+Process {
+	# check active host
+	If ($HostCheck) {
+		# define url to check and declare start
+		Write-Output "host-check-path: '$HostCheckPath'"
+
+		# retrieving value from host check URI
+		Try {
+			$HostCheckText = Get-ChildItem -Path $HostCheckPath | Sort-Object -Property 'LastWriteTimeUtc' | Select-Object -Last 1 | Get-Content
+		}
+		Catch {
+			Write-Output 'host-check-failed-connection'
+			Return
+		}
+
+		# check value against hostname
+		If ($HostCheckText -match $HostName) {
+			Write-Output 'host-check-passed-hostname'
+		}
+		Else {
+			Write-Output 'host-check-failed-hostname'
+			Return
+		}
+	}
+
+	# creat empty objects
+	$DnsZonesOnPdcRole = 0
+	$DnsRecordsRemoved = 0
+	$DnsRecordsErrored = 0
+	$DnsRecordsInTotal = 0
+
+	# get date from inputs
+	$PreviousDate = Get-PreviousDate -OlderThanUnits $OlderThanUnits -OlderThanType $OlderThanType
+	Write-TranscriptWithHostAndDate "Checking for records older than $OlderThanUnits $OlderThanType ($($PreviousDate.ToString()))"
+
+	# get DNS zones
 	Try {
-		$dns_records = Get-DnsServerResourceRecord -ComputerName $Server -ZoneName $dns_zone.ZoneName -RRType A | Where-Object { $_.TimeStamp -gt 0 -and $_.Timestamp -lt $dns_time }
-		$dns_records_total += $dns_records.Count
+		$DnsServerZones = Get-DnsServerZone -ComputerName $PdcRoleOwner | Where-Object { $_.ZoneName -like $ZoneName -and $_.ZoneName -notlike '_msdcs.*' -and $_.ZoneType -eq 'Primary' -and $_.DynamicUpdate -eq 'Secure' -and $_.IsDsIntegrated }
 	}
 	Catch {
-		Write-LogToMultiple 'ERROR: could not retrieve DNS records'
-		Return $_	
+		Write-WarningToTranscriptWithHostAndDate "could not retrieve DNS zones: $($_.Exeception.Message)"
+		Return $_
 	}
 
-	# process DNS records
-	ForEach ($dns_record in $dns_records) {
-		switch ($dns_record.RecordType) {
-			'A' { $dns_record_data = $dns_record.RecordData.IPv4Address.IPAddressToString }
-			'AAAA' { $dns_record_data = $dns_record.RecordData.IPv6Address.IPAddressToString }
-			'SRV' { $dns_record_data = "[$($dns_record.RecordData.Priority)][$($dns_record.RecordData.Weight)][$($dns_record.RecordData.Port)][$($dns_record.RecordData.DomainName)]" }
+	# process each DNS zone
+	ForEach ($DnsServerZone in $DnsServerZones) {
+		# increment total DNS records
+		$DnsZonesOnPdcRole++
+
+		# declare zone
+		Write-TranscriptWithHostAndDate "Checking for records in '$($DnsServerZone.ZoneName)'"
+	
+		# get DNS records
+		Try {
+			$DnsServerResourceRecords = Get-DnsServerResourceRecord -ComputerName $PdcRoleOwner -ZoneName $DnsServerZone.ZoneName | Where-Object { $_.TimeStamp -gt 0 -and $_.Timestamp -lt $PreviousDate } | Sort-Object -Property RecordType,HostName
 		}
-		if ($PSCmdlet.ShouldProcess("$($dns_record.HostName).$($dns_zone.ZoneName)", 'Remove-DnsServerResourceRecord')) {
-			Try {
-				# $dns_record | Remove-DnsServerResourceRecord -ComputerName $Server -ZoneName $dns_zone.ZoneName -Confirm:$false
-				$dns_removed_count++
-				Write-LogToMultiple -LogSubject $dns_zone.ZoneName -LogText "Removed record created on $($dns_record.Timestamp): $($dns_record.RecordType); $($dns_record.HostName).$($dns_zone.ZoneName); $dns_record_data"
-			}
-			Catch {
-				$dns_removed_error++
-				Write-LogToMultiple -LogSubject $dns_zone.ZoneName -LogText "ERROR: could not remove record created on $($dns_record.Timestamp): $($dns_record.RecordType); $($dns_record.HostName).$($dns_zone.ZoneName); $dns_record_data" -LogLevel 'Error'
-			}
+		Catch {
+			Write-WarningToTranscriptWithHostAndDate "could not retrieve DNS records: $($_.Exeception.Message)"
+			Return $_
 		}
+
+		# process DNS records
+		ForEach ($DnsServerResourceRecord in $DnsServerResourceRecords) {
+			# increment total DNS records
+			$DnsRecordsInTotal++
+
+			# get DNS record data
+			switch ($DnsServerResourceRecord.RecordType) {
+				'A' { 
+					$RecordData = $DnsServerResourceRecord.RecordData.IPv4Address.IPAddressToString
+				}
+				'AAAA' {
+					$RecordData = $DnsServerResourceRecord.RecordData.IPv6Address.IPAddressToString
+				}
+				'PTR' {
+					$RecordData = $DnsServerResourceRecord.RecordData.PtrDomainName
+				}
+				'SRV' {
+					$RecordData = "[$($DnsServerResourceRecord.RecordData.Priority)][$($DnsServerResourceRecord.RecordData.Weight)][$($DnsServerResourceRecord.RecordData.Port)][$($DnsServerResourceRecord.RecordData.DomainName)]"
+				}
+			}
+
+			# if remove attempt should be attempting
+			If ($PSCmdlet.ShouldProcess("$($DnsServerResourceRecord.HostName).$($DnsServerResourceRecord.ZoneName)", 'Remove-DnsServerResourceRecord')) {
+				Try {
+					# Remove-DnsServerResourceRecord -ComputerName $PdcRoleOwner -ZoneName $DnsServerZone.ZoneName -Name $DnsServerResourceRecord.HostName -Confirm:$false
+					# $DnsRecordsRemoved++
+				}
+				Catch {
+					$DnsRecordsErrored++
+					Write-WarningToTranscriptWithHostAndDate "could not remove record: $($_.Exeception.Message)"
+				}
+			}
+
+			# declare removed
+			Write-TranscriptWithHostAndDate "Removed $($DnsServerResourceRecord.RecordType) record from $($DnsServerZone.ZoneName) created on $($DnsServerResourceRecord.TimeStamp): $($DnsServerResourceRecord.HostName); $RecordData"
+		}
+	}
+
+	# report DNS records removed
+	Write-TranscriptWithHostAndDate "Found '$DnsRecordsInTotal' records(s) in '$DnsZonesOnPdcRole' zones"
+	Write-TranscriptWithHostAndDate "Removed '$DnsRecordsRemoved' records(s)"
+	If ($DnsRecordsErrored -gt 0) {
+		Write-TranscriptWithHostAndDate "Could not remove '$DnsRecordsErrored' records(s)"
 	}
 }
 
-# report DNS records removed
-Write-LogToMultiple -LogText "Found '$dns_records_total' records(s) in '$($dns_zones.Count)' zones"
-Write-LogToMultiple -LogText "Removed '$dns_removed_count' records(s)"
-If ($dns_removed_error -gt 0) {
-	Write-LogToMultiple -LogText "Could not remove '$dns_removed_error' records(s)"
-}
-
-# remove log files
-Try {
-	Remove-LogToMultiple -ScriptPath $PSCommandPath
-}
-Catch {
-	Write-Host 'ERROR: could not cleanup logs'
-	Return $_
+End {
+	# if skip transcript not requested...
+	If ($SkipTranscript -ne $true) {
+		# stop transcript with parameters
+		Try {
+			Stop-TranscriptWithHostAndDate
+		}
+		Catch {
+			Throw $_
+		}
+	}
 }
