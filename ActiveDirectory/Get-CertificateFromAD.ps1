aab1528e6762fbbb9ef69f10b6b3ced1c90af5eb
alexbarthnet <alexbarth@austin.utexas.edu>
2024-04-20 23:16:15 -0500

    fixed root module path and updated description

--- ActiveDirectory/Get-CertificateFromAD.ps1
+++ ActiveDirectory/Get-CertificateFromAD.ps1
@@ -1,83 +1,95 @@
-Param(  
-	[string]$Attribute,
-	[string]$ObjectDN,
-	[string]$User,
-	[string]$Computer
+<#
+.SYNOPSIS
+Convert an attribute on an Active Directory object into an X.509 certificate object.
+
+.DESCRIPTION
+Convert an attribute on an Active Directory object into an X.509 certificate object. The input can be a byte array, a collection where the first element is a byte array, or the string representation of a byte array.
+
+.PARAMETER Identity
+Specifies the Active Directory object to retrieve
+
+.PARAMETER Attribute
+Specifies the attribute on the Active Directory object that represents an X.509 certificate
+
+.INPUTS
+System.String. A string representing the fully qualified distinguished name of an LDAP object.
+
+.OUTPUTS
+X509Certificate2. An object representing an X509 certificate.
+
+#>
+[CmdletBinding()]
+Param (
+	[Parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $true)]
+	[object]$Identity,
+	[Parameter(Position = 1)]
+	[string]$Attribute = 'userCertificate'
 )
 
-# retrieve DN of user object
-If ($User) {
-	# determine if user value is UPN or SAM
-	If ($User -match "@") {
-		$ad_filter = "userPrincipalName -eq '$User'"
+# if Identity is an ADobject...
+If ($Identity -is [Microsoft.ActiveDirectory.Management.ADObject]) {
+	# ...and if Attribute is an ADPropertyValueCollection that contains at least one value...
+	If ($Identity.$Attribute -is [Microsoft.ActiveDirectory.Management.ADPropertyValueCollection] -and $Identity.$Attribute.Count -ge 1) {
+		# ...retrieve the values from the attribute on the object
+		$Values = $Identity.$Attribute
 	}
+	# ...and if Attribute is not ADPropertyValueCollection or does not contain at least one value...
 	Else {
-		$ad_filter = "sAMAccountName -eq '$User'"
+		# ...retrieve the DN from the object
+		$Identity = $Identity.DistinguishedName
 	}
+}
 
-	# retrieve user object from AD
-	$ad_user = $null
-	$ad_user = Get-ADUser -Filter $ad_filter
-
-	# retrieve DN for user object if it exists
-	If ($ad_user) {
-		$ObjectDN = $ad_user.distinguishedName
+# if Identity is not ADObject or the Attribute on the ADObject is null...
+If ($Identity -isnot [Microsoft.ActiveDirectory.Management.ADObject]) {
+	Try {
+		# retrieve object via S.DS
+		$Entry = [System.DirectoryServices.DirectoryEntry]::New("LDAP://$Identity")
+		# retrieve values from attribute on the the object
+		$Values = $Entry.$Attribute
 	}
+	Catch {
+		Throw $_
+	}
+}
+
+# create empty array for certificates
+$Certificates = @()
+
+# process each value in attribute
+ForEach ($Value in $Values) {
+	# convert value to x509 certificate
+	Try {
+		# do *NOT* pipe the value to the function; the pipeline will unroll byte arrays and break the function
+		$Certificate = ConvertTo-X509Certificate -InputObject $Value
+	}
+	Catch {
+		Throw $_
+	}
+	# if chain requested...
+	If ($Chain) {
+		# ...retrieve chain...
+		Try {
+			$CertificateChain = Get-CertificateChain -Certificate $Certificate
+		}
+		Catch {
+			Throw $_
+		}
+		# ...add certificates in chain to array
+		ForEach ($Certificate in $CertificateChain) {
+			$Certificates += $Certificate
+		}
+	}
+	# if chain not requested...
 	Else {
-		Write-Host 'ERROR: unable to locate user with provided inputs'
-		Exit
-	}
-} 
-
-# retrieve DN of computer object
-If ($Computer) {
-	# retrieve computer object from AD
-	$ad_computer = $null
-	$ad_computer = Get-ADComputer -Filter "SamAccountName -eq '$User$'"
-
-	# retrieve DN for computer object if it exists
-	If ($ad_computer) {
-		$ObjectDN = $ad_computer.distinguishedName
-	}
-	Else {
-		Write-Host 'ERROR: unable to locate computer with provided inputs'
-		Return
+		# ...add certificate to array
+		$Certificates += $Certificate
 	}
 }
 
-# set attribute for user or computer if not attribute provided
-If ($User -or $Computer -and ($null -eq $Attribute)) {
-	$Attribute = 'userCertificate'
-}
-
-# check for required inputs
-If ($ObjectDN -and $Attribute) {
-	# declare inputs
-	Write-Host "Retrieving certificate from AD object: '$objectDN'"
-	Write-Host "Retrieving certificate from attribute: '$Attribute'"
-}
-Else {
-	# declare error
-	Write-Host 'ERROR: minimum required inputs not provided'
-	Exit
-}
-
-# retrieve object
-Try {
-	$ad_object = Get-ADObject -Identity $objectDN -Properties $Attribute 
-}
-Catch {
-	Throw $_
-}
-
-# retrieve attribute from object
-If ($ad_object -and ($null -ne $ad_object.$Attribute)) {
-	$ad_attr = $ad_object.$Attribute
-	
-	If ($ad_attr) {
-		New-Object System.Security.Cryptography.X509Certificates.X509Certificate2 $ad_attr
-	}
-}
-Else {
-	Write-Host 'ERROR: unable to locate object with provided inputs'
+# return results
+switch ($Certificates.Count) {
+	0 { Return $null }
+	1 { Return $Certificate }
+	Default { Return $Certificates }
 }
