b95dd27e2b60ce5aef32d96a8a5c5033c8c5bc79
alexbarthnet <alexbarth@austin.utexas.edu>
2024-07-03 12:59:04 -0500

    added Begin block, moved functions into Begin block

--- Downloads/Get-WindowsAdminCenter.ps1
+++ Downloads/Get-WindowsAdminCenter.ps1
@@ -9,245 +9,267 @@ Param (
 	[Parameter(Position = 3)]
 	[switch]$Install,
 	[Parameter(DontShow)]
-	[string]$Uri = 'https://aka.ms/WACDownload',
-	[Parameter(DontShow)]
-	[string]$FileName = 'MicrosoftEdgeEnterpriseX64.msi'
+	[string]$Uri = 'https://aka.ms/WACDownload'
 )
 
-Function Expand-Uri {
-	[CmdletBinding()]
-	Param (
-		[string]$Uri
-	)
+Begin {
+	Function Expand-Uri {
+		[CmdletBinding()]
+		Param (
+			[string]$Uri
+		)
 
-	# define parameters for Invoke-WebRequest
-	$InvokeWebRequest = @{
-		Uri                = $Uri
-		Method             = 'Head'
-		UseBasicParsing    = $true
-		MaximumRedirection = 0
-		ErrorAction        = [System.Management.Automation.ActionPreference]::SilentlyContinue
-	}
+		# define parameters for Invoke-WebRequest
+		$InvokeWebRequest = @{
+			Uri                = $Uri
+			Method             = 'Head'
+			UseBasicParsing    = $true
+			MaximumRedirection = 0
+			ErrorAction        = [System.Management.Automation.ActionPreference]::SilentlyContinue
+		}
 
-	# get web request object
-	$WebRequest = Invoke-WebRequest @InvokeWebRequest
-
-	# check object
-	If ($WebRequest -isnot [Microsoft.PowerShell.Commands.WebResponseObject]) {
-		Throw $_
-	}
-
-	# if status is redirected and location found...
-	If ($WebRequest.StatusCode -in '301', '302' -and -not [string]::IsNullOrEmpty($WebRequest.Headers.Location)) {
-		# ...expand location
-		Expand-Uri -Uri $WebRequest.Headers.Location
-	}
-	Else {
-		Return $Uri
-	}
-}
-
-Function Get-HeadersFromUri {
-	[CmdletBinding()]
-	Param (
-		[string]$Uri
-	)
-
-	# define parameters for Invoke-WebRequest
-	$InvokeWebRequest = @{
-		Uri                = $Uri
-		Method             = 'Head'
-		UseBasicParsing    = $true
-		MaximumRedirection = 0
-		ErrorAction        = [System.Management.Automation.ActionPreference]::Stop
-	}
-
-	# retrieve response from URI
-	Try {
+		# get web request object
 		$WebRequest = Invoke-WebRequest @InvokeWebRequest
-	}
-	Catch {
-		Throw $_
-	}
 
-	# return headers
-	Return $WebRequest.Headers
-}
+		# check object
+		If ($WebRequest -isnot [Microsoft.PowerShell.Commands.WebResponseObject]) {
+			Throw $_
+		}
 
-Function Get-FileByteHash {
-	Param (
-		[Parameter(Mandatory = $true)]
-		[string]$Path,
-		[ValidateSet('MD5', 'SHA1', 'SHA256')]
-		[string]$Algorithm = 'SHA256'
-	)
-
-	# get file content as bytes
-	Try {
-		$Bytes = Get-Content -Path $Path -Raw -Encoding Byte
-	}
-	Catch {
-		Throw $_
-	}
-
-	# create hash object
-	Try {
-		switch ($Algorithm) {
-			'MD5' {
-				$HashAlgorithm = [System.Security.Cryptography.MD5]::Create()
-			}
-			'SHA1' {
-				$HashAlgorithm = [System.Security.Cryptography.SHA1]::Create()
-			}
-			'SHA256' {
-				$HashAlgorithm = [System.Security.Cryptography.SHA256]::Create()
-			}
+		# if status is redirected and location found...
+		If ($WebRequest.StatusCode -in '301', '302' -and -not [string]::IsNullOrEmpty($WebRequest.Headers.Location)) {
+			# ...expand location
+			Expand-Uri -Uri $WebRequest.Headers.Location
+		}
+		Else {
+			Return $Uri
 		}
 	}
-	Catch {
-		Throw $_
-	}
 
-	# get hash of bytes
-	Try {
-		$Hash = $HashAlgorithm.ComputeHash($Bytes)
-	}
-	Catch {
-		Throw $_
-	}
+	Function Get-HeadersFromUri {
+		[CmdletBinding()]
+		Param (
+			[string]$Uri
+		)
 
-	# convert hash to base64
-	Try {
-		$String = [System.Convert]::ToBase64String($Hash)
-	}
-	Catch {
-		Throw $_
-	}
+		# define parameters for Invoke-WebRequest
+		$InvokeWebRequest = @{
+			Uri                = $Uri
+			Method             = 'Head'
+			UseBasicParsing    = $true
+			MaximumRedirection = 0
+			ErrorAction        = [System.Management.Automation.ActionPreference]::Stop
+		}
 
-	# return string
-	Return $String
-}
-
-# expand uri
-Try {
-	$UriForBits = Expand-Uri -Uri $Uri
-}
-Catch {
-	Throw $_
-}
-
-# get file from uri
-Try {
-	$Headers = Get-HeadersFromUri -Uri $UriForBits
-}
-Catch {
-	Throw $_
-}
-
-# retrieve file name from headers
-$ChildPath = Split-Path -Path $UriForBits -Leaf
-
-# create local path
-$FilePath = Join-Path -Path $Path -ChildPath $ChildPath
-
-# if file exists...
-If ((Test-Path -Path $FilePath -PathType Leaf) -and -not $Force) {
-	# if remote hash is available...
-	If (-not [string]::IsNullOrEmpty($Headers['Content-MD5'])) {
-		# get local hash
+		# retrieve response from URI
 		Try {
-			$FileByteHash = Get-FileByteHash -Path $FilePath -Algorithm 'MD5'
+			$WebRequest = Invoke-WebRequest @InvokeWebRequest
 		}
 		Catch {
 			Throw $_
 		}
-		# if local hash matches remote hash...
-		If ($FileByteHash -eq $Headers['Content-MD5']) {
-			Write-Host 'Skipping file download: existing file hash matches value in Content-MD5 header'
-			$SkipDownload = $true
-		}
+
+		# return headers
+		Return $WebRequest.Headers
 	}
-	# if remote length is available...
-	ElseIf (-not [string]::IsNullOrEmpty($Headers['Content-Length'])) {
-		# get local length
+
+	Function Get-FileByteHash {
+		Param (
+			[Parameter(Mandatory = $true)]
+			[string]$Path,
+			[ValidateSet('MD5', 'SHA1', 'SHA256')]
+			[string]$Algorithm = 'SHA256'
+		)
+
+		# get file content as bytes
 		Try {
-			$SizeBytes = Get-Item -Path $FilePath | Select-Object -ExpandProperty SizeBytes
+			$Bytes = Get-Content -Path $Path -Raw -Encoding Byte
 		}
 		Catch {
 			Throw $_
 		}
-		# if local length matches remote length...
-		If ($SizeBytes -eq $Headers['Content-Length']) {
-			Write-Host 'Skipping file download: existing file size matches value in Content-Length header'
-			$SkipDownload = $true
+
+		# create hash object
+		Try {
+			switch ($Algorithm) {
+				'MD5' {
+					$HashAlgorithm = [System.Security.Cryptography.MD5]::Create()
+				}
+				'SHA1' {
+					$HashAlgorithm = [System.Security.Cryptography.SHA1]::Create()
+				}
+				'SHA256' {
+					$HashAlgorithm = [System.Security.Cryptography.SHA256]::Create()
+				}
+			}
 		}
+		Catch {
+			Throw $_
+		}
+
+		# get hash of bytes
+		Try {
+			$Hash = $HashAlgorithm.ComputeHash($Bytes)
+		}
+		Catch {
+			Throw $_
+		}
+
+		# convert hash to base64
+		Try {
+			$String = [System.Convert]::ToBase64String($Hash)
+		}
+		Catch {
+			Throw $_
+		}
+
+		# return string
+		Return $String
 	}
 }
 
-# if force or skipdownload was not set...
-If ($Force -or -not $SkipDownload) {
-	# ...download the file
+Process {
+	# expand uri
 	Try {
-		Start-BitsTransfer -Source $UriForBits -Destination $FilePath
+		$UriForBits = Expand-Uri -Uri $Uri
 	}
 	Catch {
-		Write-Warning -Message "could not download '$UriForBits' to '$FilePath"
-		Return $_
+		Throw $_
 	}
-}
 
-# if not install...
-If (-not $Install) {
-	Return
-}
+	# get file from uri
+	Try {
+		$Headers = Get-HeadersFromUri -Uri $UriForBits
+	}
+	Catch {
+		Throw $_
+	}
 
-# get local certificate
-If ($PSBoundParameters['Thumbprint']) {
-	If (-not (Test-Path -Path "Cert:\LocalMachine\My\$Thumbprint" -PathType Leaf)) {
-		Write-Host 'ERROR: provided thumbprint not found in local machine certificate store'
+	# retrieve file name from headers
+	$ChildPath = Split-Path -Path $UriForBits -Leaf
+
+	# create local path
+	$FilePath = Join-Path -Path $Path -ChildPath $ChildPath
+
+	# if file exists...
+	If (Test-Path -Path $FilePath -PathType 'Leaf') {
+		# if force set...
+		If ($Force) {
+			Write-Warning -Message 'Overwriting existing file: Force parameter was set and the '$FileName' file was found in '$Path' path'
+		}
+		# if remote hash is available...
+		ElseIf ($Headers.ContainsKeys('Content-MD5')) {
+			# get local hash
+			Try {
+				$FileByteHash = Get-FileByteHash -Path $FilePath -Algorithm 'MD5'
+			}
+			Catch {
+				Throw $_
+			}
+			# if local hash matches remote hash...
+			If ($FileByteHash -eq $Headers['Content-MD5']) {
+				Write-Host 'Skipping file download: existing file hash matches value in Content-MD5 header'
+				$SkipDownload = $true
+			}
+		}
+		# if remote length is available...
+		ElseIf ($Headers.ContainsKeys('Content-Length')) {
+			# get local length
+			Try {
+				$SizeBytes = Get-Item -Path $FilePath | Select-Object -ExpandProperty 'SizeBytes'
+			}
+			Catch {
+				Throw $_
+			}
+			# if local length matches remote length...
+			If ($SizeBytes -eq $Headers['Content-Length']) {
+				Write-Host 'Skipping file download: existing file size matches value in Content-Length header'
+				$SkipDownload = $true
+			}
+		}
+	}
+	ElseIf ($SkipDownload) {
+		Write-Warning -Message "Exiting with no changes: SkipDownload set to true and the '$FileName' file was not found in '$Path' path"
 		Return
 	}
-}
-Else {
-	$Certificate = Get-ChildItem -Path 'Cert:\LocalMachine\My' | Where-Object { $_.Subject.StartsWith("CN=$env:computername.", [System.StringComparison]::InvariantCultureIgnoreCase) } | Sort-Object NotBefore | Select-Object -Last 1
-	If ($null -ne $Certificate) {
-		$Thumbprint = $Certificate.Thumbprint
+
+	# if force or skipdownload was not set...
+	If ($Force -or -not $SkipDownload) {
+		# ...download the file
+		Try {
+			Start-BitsTransfer -Source $UriForBits -Destination $FilePath
+		}
+		Catch {
+			Write-Warning -Message "could not download '$UriForBits' to '$FilePath"
+			Return $_
+		}
 	}
-}
 
-# get log path
-$LogPath = $FilePath.Replace((Get-Item -Path $FilePath).Extension, '.txt')
+	# if not install...
+	If (-not $Install) {
+		Return
+	}
 
-# define arguments for Start-Process
-$ArgumentList = "/i $FilePath /qn /L*v $LogPath SME_PORT=443"
+	# get local certificate
+	If ($PSBoundParameters['Thumbprint']) {
+		If (Test-Path -Path "Cert:\LocalMachine\My\$Thumbprint" -PathType 'Leaf') {
+			Try {
+				$Certificate = Get-Item -Path "Cert:\LocalMachine\My\$Thumbprint"
+			}
+			Catch {
+				Write-Warning "could not retrieve certificate in local machine store with thumbprint: $Thumbprint"
+				Return $_
+			}
+		}
+		Else {
+			Write-Warning "could not locate certificate in local machine store with thumbprint: $Thumbprint"
+			Return
+		}
+	}
+	Else {
+		Try {
+			$Certificate = Get-ChildItem -Path 'Cert:\LocalMachine\My' | Where-Object { $_.Subject.StartsWith("CN=$env:computername.", [System.StringComparison]::InvariantCultureIgnoreCase) } | Sort-Object NotBefore | Select-Object -Last 1
+		}
+		Catch {
+			Write-Warning "could not retrieve certificates from local machine store"
+			Return $_
+		}
+	}
 
-# if thumbprint found...
-If ($null -eq $Thumbprint) {
-	$ArgumentList = "$ArgumentList CHK_REDIRECT_PORT_80=1 SSL_CERTIFICATE_OPTION=generate"
-}
-Else {
-	$ArgumentList = "$ArgumentList CHK_REDIRECT_PORT_80=1 SSL_CERTIFICATE_OPTION=installed SME_THUMBPRINT=$Thumbprint"
-}
+	# get log path
+	$LogPath = $FilePath.Replace((Get-Item -Path $FilePath).Extension, '.txt')
 
-# define parameters for Start-Process
-$StartProcess = @{
-	Wait         = $true
-	FilePath     = 'msiexec.exe'
-	ArgumentList = $ArgumentList
-}
+	# define arguments for Start-Process
+	$ArgumentList = "/i $FilePath /qn /L*v $LogPath SME_PORT=443 CHK_REDIRECT_PORT_80=1"
 
-# install / update WAC
-Try {
-	Start-Process @StartProcess
-}
-Catch {
-	Throw $_
-}
+	# if thumbprint found...
+	If ($null -eq $Certificate) {
+		$ArgumentList = "$ArgumentList SSL_CERTIFICATE_OPTION=generate"
+	}
+	Else {
+		$ArgumentList = "$ArgumentList SSL_CERTIFICATE_OPTION=installed SME_THUMBPRINT=$($Certificate.Thumbprint)"
+	}
 
-# start service
-Try {
-	Start-Service -Name 'ServerManagementGateway'
-}
-Catch {
-	Throw $_
+	# define parameters for Start-Process
+	$StartProcess = @{
+		Wait         = $true
+		FilePath     = 'msiexec.exe'
+		ArgumentList = $ArgumentList
+	}
+
+	# install / update WAC
+	Try {
+		Start-Process @StartProcess
+	}
+	Catch {
+		Throw $_
+	}
+
+	# start service
+	Try {
+		Start-Service -Name 'ServerManagementGateway'
+	}
+	Catch {
+		Throw $_
+	}
 }
