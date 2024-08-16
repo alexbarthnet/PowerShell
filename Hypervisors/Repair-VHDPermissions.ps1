7adf66dea647678d54b2cd896288e9756679eb8a
alexbarthnet <alexbarth@austin.utexas.edu>
2024-08-16 16:38:01 -0500

    major re-write

--- Hypervisors/Repair-VHDPermissions.ps1
+++ Hypervisors/Repair-VHDPermissions.ps1
@@ -1,60 +1,100 @@
-[CmdletBinding()]
+#requires -Modules 'Hyper-V'
+
+[CmdletBinding(DefaultParameterSetName = 'VM')]
 param (
-	[Parameter(Mandatory = $true, ValueFromPipeline = $true)]
-	[object[]]$VM,
+	[Parameter(ParameterSetName = 'VM', Mandatory = $true, ValueFromPipeline = $true)]
+	[object]$VM,
+	[Parameter(ParameterSetName = 'VMName', Mandatory = $true)]
+	[string]$VMName,
+	[Parameter(ParameterSetName = 'VMName')]
+	[string]$ComputerName,
 	[Parameter(DontShow)]
 	[string]$Hostname = [System.Environment]::MachineName.ToLowerInvariant()
 )
 
-Function Add-VmToAcl {
-	[CmdletBinding()]
-	param (
-		[Parameter(Mandatory = $true)]
-		[object]$VM
-	)
+Begin {
+	Function Update-VMHardDriveAcl {
+		[CmdletBinding()]
+		param (
+			[Parameter(Mandatory = $true)]
+			[object]$VM
+		)
 
-	# load local instance of VM
-	$local_vm = $VM | Get-VM
+		# retrieve VM to retrieve all properties
+		$VM = Get-VM -Id $VM.Id
 
-	# build ACE objects
-	$vhd_user = New-Object System.Security.Principal.NTAccount("NT VIRTUAL MACHINE\$($local_vm.VMId)")
-	$vhd_rule = New-Object System.Security.AccessControl.FileSystemAccessRule ($vhd_user, @('Read', 'Write', 'Synchronize'), 'None', 'None', 'Allow')
-	
-	# add ACE to each VHD on VM
-	Write-Output "Updating permissions on VHDs for VM: '$($local_vm.Name)'"
-	ForEach ($VHD in $local_vm.HardDrives.Path) {
-		Write-Output "...reviewing permissions on VHD: '$VHD'"
-		Try {
-			$vhd_acl = Get-Acl -Path $VHD
-			$vhd_acl.AddAccessRule($vhd_rule)
-			$vhd_acl | Set-Acl -Path $VHD
-			Write-Output "...updated permissions on VHD: '$VHD'"
-		}
-		Catch {
-			Write-Output "ERROR: could not update permissions on VHD: '$VHD'"
+		# retrieve VM properties
+		$VMName = $VM.Name
+		$VMId = $VM.VMid
+
+		# create VM principal
+		$VMPrincipal = [System.Security.Principal.NTAccount]::new("NT VIRTUAL MACHINE\$VMId")
+
+		# create access rule
+		$AccessRule = [System.Security.AccessControl.FileSystemAccessRule]::new($VMPrincipal, @('Read', 'Write', 'Synchronize'), 'None', 'None', 'Allow')
+
+		# add ACE to each VHD on VM
+		Write-Output "Updating permissions on VHDs for VM: $VMName"
+
+		# add ACE to each VHD on VM
+		:NextVHD ForEach ($VHD in $VM.HardDrives.Path) {
+			# retrieve ACL
+			Try {
+				$ACL = Get-Acl -Path $VHD
+				Write-Output "...retrieved permissions for VHD: $VHD"
+			}
+			Catch {
+				Write-Warning -Message "could not retrieve permissions for VHD: $VHD"
+			}
+
+			# add access rule to ACL
+			Try {
+				$ACL.AddAccessRule($AccessRule)
+			}
+			Catch {
+				Write-Warning -Message "could not add access rult to ACL for VHD: $VHD"
+			}
+
+			# update ACL
+			Try {
+				$ACL | Set-Acl -Path $VHD
+				Write-Output "...updated permissions for VHD: $VHD"
+			}
+			Catch {
+				Write-Warning -Message "could not update permissions for VHD: $VHD"
+			}
 		}
 	}
 }
 
-# process each input
-ForEach ($virtual_machine in $VM) {
-	# try to convert string into VM
-	If ($virtual_machine -is [string]) {
+Process {
+	# if VM names provided...
+	If ($PSCmdlet.ParameterSetName -eq 'VMName') {
+		# define required parameters for Get-VM
+		$GetVM = @{
+			Name = $VMName
+		}
+
+		# define optional parameters for Get-VM
+		If ($PSBoundParameters.ContainsKey('ComputerName')) {
+			$GetVM['ComputerName'] = $ComputerName
+		}
+
+		# retrieve VM
 		Try {
-			$virtual_machine = Get-VM -Name $virtual_machine
+			$VM = Get-VM @GetVM
 		}
 		Catch {
-			Write-Host "ERROR: could not retrieve VM with input '$virtual_machine'"
-			Continue
+			Write-Warning -Message "could not retrieve VM with name: $Name"
+			Return $_
 		}
 	}
+
 	# process VM
-	If ($virtual_machine -is [Microsoft.HyperV.PowerShell.VirtualMachine]) {
-		If ($virtual_machine.ComputerName -ne $Hostname) {
-			Invoke-Command -ComputerName $virtual_machine.ComputerName -ScriptBlock ${function:Add-VmToAcl} -ArgumentList $virtual_machine
-		}
-		Else {
-			Add-VmToAcl $virtual_machine
-		}
+	If ($VM.ComputerName -ne $Hostname) {
+		Invoke-Command -ComputerName $VM.ComputerName -ScriptBlock ${function:Update-VMHardDriveAcl} -ArgumentList $VM
+	}
+	Else {
+		Update-VMHardDriveAcl $VM
 	}
 }
