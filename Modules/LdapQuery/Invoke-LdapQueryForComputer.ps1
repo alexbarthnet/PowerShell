f160881c983680595bf8e04e990ce9b6387f4140
alexbarthnet <github@alexbarth.net>
2024-08-29 12:08:33 -0500

    added parameter for overriding default identity

--- Modules/LdapQuery/Invoke-LdapQueryForComputer.ps1
+++ Modules/LdapQuery/Invoke-LdapQueryForComputer.ps1
@@ -3,28 +3,30 @@
 [CmdletBinding(DefaultParameterSetName = 'Default')]
 Param(
 	[Parameter(Position = 0)]
-	[string]$Server = [System.DirectoryServices.ActiveDirectory.Domain]::GetComputerDomain().PdcRoleOwner.Name,
+	[string]$ComputerName = [System.Environment]::MachineName,
 	[Parameter(Position = 1)]
-	[string]$SearchBase = [System.DirectoryServices.ActiveDirectory.Domain]::GetComputerDomain().GetDirectoryEntry().DistinguishedName,
+	[string]$Server = [System.DirectoryServices.ActiveDirectory.Domain]::GetComputerDomain().PdcRoleOwner.Name,
 	[Parameter(Position = 2)]
-	[string]$Filter = "(&(objectCategory=computer)(objectClass=computer)(sAMAccountName=$([System.Environment]::MachineName)$))",
+	[string]$SearchBase = [System.DirectoryServices.ActiveDirectory.Domain]::GetComputerDomain().GetDirectoryEntry().DistinguishedName,
 	[Parameter(Position = 3)]
+	[string]$Filter = "(&(objectCategory=computer)(objectClass=computer)(sAMAccountName=$ComputerName$))",
+	[Parameter(Position = 4)]
 	[string[]]$Attributes = '*',
-	[Parameter(Position = 4)][ValidateSet('Base', 'OneLevel', 'Subtree')]
+	[Parameter(Position = 5)][ValidateSet('Base', 'OneLevel', 'Subtree')]
 	[string]$SearchScope = 'Subtree',
-	[Parameter(Position = 5)][ValidateRange(1, 65535)]
+	[Parameter(Position = 6)][ValidateRange(1, 65535)]
 	[int]$Port = 636,
-	[Parameter(Position = 6)][ValidateRange(1, [int]::MaxValue)]
-	[int]$SizeLimit = [int]::MaxValue,
 	[Parameter(Position = 7)][ValidateRange(1, [int]::MaxValue)]
+	[int]$SizeLimit = [int]::MaxValue,
+	[Parameter(Position = 8)][ValidateRange(1, [int]::MaxValue)]
 	[int]$PageSize = [int]1000,
-	[Parameter(Position = 8)]
+	[Parameter(Position = 9)]
 	[boolean]$SSL = $true,
-	[Parameter(Position = 9, Mandatory = $True, ParameterSetName = 'Certificate')]
+	[Parameter(Position = 10, Mandatory = $True, ParameterSetName = 'Certificate')]
 	[System.Security.Cryptography.X509Certificates.X509Certificate2]$Certificate,
-	[Parameter(Position = 9, Mandatory = $True, ParameterSetName = 'Credential')]
+	[Parameter(Position = 10, Mandatory = $True, ParameterSetName = 'Credential')]
 	[pscredential]$Credential,
-	[Parameter(Position = 9, Mandatory = $True, ParameterSetName = 'Kerberos')]
+	[Parameter(Position = 10, Mandatory = $True, ParameterSetName = 'Kerberos')]
 	[switch]$Kerberos
 )
 
--- Modules/LdapQuery/Invoke-LdapQueryForUser.ps1
+++ Modules/LdapQuery/Invoke-LdapQueryForUser.ps1
@@ -3,28 +3,30 @@
 [CmdletBinding(DefaultParameterSetName = 'Default')]
 Param(
 	[Parameter(Position = 0)]
-	[string]$Server = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain().PdcRoleOwner.Name,
+	[string]$UserName = [System.Environment]::UserName,
 	[Parameter(Position = 1)]
-	[string]$SearchBase = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain().GetDirectoryEntry().DistinguishedName,
+	[string]$Server = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain().PdcRoleOwner.Name,
 	[Parameter(Position = 2)]
-	[string]$Filter = "(&(objectCategory=person)(objectClass=user)(sAMAccountName=$([System.Environment]::UserName.ToLower())))",
+	[string]$SearchBase = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain().GetDirectoryEntry().DistinguishedName,
 	[Parameter(Position = 3)]
+	[string]$Filter = "(&(objectCategory=person)(objectClass=user)(sAMAccountName=$UserName))",
+	[Parameter(Position = 4)]
 	[string[]]$Attributes = '*',
-	[Parameter(Position = 4)][ValidateSet('Base', 'OneLevel', 'Subtree')]
+	[Parameter(Position = 5)][ValidateSet('Base', 'OneLevel', 'Subtree')]
 	[string]$SearchScope = 'Subtree',
-	[Parameter(Position = 5)][ValidateRange(1, 65535)]
+	[Parameter(Position = 6)][ValidateRange(1, 65535)]
 	[int]$Port = 636,
-	[Parameter(Position = 6)][ValidateRange(1, [int]::MaxValue)]
-	[int]$SizeLimit = [int]::MaxValue,
 	[Parameter(Position = 7)][ValidateRange(1, [int]::MaxValue)]
+	[int]$SizeLimit = [int]::MaxValue,
+	[Parameter(Position = 8)][ValidateRange(1, [int]::MaxValue)]
 	[int]$PageSize = [int]1000,
-	[Parameter(Position = 8)]
+	[Parameter(Position = 9)]
 	[boolean]$SSL = $true,
-	[Parameter(Position = 9, Mandatory = $True, ParameterSetName = 'Certificate')]
+	[Parameter(Position = 10, Mandatory = $True, ParameterSetName = 'Certificate')]
 	[System.Security.Cryptography.X509Certificates.X509Certificate2]$Certificate,
-	[Parameter(Position = 9, Mandatory = $True, ParameterSetName = 'Credential')]
+	[Parameter(Position = 10, Mandatory = $True, ParameterSetName = 'Credential')]
 	[pscredential]$Credential,
-	[Parameter(Position = 9, Mandatory = $True, ParameterSetName = 'Kerberos')]
+	[Parameter(Position = 10, Mandatory = $True, ParameterSetName = 'Kerberos')]
 	[switch]$Kerberos
 )
 
