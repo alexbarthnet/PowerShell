aeea21abf5aa398c64d737857f9f05e9a2f04179
alexbarthnet <github@alexbarth.net>
2025-03-20 13:16:07 -0500

    major rework

--- GroupPolicy/GPOBackups/Import-GPOBackups.ps1
+++ GroupPolicy/GPOBackups/Import-GPOBackups.ps1
@@ -1,93 +1,103 @@
 [CmdletBinding(SupportsShouldProcess)]
 Param(
-    [Parameter(Mandatory)]
-    [string]$Path,
-    [string]$Filter
+	[Parameter(Mandatory)]
+	[string]$Path
 )
 
 # if Path is not an absolute path...
 If (![System.IO.Path]::IsPathRooted($Path)) {
-    # get unresolved absolute path
-    Try {
-        $Path = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($Path)
-    }
-    Catch {
-        Write-Warning -Message "could not create absolute path from the provided Path parameter: $Path"
-        Return
-    }
+	# get unresolved absolute path
+	Try {
+		$Path = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($Path)
+	}
+	Catch {
+		Write-Warning -Message "could not create absolute path from the provided Path parameter: $Path"
+		Return
+	}
 
-    # report absolute path
-    Write-Warning -Message "converted relative path in provided Path parameter to absolute path: $Path"
+	# report absolute path
+	Write-Warning -Message "converted relative path in provided Path parameter to absolute path: $Path"
 }
 
-# retrieve backup xml files...
+# if path does not exists...
+If (![System.IO.Directory]::Exists($Path)) {
+	# report state
+	Write-Verbose -Message 'Path not found; creating...'
+
+}
+
+# retrieve GPO backups...
 Try {
-    $BackupXmlFiles = Get-ChildItem -Path $Path -Include 'Backup.xml' -Recurse -ErrorAction 'Stop' | Where-Object { $_.Parent.FullName -eq $Path.FullName }
+	$GPOBackups = Get-ChildItem -Path $Path -Directory -ErrorAction 'Stop'
 }
 Catch {
-    Write-Warning -Message "could not retrieve 'Backup.xml' files from path: $Path"
-    Return $_
+	Write-Warning -Message "could not retrieve directories in path: $Path"
+	Return $_
 }
 
-# report count of backup XML files
-Write-Verbose -Message "found backup XML files: $($BackupXmlFiles.Count)"
+# report count of GPO backups
+Write-Verbose -Message "found GPO backup folders: $($GPOBackups.Count)"
 
-# define hashtable for GPO IDs
-$GpoIdHashtable = @{}
+# loop through GPO backups
+:NextGPOBackup ForEach ($GPOBackup in $GPOBackups) {
+	# define backup id from folder name
+	$BackupId = $GPOBackup.Name
 
-# loop through each backup xml file
-:NextBackupXmlFile ForEach ($BackupXmlFile in $BackupXmlFiles) {
-    # define XML document
-    $Xml = [System.Xml.XmlDocument]::new()
+	# if GPO backup is not a GUID...
+	If (![System.Guid]::TryParse($BackupId, [ref][System.Guid]::Empty)) {
+		# report state and continue to next GPO backup
+		Write-Warning -Message "found directory that does not parse as a GUID: $BackupId"
+		Continue NextGPOBackup
+	}
 
-    # load backup XML file into XML document
-    Try {
-        $Xml.Load($BackupXmlFile)
-    }
-    Catch {
-        Write-Warning -Message "could not load XML document from existing file: $($BackupXmlFile.FullName)"
-        Return $_
-    }
+	# define XML file for GPO backup
+	$BackupXml = Join-Path -Path $GPOBackup -ChildPath 'Backup.xml'
 
-    # get GPO GUID from XML document
-    $Guid = $Xml.GroupPolicyBackupScheme.GroupPolicyObject.GroupPolicyCoreSettings.ID.InnerText
+	# if XML file not found...
+	If (![System.IO.File]::Exists($BackupXml)) {
+		# report state and continue to next GPO backup
+		Write-Warning -Message "could not locate expected XML file at path: $BackupXml"
+		Continue NextGPOBackup
+	}
 
-    # get GPO name from XML document
-    $TargetName = $Xml.GroupPolicyBackupScheme.GroupPolicyObject.GroupPolicyCoreSettings.DisplayName.InnerText
+	# define XML document
+	$Xml = [System.Xml.XmlDocument]::new()
 
-    # create hashtable for GPO backup
-    $GpoBackupHashtable = @{
-        LastWriteTime = $BackupXmlFile.LastWriteTime
-        BackupId      = $BackupXmlFile.Directory.Name
-        TargetName    = $Xml.GroupPolicyBackupScheme.GroupPolicyObject.GroupPolicyCoreSettings.DisplayName.InnerText
-    }
+	# load backup XML file into XML document
+	Try {
+		$Xml.Load($BackupXml)
+	}
+	Catch {
+		Write-Warning -Message "could not load XML document from file: $BackupXml"
+		Return $_
+	}
 
-    # if GPO GUID found in GPO IDs hashtable...
-    If ($GpoIdHashtable.ContainsKey($Guid)) {
-        # if LastWriteTime of existing hashtable value is newer than current GPO backup LastWriteTime...
-        If ($GpoIdHashtable[$Guid].LastWriteTime -ge $GpoBackupHashtable.LastWriteTime) {
-            Continue NextBackupXmlFile
-        }
-    }
+	# get GPO GUID from XML document
+	$TargetGuid = $Xml.GroupPolicyBackupScheme.GroupPolicyObject.GroupPolicyCoreSettings.ID.InnerText
 
-    # add GPO GUID to hashtable with value 
-    $GpoIdHashtable[$Guid] = $GpoBackupHashtable
-}
+	# if GPO GUID is empty...
+	If ([System.String]::IsNullOrEmpty($TargetGuid)) {
+		# report state and continue to next GPO backup
+		Write-Warning -Message "could not locate GUID for GPO in XML file at path: $BackupXml"
+		Continue NextGPOBackup
+	}
 
-# report count of backup XML files
-Write-Verbose -Message "found unqiue GPOs in backup XML files: $($GpoIdHashtable.Keys.Count)"
+	# get GPO name from XML document
+	$TargetName = $Xml.GroupPolicyBackupScheme.GroupPolicyObject.GroupPolicyCoreSettings.DisplayName.InnerText
 
-ForEach ($GpoId in $GpoIdHashtable.Keys) {
-    # retrieve values from hashtable
-    $BackupId = $GpoIdHashtable[$GpoId].BackupId
-    $TargetName = $GpoIdHashtable[$GpoId].TargetName
+	# if GPO name is empty...
+	If ([System.String]::IsNullOrEmpty($TargetName)) {
+		# report state and continue to next GPO backup
+		Write-Warning -Message "could not locate name for GPO in XML file at path: $BackupXml"
+		Continue NextGPOBackup
+	}
 
-    # import GPO
-    Try {
-        Import-GPO -BackupId $BackupId -Path $Path -TargetName $TargetName -CreateIfNeeded -WhatIf:$WhatIfPreference
-    }
-    Catch {
-        Write-Warning -Message "could not import GPO with '$BackupId' backup ID to GPO with name: $TargetName"
-        Return $_
-    }
+	# import GPO
+	Try {
+		Import-GPO -BackupId $BackupId -Path $Path -TargetGuid $TargetGuid -TargetName $TargetName -CreateIfNeeded -WhatIf:$WhatIfPreference
+	}
+	Catch {
+		Write-Warning -Message "could not import GPO with '$BackupId' backup ID to GPO with name: $TargetName"
+		Return $_
+	}
 }
