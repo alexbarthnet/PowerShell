bf734de6bdf2b99e58c7dea26e91bce228be6542
alexbarthnet <github@alexbarth.net>
2025-03-28 17:39:49 -0500

    major revision

--- Provisioning/VMTemplate/Write-WindowsISOForVMTemplate.ps1
+++ Provisioning/VMTemplate/Write-WindowsISOForVMTemplate.ps1
@@ -11,14 +11,17 @@ Path to the original Windows ISO image.
 .PARAMETER PathForUpdatedIsoImage
 Path for the updated Windows ISO image.
 
+.PARAMETER PathToAutounattendFile
+Path to autounattend XML file to add to Windows ISO image. The file will be saved as 'Autounattend.xml' at the root of the ISO file system. This file must enter the auditUser pass and call the update and invoke scripts during the auditUser pass.
+
 .PARAMETER PathToUnattendFile
-Path to unattend XML file to add to Windows ISO image. The file will be saved as 'Autounattend.xml' at the root of the ISO file system. This file must call the update and invoke scripts during the auditUser pass.
+Path to unattend XML file to add to Windows ISO image. The file will be saved as 'Unattend.xml' at the root of the ISO file system. This file is called when the autounattend file does not shutdown after the auditUser pass.
 
 .PARAMETER PathToUpdateScript
 Path to required "update" PS1 file to add to Windows WIM image. The file will be saved as 'Update-Windows.ps1' under the Windows directory in the WIM image.
 
 .PARAMETER PathToInvokeScript
-Path to required "invoke" PS1 file to add to Windows WIM image. The file will be saved as 'Invoke-ScriptsFromCDROM.ps1' under the Windows directory in the WIM image.
+Path to required "invoke" PS1 file to add to Windows WIM image. The file will be saved as 'Invoke-ScriptsFromRemovableMedia.ps1' under the Windows directory in the WIM image.
 
 .PARAMETER PathToScriptFolder
 Path to optional folder containing PS1 scripts to add to Windows ISO image.
@@ -29,11 +32,11 @@ Path to folder containing required oscdimg.exe program. Required when the deploy
 .PARAMETER NoNewWindow
 Switch parameter to start the oscdimg program in the current window. Primarily used to debug any issues with creating the updated ISO image.
 
-.PARAMETER SkipRemove
-Switch parameter to skip removing the temporary files created by this script. Primarily used to debug any issues with the contents of the ISO image or the WIM file.
-
 .PARAMETER StagingPath
 Path to folder for staging the ISO file contents and mounting the WIM image. The default staging path is a randomly named folder in the system temp directory.
+
+.PARAMETER EmptyStagingPath
+Switch parameter to remove any existing files and folders in the StagingPath folder.
 
 .PARAMETER ProductKey
 String with product key for the Windows image. The default value is the KMS key for Windows Server 2025 Datacenter.
@@ -76,6 +79,7 @@ Param(
 	[Parameter(Mandatory = $true)]
 	[string]$PathForUpdatedIsoImage,
 	[Parameter(Mandatory = $true)]
+	[string]$PathToAutounattendFile,
 	[string]$PathToUnattendFile,
 	[Parameter(Mandatory = $true)]
 	[string]$PathToUpdateScript,
@@ -83,36 +87,112 @@ Param(
 	[string]$PathToScriptFolder,
 	[string]$PathToBinaryFolder,
 	[switch]$NoNewWindow,
-	[switch]$SkipRemove,
 	[switch]$SkipExclude,
 	[string]$StagingPath,
+	[switch]$EmptyStagingPath,
+	[switch]$ReuseStagingPath,
 	[string]$ProductKey = 'D764K-2NDRG-47T6Q-P8T8W-YP6DF',
 	[uint16]$Index = 4
 )
 
 Begin {
-	# if path to oscdimg folder not provided...
-	If (!$PSBoundParameters.ContainsKey('PathToBinaryFolder')) {
-		# define default folder path
-		$PathToBinaryFolder = Join-Path -Path ([System.Environment]::GetFolderPath('ProgramFilesx86')) -ChildPath '\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\amd64\Oscdimg'
-	}
+	Function New-TemporaryFolder {
+		Param(
+			[switch]$ForMachine
+		)
 
-	# define path to required program
-	$FilePath = Join-Path -Path $PathToBinaryFolder -ChildPath 'oscdimg.exe'
+		# if temporary folder for machine requested...
+		If ($ForMachine) {
+			# retrieve TEMP environment variable for machine
+			$PathForTEMP = [System.Environment]::GetEnvironmentVariable('TEMP', 'Machine')
+		}
+		Else {
+			# retrieve TEMP environment variable for user
+			$PathForTEMP = [System.Environment]::GetEnvironmentVariable('TEMP', 'User')
+		}
 
-	# validate application path
-	Try {
-		$null = Get-Item -Path $FilePath
-	}
-	Catch {
-		$PSCmdlet.ThrowTerminatingError($_)
+		# define path for temporary folder
+		Do {
+			# define temporary folder name
+			$NameForTemporaryFolder = [System.IO.Path]::GetRandomFileName().Replace('.', [System.String]::Empty)
+			# combine TEMP path and temporary folder name
+			$PathForTemporaryFolder = Join-Path -Path $PathForTEMP -ChildPath $NameForTemporaryFolder
+		}
+		Until (![System.IO.Directory]::Exists($PathForTemporaryFolder))
+
+		# create temporary folder
+		Try {
+			$TemporaryFolder = New-Item -Force -ItemType Directory -Path $PathForTemporaryFolder
+		}
+		Catch {
+			$PSCmdlet.ThrowTerminatingError($_)
+		}
+
+		# return temporary folder
+		Return $TemporaryFolder
 	}
 
 	# if staging path defined...
-	If (!$PSBoundParameters.ContainsKey('StagingPath')) {
-		$TemporaryPathRoot = [System.Environment]::GetEnvironmentVariable('TEMP', 'Machine')
-		$TemporaryPathBase = [System.IO.Path]::GetRandomFileName().Replace('.', [System.String]::Empty)
-		$StagingPath = Join-Path -Path $TemporaryPathRoot -ChildPath $TemporaryPathBase
+	If ($PSBoundParameters.ContainsKey('StagingPath')) {
+		# if StagingPath is not an absolute path...
+		If (![System.IO.Path]::IsPathRooted($StagingPath)) {
+			# get unresolved absolute path
+			Try {
+				$StagingPath = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($StagingPath)
+			}
+			Catch {
+				Write-Warning -Message "could not create absolute path from the provided Path parameter: $StagingPath"
+				$PSCmdlet.ThrowTerminatingError($_)
+			}
+
+			# report absolute path
+			Write-Warning -Message "converted relative path in provided StagingPath parameter to absolute path: $StagingPath"
+		}
+
+		# if staging path not found...
+		If (![System.IO.Directory]::Exists($StagingPath)) {
+			Write-Warning -Message "could not locate directory for provided StagingPath: $StagingPath"
+			$PSCmdlet.ThrowTerminatingError($_)
+		}
+
+		# retrieve child items in StagingPath
+		Try {
+			$StagingPathItems = Get-ChildItem -Path $StagingPath -Force -Recurse
+		}
+		Catch {
+			Write-Warning -Message 'could not check StagingPath for existing files and folders'
+			$PSCmdlet.ThrowTerminatingError($_)
+		}
+
+		# if child items found in StagingPath...
+		If ($null -ne $StagingPathItems -and -not $ReuseStagingPath) {
+			# if EmptyStagingPath not requested...
+			If (!$EmptyStagingPath) {
+				# warn and inquire
+				Write-Warning -Message 'found existing files or folders in provided StagingPath. Continue to empty StagingPath.' -WarningAction Inquire
+			}
+
+			# remove child items in StagingPath
+			Try {
+				Get-ChildItem -Path $StagingPath -Force -Recurse | Remove-Item -Force -Recurse
+			}
+			Catch {
+				$PSCmdlet.ThrowTerminatingError($_)
+			}
+		}
+	}
+	# if staging path not defined...
+	Else {
+		# create temporary folder
+		Try {
+			$TemporaryFolder = New-TemporaryFolder
+		}
+		Catch {
+			$PSCmdlet.ThrowTerminatingError($_)
+		}
+
+		# define staging path from temporary folder full name
+		$StagingPath = $TemporaryFolder.FullName
 	}
 
 	# create base temporary path
@@ -146,173 +226,220 @@ Begin {
 		}
 		Catch {
 			Write-Warning -Message "could not create exclusion for temporary path: $StagingPath"
+			$PSCmdlet.ThrowTerminatingError($_)
 		}
 	}
 
+	# if path to oscdimg folder not provided...
+	If (!$PSBoundParameters.ContainsKey('PathToBinaryFolder')) {
+		# define default folder path
+		$PathToBinaryFolder = Join-Path -Path ([System.Environment]::GetFolderPath('ProgramFilesx86')) -ChildPath '\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\amd64\Oscdimg'
+	}
+	
+	# define path to required program
+	$FilePath = Join-Path -Path $PathToBinaryFolder -ChildPath 'oscdimg.exe'
+	
+	# validate application path
+	Try {
+		$null = Get-Item -Path $FilePath
+	}
+	Catch {
+		$PSCmdlet.ThrowTerminatingError($_)
+	}
+
 	# define relative items
-	$WimPathOnISO = "$TemporaryPathForISO\sources\install.wim"
-	$XmlPathOnISO = "$TemporaryPathForISO\Autounattend.xml"
+	$AutounattendXmlOnISO = "$TemporaryPathForISO\Autounattend.xml"
+	$UnattendXmlOnISO = "$TemporaryPathForISO\Unattend.xml"
+	$ImagePathForWIM = "$TemporaryPathForISO\sources\install.wim"
 	$UpdatePs1OnWIM = "$TemporaryPathForWIM\Windows\Update-Windows.ps1"
-	$InvokePs1OnWIM = "$TemporaryPathForWIM\Windows\Invoke-ScriptsFromCDROM.ps1"
+	$InvokePs1OnWIM = "$TemporaryPathForWIM\Windows\Invoke-ScriptsFromRemovableMedia.ps1"
 }
 
 Process {
-	# report state
-	"{0}`t{1}: {2}" -f [System.Datetime]::UtcNow.ToString('o'), 'Mounting ISO image', $PathToOriginalIsoImage
-
-	# mount the original ISO image
-	Try {
-		$DiskImage = Mount-DiskImage -ImagePath $PathToOriginalIsoImage
-	}
-	Catch {
-		Return $_
-	}
-
-	# retrieve volume for disk image
-	Try {
-		$Volume = Get-Volume -DiskImage $DiskImage
-	}
-	Catch {
-		Return $_
-	}
-
-	# retrieve volume properties
-	$DriveLetter = $Volume.DriveLetter
-	$FileSystemLabel = $Volume.FileSystemLabel
-
-	# report state
-	"{0}`t{1}: {2}" -f [System.Datetime]::UtcNow.ToString('o'), 'Copying ISO contents to path', $TemporaryPathForISO
-
-	# copy ISO contents to temporary path
-	Try {
-		Copy-Item -Path ('{0}:\*' -f $DriveLetter) -Destination $TemporaryPathForISO -Recurse -Force
-	}
-	Catch {
-		Return $_
-	}
-
-	# report state
-	"{0}`t{1}" -f [System.Datetime]::UtcNow.ToString('o'), 'Dismounting ISO image...'
-
-	# dismount ISO image
-	Try {
-		$null = $DiskImage | Dismount-DiskImage
-	}
-	Catch {
-		Return $_
-	}
-
-	# clear readonly flag on windows image
-	Try {
-		(Get-Item -Path $WimPathOnISO).IsReadOnly = $false
-	}
-	Catch {
-		Return $_
-	}
-
-	# report state
-	"{0}`t{1}: {2}" -f [System.Datetime]::UtcNow.ToString('o'), 'Mounting WIM image', $WimPathOnISO
-
-	# mount windows image
-	Try {
-		$null = Mount-WindowsImage -Path $TemporaryPathForWIM -ImagePath $WimPathOnISO -Index $Index
-	}
-	Catch {
-		Return $_
-	}
-
-	# report state
-	"{0}`t{1}: {2}" -f [System.Datetime]::UtcNow.ToString('o'), 'Updating WIM with "update" script', $UpdatePs1OnWIM
-
-	# add PS1 script to windows image
-	Try {
-		Copy-Item -Path $PathToUpdateScript -Destination $UpdatePs1OnWIM
-	}
-	Catch {
-		Return $_
-	}
-
-	# if invoke script provided...
-	If ($PSBoundParameters.ContainsKey('PathToInvokeScript')) {
+	# if staging path provided and reuse staging path not set...
+	If ($StagingPath -and -not $ReuseStagingPath) {
 		# report state
-		"{0}`t{1}: {2}" -f [System.Datetime]::UtcNow.ToString('o'), 'Updating WIM with "invoke" script', $InvokePs1OnWIM
+		"{0}`t{1}: {2}" -f [System.Datetime]::UtcNow.ToString('o'), 'Mounting ISO image', $PathToOriginalIsoImage
 
-		# add PS1 script to windows image
+		# mount the original ISO image
 		Try {
-			Copy-Item -Path $PathToInvokeScript -Destination $InvokePs1OnWIM
+			$DiskImage = Mount-DiskImage -ImagePath $PathToOriginalIsoImage
 		}
 		Catch {
 			Return $_
 		}
-	}
 
-	# report state
-	"{0}`t{1}" -f [System.Datetime]::UtcNow.ToString('o'), 'Dismounting WIM image...'
+		# retrieve volume for disk image
+		Try {
+			$Volume = Get-Volume -DiskImage $DiskImage
+		}
+		Catch {
+			Return $_
+		}
 
-	# dismount windows image
-	Try {
-		$null = Dismount-WindowsImage -Path $TemporaryPathForWIM -Save
-	}
-	Catch {
-		Return $_
-	}
+		# retrieve volume properties
+		$DriveLetter = $Volume.DriveLetter
+		$FileSystemLabel = $Volume.FileSystemLabel
 
-	# report state
-	"{0}`t{1}: {2}" -f [System.Datetime]::UtcNow.ToString('o'), 'Updating ISO contents with unattend file', $XmlPathOnISO
+		# report state
+		"{0}`t{1}: {2}" -f [System.Datetime]::UtcNow.ToString('o'), 'Copying ISO contents to path', $TemporaryPathForISO
 
-	# get contents of unattend file
-	Try {
-		$Content = Get-Content -Path $PathToUnattendFile -Raw
-	}
-	Catch {
-		Return $_
-	}
+		# copy ISO contents to temporary path
+		Try {
+			Copy-Item -Path ('{0}:\*' -f $DriveLetter) -Destination $TemporaryPathForISO -Recurse -Force
+		}
+		Catch {
+			Return $_
+		}
 
-	# update content with index and product key
-	$Content = $Content.Replace('%INDEX%', $Index)
-	$Content = $Content.Replace('%PRODUCTKEY%', $ProductKey)
+		# report state
+		"{0}`t{1}" -f [System.Datetime]::UtcNow.ToString('o'), 'Dismounting ISO image...'
 
-	# add unattend file to ISO
-	Try {
-		$Content | Set-Content -Path $XmlPathOnISO
-	}
-	Catch {
-		Return $_
-	}
+		# dismount ISO image
+		Try {
+			$null = $DiskImage | Dismount-DiskImage
+		}
+		Catch {
+			Return $_
+		}
 
-	# if script folder provided...
-	If ($PSBoundParameters.ContainsKey('PathToScriptFolder')) {
-		# define scripts folder on ISO
-		$ScriptFolderForISO = Join-Path -Path $TemporaryPathForISO -ChildPath 'scripts'
+		# clear readonly flag on windows image
+		Try {
+		(Get-Item -Path $ImagePathForWIM).IsReadOnly = $false
+		}
+		Catch {
+			Return $_
+		}
 
-		# if script folder on ISO not found...
-		If (![System.IO.Directory]::Exists($ScriptFolderForISO)) {
+		# report state
+		"{0}`t{1}: {2}" -f [System.Datetime]::UtcNow.ToString('o'), 'Mounting WIM image', $ImagePathForWIM
+
+		# mount windows image
+		Try {
+			$null = Mount-WindowsImage -Path $TemporaryPathForWIM -ImagePath $ImagePathForWIM -Index $Index
+		}
+		Catch {
+			Return $_
+		}
+
+		# report state
+		"{0}`t{1}: {2}" -f [System.Datetime]::UtcNow.ToString('o'), 'Updating WIM with "update" script', $UpdatePs1OnWIM
+
+		# add PS1 script to windows image
+		Try {
+			Copy-Item -Path $PathToUpdateScript -Destination $UpdatePs1OnWIM
+		}
+		Catch {
+			Return $_
+		}
+
+		# if invoke script provided...
+		If ($PSBoundParameters.ContainsKey('PathToInvokeScript')) {
 			# report state
-			"{0}`t{1}: {2}" -f [System.Datetime]::UtcNow.ToString('o'), 'Creating ISO scripts folder', $ScriptFolderForISO
+			"{0}`t{1}: {2}" -f [System.Datetime]::UtcNow.ToString('o'), 'Updating WIM with "invoke" script', $InvokePs1OnWIM
 
-			# create folder
+			# add PS1 script to windows image
 			Try {
-				$null = New-Item -ItemType Directory -Path $ScriptFolderForISO
+				Copy-Item -Path $PathToInvokeScript -Destination $InvokePs1OnWIM
 			}
 			Catch {
 				Return $_
 			}
 		}
 
-		# retrieve files in script folder
-		$Files = Get-ChildItem -Path $PathToScriptFolder -Filter '*.ps1'
+		# report state
+		"{0}`t{1}" -f [System.Datetime]::UtcNow.ToString('o'), 'Dismounting WIM image...'
 
-		# loop through files
-		ForEach ($File in $Files) {
+		# dismount windows image
+		Try {
+			$null = Dismount-WindowsImage -Path $TemporaryPathForWIM -Save
+		}
+		Catch {
+			Return $_
+		}
+
+		# report state
+		"{0}`t{1}: {2}" -f [System.Datetime]::UtcNow.ToString('o'), 'Updating ISO contents with unattend file for sysprep', $AutounattendXmlOnISO
+
+		# get contents of unattend file
+		Try {
+			$Content = Get-Content -Path $PathToAutounattendFile -Raw
+		}
+		Catch {
+			Return $_
+		}
+
+		# update content with index and product key
+		$Content = $Content.Replace('%INDEX%', $Index)
+		$Content = $Content.Replace('%PRODUCTKEY%', $ProductKey)
+
+		# add unattend file to ISO
+		Try {
+			$Content | Set-Content -Path $AutounattendXmlOnISO
+		}
+		Catch {
+			Return $_
+		}
+
+		# if unattend file provided...
+		If ($PSBoundParameters.ContainsKey('PathToUnattendFile')) {
 			# report state
-			"{0}`t{1}: {2}" -f [System.Datetime]::UtcNow.ToString('o'), 'Adding script to ISO scripts folder', $File.Name
+			"{0}`t{1}: {2}" -f [System.Datetime]::UtcNow.ToString('o'), 'Updating ISO contents with unattend file for install', $UnattendXmlOnISO
 
-			# copy item to folder
+			# get contents of unattend file
 			Try {
-				Copy-Item -Path $File -Destination $ScriptFolderForISO
+				$Content = Get-Content -Path $PathToUnattendFile -Raw
 			}
 			Catch {
 				Return $_
+			}
+
+			# update content with index and product key
+			$Content = $Content.Replace('%PRODUCTKEY%', $ProductKey)
+
+			# add unattend file to ISO
+			Try {
+				$Content | Set-Content -Path $UnattendXmlOnISO
+			}
+			Catch {
+				Return $_
+			}
+		}
+
+		# if script folder provided...
+		If ($PSBoundParameters.ContainsKey('PathToScriptFolder')) {
+			# define scripts folder on ISO
+			$ScriptFolderForISO = Join-Path -Path $TemporaryPathForISO -ChildPath 'scripts'
+
+			# if script folder on ISO not found...
+			If (![System.IO.Directory]::Exists($ScriptFolderForISO)) {
+				# report state
+				"{0}`t{1}: {2}" -f [System.Datetime]::UtcNow.ToString('o'), 'Creating ISO scripts folder', $ScriptFolderForISO
+
+				# create folder
+				Try {
+					$null = New-Item -ItemType Directory -Path $ScriptFolderForISO
+				}
+				Catch {
+					Return $_
+				}
+			}
+
+			# retrieve files in script folder
+			$Files = Get-ChildItem -Path $PathToScriptFolder -Filter '*.ps1'
+
+			# loop through files
+			ForEach ($File in $Files) {
+				# report state
+				"{0}`t{1}: {2}" -f [System.Datetime]::UtcNow.ToString('o'), 'Adding script to ISO scripts folder', $File.Name
+
+				# copy item to folder
+				Try {
+					Copy-Item -Path $File -Destination $ScriptFolderForISO
+				}
+				Catch {
+					Return $_
+				}
 			}
 		}
 	}
@@ -354,13 +481,15 @@ End {
 			Write-Warning -Message "could not remove exclusion for temporary path: $StagingPath"
 		}
 	}
-
-	# if Skip Remove not requested...
-	If (!$SkipRemove) {
-		# report state
-		"{0}`t{1}" -f [System.Datetime]::UtcNow.ToString('o'), 'Removing temporary files...'
-
-		# remove temporary items
-		Get-Item -Path $TemporaryPath | Remove-Item -Recurse -Force
+	
+	# if TemporaryFolder created...
+	If ([System.IO.Directory]::Exists($script:TemporaryFolder)) {
+		# remove temporary folder and all child items
+		Try {
+			Remove-Item -Path $TemporaryFolder -Recurse -Force
+		}
+		Catch {
+			Return $_
+		}
 	}
 }
