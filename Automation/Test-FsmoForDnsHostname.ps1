0303ae3ccd327afefb6038ed788f86a4eb259293
alexbarthnet <github@alexbarth.net>
2025-07-17 13:04:37 -0500

    initial commit

---
+++ Automation/Test-FsmoForDnsHostname.ps1
@@ -0,0 +1,69 @@
+<#
+.SYNOPSIS
+Test if the current FSMO role owner matches the local DNS hostname.
+
+.DESCRIPTION
+Test if the current FSMO role owner matches the local DNS hostname.
+
+.PARAMETER Role
+The FSMO role owner to evaluate.
+
+.PARAMETER DnsHostName
+The DNS hostname expected in the role. The default value is the DNS hostname of the local system.
+
+.INPUTS
+String.
+
+.OUTPUTS
+Boolean.
+
+.EXAMPLE
+.\Test-FsmoForHostname.ps1 -Role 'PDC'
+
+.NOTES
+The path may be a folder or a file. The file with the last write time is selected when the path is a folder.
+#>
+
+Param(
+	# local host name
+	[Parameter(DontShow)]
+	[string]$HostName = [System.Net.NetworkInformation.IPGlobalProperties]::GetIPGlobalProperties().HostName.ToLowerInvariant(),
+	# local domain name
+	[Parameter(DontShow)]
+	[string]$DomainName = [System.Net.NetworkInformation.IPGlobalProperties]::GetIPGlobalProperties().DomainName.ToLowerInvariant(),
+	# role to evaluate
+	[Parameter(Position = 0, Mandatory = $True)][ValidateSet('PDC', 'RID', 'Infrastructure', 'Schema', 'Naming')]
+	[string]$Role,
+	# local DNS hostname
+	[Parameter(Position = 1)]
+	[string]$DnsHostName = ($HostName, $DomainName -join '.').TrimEnd('.')
+)
+
+Process {
+	# retrieve name of role owner
+	switch ($Role) {
+		'PDC' {
+			$RoleOnwer = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain().PdcRoleOwner.Name
+		}
+		'RID' {
+			$RoleOnwer = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain().RidRoleOwner.Name
+		}
+		'Infrastructure' {
+			$RoleOnwer = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain().InfrastructureRoleOwner.Name
+		}
+		'Schema' {
+			$RoleOnwer = [System.DirectoryServices.ActiveDirectory.Forest]::GetCurrentForest().SchemaRoleOwner.Name
+		}
+		'Naming' {
+			$RoleOnwer = [System.DirectoryServices.ActiveDirectory.Forest]::GetCurrentForest().NamingRoleOwner.Name
+		}
+	}
+
+	# if role owner matches DNS hostname...
+	If ($RoleOnwer -eq $DnsHostName) {
+		Return $true
+	}
+	Else {
+		Return $false
+	}
+}
