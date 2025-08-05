f3db5167e07392f424bad64f79a429606cdd21d3
alexbarthnet <github@alexbarth.net>
2025-08-05 11:30:09 -0500

    updated reporting and object storage

--- ADAM/Remove-ADDeletedObjectsFromInstance.ps1
+++ ADAM/Remove-ADDeletedObjectsFromInstance.ps1
@@ -1,6 +1,6 @@
 #requires -Modules ActiveDirectory
 
-[CmdletBinding()]
+[CmdletBinding(SupportsShouldProcess)]
 param (
 	# local host name
 	[Parameter(DontShow)]
@@ -25,143 +25,201 @@ param (
 	[string]$Server = ('{0}:{1}' -f $DnsHostName, $Port)
 )
 
-Begin {
+begin {
+	function Remove-ADDeletedObject {
+		[CmdletBinding(SupportsShouldProcess)]
+		param(
+			[string]$Identity
+		)
+
+		# if WhatIf provided...
+		if ($PSCmdlet.ShouldProcess($ExistingObjectIdentity)) {
+			# remove existing object
+			try {
+				Remove-ADObject -Server $Server -Identity $ExistingObjectIdentity -Confirm:$false
+			}
+			# continue to next deleted object if object not found
+			catch [System.DirectoryServices.ActiveDirectory.ActiveDirectoryObjectNotFoundException] {
+				continue NextDeletedObject
+			}
+			# throw error if exception other than object not found
+			catch {
+				throw $_
+			}
+		}
+	}
+
 	# test access to Deleted Objects container on PDC
-	Try {
+	try {
+		$DomainRoot = Get-ADObject -Server $PdcRoleOwner -Identity $DomainNCName -IncludeDeletedObjects | Select-Object -ExpandProperty 'DistinguishedName'
+	}
+	catch {
+		Write-Warning -Message "could not access Deleted Objects container on server: $PdcRoleOwner"
+		throw $_
+	}
+
+	# test access to Deleted Objects container on PDC
+	try {
 		$DeletedObjectsContainer = Get-ADObject -Server $PdcRoleOwner -Identity "CN=Deleted Objects,$DomainNCName" -IncludeDeletedObjects | Select-Object -ExpandProperty 'DistinguishedName'
 	}
-	Catch {
+	catch {
 		Write-Warning -Message "could not access Deleted Objects container on server: $PdcRoleOwner"
-		Throw $_
+		throw $_
 	}
 
 	# retreive matching partition from local instance
-	Try {
+	try {
 		$PartitionRoot = Get-ADObject -Server $Server -Identity $DomainNCName -Properties 'whenCreated'
 	}
-	Catch {
+	catch {
 		Write-Warning -Message "could not access expected '$DomainNCName' partition on server: $Server"
-		Throw $_
+		throw $_
 	}
 
 	# retreive configuration NC from local instance
-	Try {
+	try {
 		$ConfigurationNC = Get-ADRootDSE -Server $Server | Select-Object -ExpandProperty 'configurationNamingContext'
 	}
-	Catch {
+	catch {
 		Write-Warning -Message "could not retrieve configuration naming context from 'rootDSE' object on server: $Server"
-		Throw $_
+		throw $_
 	}
 
 	# define ADAM Sync Services direction
+	$ServiceGuid = $DomainRoot.objectGuid
 	$ServiceName = 'ADAMSync'
 	$ServiceMode = 'Reverse'
 
 	# define ADAM Sync Services object and parent container
-	$ServicesContainer = "CN=Services,$ConfigurationNC"
-	$ADAMSyncParentContainer = "CN=$ServiceName,$ServicesContainer"
-	$ADAMSyncStatusContainer = "CN=$ServiceMode,$ADAMSyncParentContainer"
+	$ConfigServicesContainer = 'CN=Services,{0}' -f $ConfigurationNC
+	$ADAMSyncParentContainer = 'CN={0},{1}' -f $ServiceName, $ConfigServicesContainer
+	$ADAMSyncDomainContainer = 'CN={0},{1}' -f $ServiceGuid, $ADAMSyncParentContainer
+	$ADAMSyncStatusContainer = 'CN={0},{1}' -f $ServiceMode, $ADAMSyncDomainContainer
 
-	# retrieve ADAMSync Services container
-	Try {
+	# verify ADAMSync Parent container
+	try {
 		$null = Get-ADObject -Server $Server -Identity $ADAMSyncParentContainer
 	}
-	Catch {
-		# create ADAMSync Services object with properties
-		Try {
-			New-ADObject -Type 'container' -Name $ServiceName -Path $ServicesContainer
+	catch {
+		# create ADAMSync Parent container in Services container
+		try {
+			New-ADObject -Type 'container' -Name $ServiceName -Path $ConfigServicesContainer
 		}
-		Catch {
-			Write-Warning -Message "could not create '$ServiceName' container in '$ServicesContainer' on server: $Server"
-			Throw $_
+		catch {
+			Write-Warning -Message "could not create '$ServiceName' container in '$ConfigServicesContainer' on server: $Server"
+			throw $_
+		}
+	}
+
+	# verify ADAMSync Domain container
+	try {
+		$null = Get-ADObject -Server $Server -Identity $ADAMSyncDomainContainer
+	}
+	catch {
+		# create ADAMSync Domain container in Parent container
+		try {
+			New-ADObject -Type 'container' -Name $ServiceGuid -Path $ADAMSyncParentContainer
+		}
+		catch {
+			Write-Warning -Message "could not create '$ServiceGuid' container in '$ADAMSyncParentContainer' on server: $Server"
+			throw $_
+		}
+	}
+
+	# verify ADAMSync Status container
+	try {
+		$null = Get-ADObject -Server $Server -Identity $ADAMSyncStatusContainer
+	}
+	catch {
+		# create ADAMSync Status container in Domain container
+		try {
+			New-ADObject -Type 'container' -Name $ServiceMode -Path $ADAMSyncDomainContainer
+		}
+		catch {
+			Write-Warning -Message "could not create '$ServiceMode' container in '$ADAMSyncDomainContainer' on server: $Server"
+			throw $_
 		}
 	}
 
 	# retrieve ADAMSync Services object with properties
-	Try {
+	try {
 		$ADAMSyncObject = Get-ADObject -Server $Server -Identity $ADAMSyncStatusContainer -Properties 'adminDescription', 'whenChanged', 'whenCreated'
 	}
-	Catch {
-		# create ADAMSync Services object with properties
-		Try {
-			New-ADObject -Type 'container' -Name $ServiceMode -Path $ADAMSyncParentContainer
-		}
-		Catch {
-			Write-Warning -Message "could not create '$ServiceMode' container in '$ADAMSyncParentContainer' on server: $Server"
-			Throw $_
-		}
-
-		# retrieve ADAMSync Services object with properties
-		Try {
-			$ADAMSyncObject = Get-ADObject -Server $Server -Identity $ADAMSyncStatusContainer -Properties 'adminDescription', 'whenChanged', 'whenCreated'
-		}
-		Catch {
-			Write-Warning -Message "could not retrieve '$ServiceMode' container in '$ADAMSyncParentContainer' on server: $Server"
-			Throw $_
-		}
+	catch {
+		Write-Warning -Message "could not retrieve '$ServiceMode' container in '$ADAMSyncDomainContainer' on server: $Server"
+		throw $_
 	}
 
 	# if whenChanged matches whenCreated...
-	If ($ADAMSyncObject.whenChanged -eq $ADAMSyncObject.whenCreated) {
+	if ($ADAMSyncObject.whenChanged -eq $ADAMSyncObject.whenCreated) {
 		# set WhenChanged string to when partition was created
 		$WhenChanged = $PartitionRoot.whenCreated.ToString('yyyyMMddHHmmss.fZ')
 	}
 	# if whenChanged does not match whenCreated...
-	Else {
+	else {
 		# set WhenChanged string to when ADAMSync Services object was last updated was created
 		$WhenChanged = $ADAMSyncObject.whenChanged.ToString('yyyyMMddHHmmss.fZ')
 	}
 }
 
-Process {
+process {
 	# define parameters
 	$GetADObject = @{
 		Server                = $PdcRoleOwner
 		SearchBase            = $DeletedObjectsContainer
-		Filter                = "objectClass -eq 'user' -and whenChanged -gt '$WhenChanged'"
+		LDAPFilter            = '(&(!(objectClass=contact))(!(objectClass=computer))(|(objectClass=user)(objectClass=group)))'
 		Properties            = 'objectGuid'
 		IncludeDeletedObjects = $true
 	}
 
 	# retrieve deleted objects in scope
-	Try {
+	try {
 		$DeletedObjects = Get-ADObject @GetADObject | Select-Object -Property $Properties
 	}
-	Catch {
+	catch {
 		Write-Warning -Message "could not retrieve deleted objects since '$WhenChanged' from '$Domain' domain on server: $Server"
-		Throw $_
+		throw $_
 	}
 	
 	# loop through deleted objects
-	:NextDeletedObject ForEach ($DeletedObject in $DeletedObjects) {
+	:NextDeletedObject foreach ($DeletedObject in $DeletedObjects) {
+		# retrieve original object class from deleted object
+		$ObjectClass = $DeletedObject.objectClass
+		
 		# retrieve original object name from deleted object DN
 		$ExistingObjectIdentity = '{0},{1}' -f $DeletedObject.DistinguishedName.Split('\0ADEL:', 2)[0], $DeletedObject.LastKnownParent
 
+		# report state
+		Write-Host "found deleted '$ObjectClass' object with identity: $ExistingObjectIdentity"
+
 		# if WhatIf provided...
-		If ($PSCmdlet.ShouldProcess($ExistingObjectIdentity)) {
+		if ($PSCmdlet.ShouldProcess($ExistingObjectIdentity)) {
 			# remove existing object
-			Try {
+			try {
 				Remove-ADObject -Server $Server -Identity $ExistingObjectIdentity -Confirm:$false
 			}
 			# continue to next deleted object if object not found
-			Catch [System.DirectoryServices.ActiveDirectory.ActiveDirectoryObjectNotFoundException] {
-				Continue NextDeletedObject
+			catch [System.DirectoryServices.ActiveDirectory.ActiveDirectoryObjectNotFoundException] {
+				continue NextDeletedObject
 			}
 			# throw error if exception other than object not found
-			Catch {
-				Throw $_
+			catch {
+				throw $_
 			}
+
+			# report state
+			Write-Host "removed '$ObjectClass' object with identity: $ExistingObjectIdentity"
 		}
 	}
 }
 
-End {
+end {
 	# update ADAMSync Services object
-	Try {
-		Set-ADObject -Server $Server -Identity $ADAMSyncContainer -Replace @{ adminDescription = "Sync on $Server against '$PdcRoleOwner' domain controller" }
+	try {
+		Set-ADObject -Server $Server -Identity $ADAMSyncStatusContainer -Replace @{ adminDescription = "Completed '$ServiceMode' sync on $Server against '$PdcRoleOwner' domain controller" }
 	}
-	Catch {
-		Write-Warning -Message "could not update 'ADAMSync' container in '$ServicesContainer' on server: $Server"
-		Throw $_
+	catch {
+		Write-Warning -Message "could not update 'adminDescription' property on '$ADAMSyncStatusContainer' container on server: $Server"
+		throw $_
 	}
 }
