9fb0d310a965de6f5d966ffb2bfaa81cb653861b
alexbarthnet <github@alexbarth.net>
2025-08-05 08:48:25 -0500

    added try-catch blocks, added reporting, updated formatting

--- DFSR/Install-DFSR.ps1
+++ DFSR/Install-DFSR.ps1
@@ -1,5 +1,5 @@
 [CmdletBinding()]
-Param(
+param(
     [Parameter(DontShow)]
     [string]$ComputerName = [System.Net.NetworkInformation.IPGlobalProperties]::GetIPGlobalProperties().HostName.ToLowerInvariant(),
     [Parameter(DontShow)]
@@ -21,7 +21,7 @@ Import-Module -Name ServerManager
 $WindowsFeature = Install-WindowsFeature -Name 'FS-DFS-Replication' -IncludeManagementTools
 
 # if feature installed...
-If ($WindowsFeature.ExitCode -eq 'Success') {
+if ($WindowsFeature.ExitCode -eq 'Success') {
     # restart service to address WMI issues
     Restart-Service -Name 'DFSR'
 }
@@ -30,7 +30,7 @@ If ($WindowsFeature.ExitCode -eq 'Success') {
 $Dfsr = @{ DomainName = $DomainName; GroupName = $GroupName }
 
 # create content path if not found
-If (![System.IO.Directory]::Exists($ContentPath)) { 
+if (![System.IO.Directory]::Exists($ContentPath)) { 
     $null = [System.IO.Directory]::CreateDirectory($ContentPath)
 }
 
@@ -38,47 +38,73 @@ If (![System.IO.Directory]::Exists($ContentPath)) {
 $DfsReplicationGroup = Get-DfsReplicationGroup @Dfsr
 
 # if DFSR group not found...
-If (!$DfsReplicationGroup) { 
+if (!$DfsReplicationGroup) { 
     # create DFSR group
-    New-DfsReplicationGroup @Dfsr
+    try {
+        New-DfsReplicationGroup @Dfsr
+    }
+    catch {
+        Write-Warning -Message "could not create '$GroupName' DFS-R group: $($_.Exception.Message)"
+        return $_
+    }
 
     # create DFSR folder
-    New-DfsReplicatedFolder @Dfsr -FolderName $GroupName
+    try {
+        New-DfsReplicatedFolder @Dfsr -FolderName $GroupName
+    }
+    catch {
+        Write-Warning -Message "could not create '$GroupName' folder in '$GroupName' DFS-R group: $($_.Exception.Message)"
+        return $_
+    }
 }
 
 # if account name provided
-If ($AccountName) {
+if ($AccountName) {
     # delegate permissions to the DFSR group if necessary
-    Grant-DfsrDelegation @Dfsr -AccountName $AccountName -Force
+    try {
+        Grant-DfsrDelegation @Dfsr -AccountName $AccountName -Force
+    }
+    catch {
+        Write-Warning -Message "could not grant DFS-R delegation to '$AccountName' account on '$GroupName' DFS-R group: $($_.Exception.Message)"
+        return $_
+    }
 }
 
 # retrieve DFS members and split into former and current members
 $DfsrFormerMembers, $DfsrMembers = (Get-DfsrMember @Dfsr).Where({ [string]::IsNullOrEmpty($_.ComputerName) }, [System.Management.Automation.WhereOperatorSelectionMode]::Split)
 
 # if group has no members or force primary member was set...
-If ($DfsrMembers.Count -eq 0 -or $ForcePrimaryMember) {
+if ($DfsrMembers.Count -eq 0 -or $ForcePrimaryMember) {
     # set primary member to true
     $PrimaryMember = $true
-# if group has only computer as member...
-} ElseIf ($DfsrMembers.Count -eq 1 -and $DfsrMembers.ComputerName -eq $ComputerName) {
+    # if group has only computer as member...
+}
+elseif ($DfsrMembers.Count -eq 1 -and $DfsrMembers.ComputerName -eq $ComputerName) {
     # set primary member to true
     $PrimaryMember = $true
-# if group has multiple members...
-} Else {
+    # if group has multiple members...
+}
+else {
     # set primary member to false
     $PrimaryMember = $false
 }
 
 # loop through former members
-ForEach ($Guid in $DfsrFormerMembers.Identifier.Guid) { 
+foreach ($Guid in $DfsrFormerMembers.Identifier.Guid) { 
     # remove former members
     Start-Process -Wait -NoNewWindow -FilePath 'dfsradmin.exe' -ArgumentList 'mem', 'delete', "/RgName:$GroupName", "/MemGuid:$Guid"
 }
 
 # if computer is not a member...
-If ($ComputerName -notin $DfsrMembers.ComputerName) { 
+if ($ComputerName -notin $DfsrMembers.ComputerName) { 
     # join DFSR group
-    Add-DfsrMember @Dfsr -ComputerName $ComputerName
+    try {
+        Add-DfsrMember @Dfsr -ComputerName $ComputerName
+    }
+    catch {
+        Write-Warning -Message "could not add '$ComputerName' computer to '$GroupName' DFS-R group: $($_.Exception.Message)"
+        return $_
+    }
 
     # allow 30 seconds for AD replication
     Start-Sleep -Seconds 30
@@ -88,30 +114,55 @@ If ($ComputerName -notin $DfsrMembers.ComputerName) {
 $DfsrConnections = Get-DfsrConnection @Dfsr | Where-Object { $_.SourceComputerName -eq $ComputerName }
 
 # retrieve other computers in DFSR group without connection to current computer
-$DestinationComputerNames = (Get-DfsrMember @Dfsr | Where-Object { $_.ComputerName -ne $ComputerName -and $_.ComputerName -notin $DfsrConnections.DestinationComputerName }).ComputerName
+$DestinationComputers = Get-DfsrMember @Dfsr | Where-Object { $_.ComputerName -ne $ComputerName -and $_.ComputerName -notin $DfsrConnections.DestinationComputerName }
 
-# create connections with current computer
-ForEach ($DestinationComputerName in $DestinationComputerNames) { Add-DfsrConnection @Dfsr -SourceComputerName $ComputerName -DestinationComputerName $DestinationComputerName }
+# loop through destination computers
+foreach ($DestinationComputerName in $DestinationComputers.Names) { 
+    # create connection with current computer
+    try {
+        Add-DfsrConnection @Dfsr -SourceComputerName $ComputerName -DestinationComputerName $DestinationComputerName
+    }
+    catch {
+        Write-Warning -Message "could not create DFS-R connection with '$DestinationComputerName' computer in '$GroupName' DFS-R group: $($_.Exception.Message)"
+        return $_
+    }
+}
 
 # retrieve new connections
-$NewDfsrConnections = Get-DfsrConnection @Dfsr | Where-Object { $_.SourceComputerName -eq $ComputerName -and $_.DestinationComputerName -in $DestinationComputerNames }
+$NewDfsrConnections = Get-DfsrConnection @Dfsr | Where-Object { $_.SourceComputerName -eq $ComputerName -and $_.DestinationComputerName -in $DestinationComputers.Names }
 
-# report new connections
-$NewDfsrConnections | Format-Table -Property GroupName, SourceComputerName, DestinationComputerName, DomainName, Enabled, Identifier
+# if new connections created...
+if ($NewDfsrConnections) {
+    # report new connections
+    Write-Host "created new DFS-R connection:"
+    $NewDfsrConnections | Format-Table -Property GroupName, SourceComputerName, DestinationComputerName, DomainName, Enabled, Identifier
+}
 
 # define local content path for DFSR membership
-Set-DfsrMembership @Dfsr -ComputerName $ComputerName -FolderName $GroupName -ContentPath $ContentPath -PrimaryMember $PrimaryMember -Force
+try {
+    Set-DfsrMembership @Dfsr -ComputerName $ComputerName -FolderName $GroupName -ContentPath $ContentPath -PrimaryMember $PrimaryMember -Force
+}
+catch {
+    Write-Warning -Message "could not set DFS-R membership in '$GroupName' DFS-R group: $($_.Exception.Message)"
+    return $_
+}
 
 # if new connections created and not primary member...
-If ($NewDfsrConnections -and -not $PrimaryMember) { 
+if ($NewDfsrConnections -and -not $PrimaryMember) { 
     # stop service on current computer
     Stop-Service -Name 'DFSR'
+
+    # report state
+    Write-Host 'Pausing 30 seconds for AD replication'
 
     # allow 30 seconds for AD replication
     Start-Sleep -Seconds 30
 
     # update configuration on remote computers
-    $DestinationComputerNames | Update-DfsrConfigurationFromAD -Verbose
+    $DestinationComputers.Names | Update-DfsrConfigurationFromAD -Verbose
+
+    # report state
+    Write-Host 'Pausing 30 seconds for remote computers to update'
 
     # allow 30 seconds for service on remote computers to update
     Start-Sleep -Seconds 30
