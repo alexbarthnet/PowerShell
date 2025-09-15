e6961321ee4a4e8e7a4e359dcba0abec629bd6e9
alexbarthnet <81780226+alexbarthnet@users.noreply.github.com>
2025-09-15 13:34:53 -0500

    initial commit

---
+++ DFS/Install-DFSN.ps1
@@ -0,0 +1,113 @@
+[CmdletBinding()]
+param(
+    [Parameter(DontShow)]
+    [string]$HostName = [System.Net.NetworkInformation.IPGlobalProperties]::GetIPGlobalProperties().HostName.ToLowerInvariant(),
+    [Parameter(DontShow)]
+    [string]$DomainName = [System.Net.NetworkInformation.IPGlobalProperties]::GetIPGlobalProperties().DomainName.ToLowerInvariant(),
+    [Parameter(DontShow)]
+    [string]$DnsHostName = ($HostName, $DomainName -join '.').TrimEnd('.'),
+    [Parameter(Position = 0, Mandatory)]
+    [string]$Path,
+    [Parameter(Position = 1, Mandatory)]
+    [string]$TargetPath,
+    [Parameter(Position = 2)]
+    [string]$AdminAccounts
+)
+
+# define error preference
+$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
+
+# import module
+try {
+    Import-Module -Name ServerManager
+}
+catch {
+    Write-Warning -Message "could not load 'ServerManager' module: $($_.Exception.Message)"
+    return $_
+}
+
+# install DFS-N feature
+try {
+    $WindowsFeature = Install-WindowsFeature -Name 'FS-DFS-Namespace' -IncludeManagementTools
+}
+catch {
+    Write-Warning -Message "could not install 'FS-DFS-Namespace' feature: $($_.Exception.Message)"
+    return $_
+}
+
+# if DFS-N feature installed...
+if ($WindowsFeature.ExitCode -eq 'Success') {
+    # restart DFS-N service to address WMI issues
+    Restart-Service -Name 'DFS'
+}
+
+# check for existing DFS-N root 
+try {
+    $DfsnRoot = Get-DfsnRoot -Path $Path -ErrorAction 'Stop' 
+}
+catch {
+    # define required parameters for New-DfsnRoot
+    $NewDfsnRoot = @{
+        Path                         = $Path
+        TargetPath                   = $TargetPath
+        Type                         = 'DomainV2'
+        EnableAccessBasedEnumeration = $true
+        ErrorAction                  = 'Stop'
+    }
+
+    # define required parameters for New-DfsnRoot
+    If ($PSBoundParameters.ContainsKey('AdminAccounts')) {
+        $NewDfsnRoot.GrantAdminAccounts = $AdminAccounts
+    }
+
+    # create new DFS-N root
+    try {
+        $null = New-DfsnRoot @NewDfsnRoot
+    }
+    catch {
+        Write-Warning -Message "could not create '$Path' DFS-N root: $($_.Exception.Message)"
+        return $_
+    }
+}
+
+# if existing DFS-N root retrieved...
+If ($DfsnRoot) {
+    # report state
+    Write-Host "Located '$Path' DFS-N root"
+}
+else {
+    # report state
+    Write-Host "Created '$Path' DFS-N root"
+}
+
+# check for existing DFS-N root target
+try {
+    $DfsnRootTarget = Get-DfsnRootTarget -Path $Path -TargetPath $TargetPath -ErrorAction 'Stop'
+}
+catch {
+    # define parameters for New-DfsnRootTarget
+    $NewDfsnRoot = @{
+        Path                         = $Path
+        TargetPath                   = $TargetPath
+        ErrorAction                  = 'Stop'
+    }
+
+    # create new DFS-N root target
+    try {
+        $null = New-DfsnRootTarget -Path $Path -TargetPath $TargetPath -ErrorAction 'Stop'
+    }
+    catch {
+        Write-Warning -Message "could not create '$TargetPath' DFS-N root target: $($_.Exception.Message)"
+        return $_
+    }
+}
+
+# if existing DFS-N root target retrieved...
+If ($DfsnRootTarget) {
+    # report state
+    Write-Host "Located '$TargetPath' DFS-N root target"
+}
+else {
+    # report state
+    Write-Host "Created '$TargetPath' DFS-N root target"
+}
