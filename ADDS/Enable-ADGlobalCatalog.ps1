182be0d9a49880d57a0fa5d252978c0a09c48b0d
alexbarthnet <81780226+alexbarthnet@users.noreply.github.com>
2025-11-05 09:01:06 -0600

    removed transcript code

--- ADDS/Enable-ADGlobalCatalog.ps1
+++ ADDS/Enable-ADGlobalCatalog.ps1
@@ -1,11 +1,11 @@
-#requires -modules ActiveDirectory,TranscriptWithHostAndDate
+#requires -modules ActiveDirectory
 
 <#
 .SYNOPSIS
-Enable the Global Catalog role for the local system on a remote domain controller.
+Enable the Global Catalog role for the local system via the object for local system on a remote domain controller.
 
 .DESCRIPTION
-Enable the Global Catalog role for the local system on a remote domain controller.
+Enable the Global Catalog role for the local system via the object for local system on a remote domain controller.
 
 #>
 
@@ -28,102 +28,74 @@ Param (
 	[string]$DnsHostName = ($HostName, $DomainName -join '.').TrimEnd('.')
 )
 
-Begin {
-	# if skip transcript not requested...
-	If (!$SkipTranscript) {
-		# start transcript with default parameters
-		Try {
-			Start-TranscriptWithHostAndDate
-		}
-		Catch {
-			Throw $_
-		}
-	}
+# if local system is not a domain controller...
+If ($DomainRole -lt 4) {
+	# report and return
+	Write-Warning -Message 'local system is not a domain controller'
+	Return
 }
 
-Process {
-	# if local system is not a domain controller...
-	If ($DomainRole -lt 4) {
-		# report and return
-		Write-Warning -Message 'local system is not a domain controller'
-		Return
-	}
-
-	# get local domain controller by name
-	Try {
-		$LocalDomainController = Get-ADDomainController -Identity $DnsHostName
-	}
-	Catch {
-		Write-Warning -Message 'could not retrieve local domain controller'
-		Return $_
-	}
-
-	# if local domain controller is read only...
-	If ($LocalDomainController.IsReadOnly) {
-		# report and return
-		Write-Warning -Message 'local system is a read-only domain controller'
-		Return
-	}
-
-	# get remote domain controller by discovery
-	Try {
-		$RemoteDomainController = Get-ADDomainController -AvoidSelf -Discover -ForceDiscover -NextClosestSite -Writable
-	}
-	Catch {
-		Write-Warning -Message 'could not retrieve remote domain controller'
-		Return $_
-	}
-
-	# if remote domain controller not found...
-	If ($null -eq $script:RemoteDomainController) {
-		# warn and return
-		Write-Warning -Message 'could not locate a remote domain controller'
-		Return
-	}
-	Else {
-		# retrieve server name from property collection
-		$Server = $RemoteDomainController.HostName.Value
-		# declare found
-		Write-Verbose -Verbose -Message "found remote domain controller: $Server"
-	}
-
-	# get DSA object for local domain controller from remote domain controller
-	Try {
-		$ADObject = Get-ADObject -Server $Server -Identity $LocalDomainController.NTDSSettingsObjectDN -Properties 'options'
-	}
-	Catch {
-		Write-Warning -Message "could not find DSA for local domain controller on remote domain controller: $Server"
-		Return $_
-	}
-
-	# if Global Catalog role already enabled on DSA object...
-	If ($ADObject.options -eq '1') {
-		Write-Warning -Message "found Global Catalog role already enabled for local domain controller on remote domain controller: $Server"
-		Return
-	}
-
-	# enable Global Catalog role for local domain controller on remote domain controller
-	Try {
-		Set-ADObject -Server $Server -Identity $LocalDomainController.NTDSSettingsObjectDN -Replace @{ options = '1' }
-	}
-	Catch {
-		Write-Warning -Message "could not enable Global Catalog role for local domain controller on remote domain controller: $Server"
-		Return $_
-	}
-
-	# declare complete
-	Write-Verbose -Verbose -Message "enabled Global Catalog role for local domain controller on remote domain controller: $Server"
+# get local domain controller by name
+Try {
+	$LocalDomainController = Get-ADDomainController -Identity $DnsHostName
+}
+Catch {
+	Write-Warning -Message 'could not retrieve local domain controller'
+	Return $_
 }
 
-End {
-	# if skip transcript not requested...
-	If (!$SkipTranscript) {
-		# stop transcript with default parameters
-		Try {
-			Stop-TranscriptWithHostAndDate
-		}
-		Catch {
-			Throw $_
-		}
-	}
+# if local domain controller is read only...
+If ($LocalDomainController.IsReadOnly) {
+	# report and return
+	Write-Warning -Message 'local system is a read-only domain controller'
+	Return
 }
+
+# get remote domain controller by discovery
+Try {
+	$RemoteDomainController = Get-ADDomainController -AvoidSelf -Discover -ForceDiscover -NextClosestSite -Writable
+}
+Catch {
+	Write-Warning -Message 'could not retrieve remote domain controller'
+	Return $_
+}
+
+# if remote domain controller not found...
+If ($null -eq $script:RemoteDomainController) {
+	# warn and return
+	Write-Warning -Message 'could not locate a remote domain controller'
+	Return
+}
+Else {
+	# retrieve server name from property collection
+	$Server = $RemoteDomainController.HostName.Value
+	# declare found
+	Write-Host "found remote domain controller: $Server"
+}
+
+# get DSA object for local domain controller from remote domain controller
+Try {
+	$ADObject = Get-ADObject -Server $Server -Identity $LocalDomainController.NTDSSettingsObjectDN -Properties 'options'
+}
+Catch {
+	Write-Warning -Message "could not find DSA for local domain controller on remote domain controller: $Server"
+	Return $_
+}
+
+# if Global Catalog role already enabled on DSA object...
+If ($ADObject.options -eq '1') {
+	Write-Warning -Message "found Global Catalog role already enabled for local domain controller on remote domain controller: $Server"
+	Return
+}
+
+# enable Global Catalog role for local domain controller on remote domain controller
+Try {
+	Set-ADObject -Server $Server -Identity $LocalDomainController.NTDSSettingsObjectDN -Replace @{ options = '1' }
+}
+Catch {
+	Write-Warning -Message "could not enable Global Catalog role for local domain controller on remote domain controller: $Server"
+	Return $_
+}
+
+# declare complete
+Write-Host "enabled Global Catalog role for local domain controller on remote domain controller: $Server"
