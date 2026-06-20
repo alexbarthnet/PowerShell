701a4fd430613fccdf5725a99ef5bfa560fa614c
alexbarthnet <81780226+alexbarthnet@users.noreply.github.com>
2026-06-20 16:27:04 -0500

    updated reporting, added checks

--- Clusters/Storage/Start-ClusterSharedVolumeReFSDedupJob.ps1
+++ Clusters/Storage/Start-ClusterSharedVolumeReFSDedupJob.ps1
@@ -1,6 +1,8 @@
 [CmdletBinding()]
 param(
-    [string]$ExcludeFolder = '.exclude'
+	[uint16]$DurationHours = 1,
+	[string]$ExcludeFolder = '.exclude',
+	[switch]$FullRun
 )
 
 function Format-Bytes {
@@ -8,16 +10,31 @@ function Format-Bytes {
 		[Parameter(Position = 0, Mandatory = $true)]
 		[uint64]$Size,
 		[Parameter(Position = 1)]
-		[int32]$RoundTo = 2
+		[uint32]$RoundTo = 2,
+		[Parameter(Position = 2)]
+		[uint32]$Decimals = 0
 	)
 	switch ($Size) {
-		{ $_ -ge 1PB } { "$([math]::Round($Size / 1PB,$RoundTo)) PB"; break }
-		{ $_ -ge 1TB } { "$([math]::Round($Size / 1TB,$RoundTo)) TB"; break }
-		{ $_ -ge 1GB } { "$([math]::Round($Size / 1GB,$RoundTo)) GB"; break }
-		{ $_ -ge 1MB } { "$([math]::Round($Size / 1MB,$RoundTo)) MB"; break }
-		{ $_ -ge 1KB } { "$([math]::Round($Size / 1KB,$RoundTo)) KB"; break }
-		default { "$([math]::Round($Size,$RoundTo)) B" }
+		{ $_ -ge 1PB } { $Value = [math]::Round($Size / 1PB, $RoundTo); $Unit = 'PB'; break }
+		{ $_ -ge 1TB } { $Value = [math]::Round($Size / 1TB, $RoundTo); $Unit = 'TB'; break }
+		{ $_ -ge 1GB } { $Value = [math]::Round($Size / 1GB, $RoundTo); $Unit = 'GB'; break }
+		{ $_ -ge 1MB } { $Value = [math]::Round($Size / 1MB, $RoundTo); $Unit = 'MB'; break }
+		{ $_ -ge 1KB } { $Value = [math]::Round($Size / 1KB, $RoundTo); $Unit = 'KB'; break }
+		default { $Value = [math]::Round($Size, $RoundTo); $Unit = 'B' }
 	}
+	if ($PSBoundParameters.ContainsKey('Decimals')) {
+		$Value = "{0:F$Decimals}" -f $Value
+	}
+
+	return "$Value $Unit"
+}
+
+# if fullrun requested...
+if ($FullRun.IsPresent) {
+	$Interval = 10
+}
+else {
+	$Interval = 1
 }
 
 # retrieve the local cluster node
@@ -27,40 +44,79 @@ $ClusterNode = Get-ClusterNode -Name $env:COMPUTERNAME
 $ClusterSharedVolumes = Get-ClusterSharedVolume -InputObject $ClusterNode
 
 # loop through cluster shared volumes
-foreach ($ClusterSharedVolume in $ClusterSharedVolumes) {
-    # retrieve the volume from the cluster shared volume
-    $Volume = $ClusterSharedVolume.SharedVolumeInfo.FriendlyVolumeName
+:NextClusterSharedVolume foreach ($ClusterSharedVolume in $ClusterSharedVolumes) {
+	# retrieve the volume from the cluster shared volume
+	$Volume = $ClusterSharedVolume.SharedVolumeInfo.FriendlyVolumeName
 
-    # retrieve the ReFS dedupe configuration
-    $ReFSDedupStatus = Get-ReFSDedupStatus -Volume $Volume
-    
-    # if ReFS deduplication is enabled...
-    if ($ReFSDedupStatus.Enabled) {
-        # start a deduplication job
-        $Job = Start-ReFSDedupJob -Volume $Volume -ExcludeFolder $ExcludeFolder -CpuPercentage 25 -ConcurrentOpenFiles 1 -Duration ([System.TimeSpan]::FromHours(1))
+	# retrieve the ReFS dedupe configuration
+	$ReFSDedupStatus = Get-ReFSDedupStatus -Volume $Volume
 
-        # while job exists...
-        while ($Job.State -notin 'Completed', 'Failed') {
-            # sleep for 1 second
-            Start-Sleep -Seconds 1
 
-            # update job object
-            $Job = Get-Job -Id $Job.Id
+	# if ReFS deduplication is enabled...
+	if ($ReFSDedupStatus.Enabled) {
+		# if ReFS deduplication job is running...
+		if ($ReFSDedupStatus.State -in 'Optimizing') {
+			Write-Warning -Message "$([System.Datetime]::Now.ToString('yyyy-MM-dd-HH:mm:ss')): Volume: $Volume; Type: $($ReFSDedupStatus.Type); State: $($ReFSDedupStatus.State); found existing deduplication job"
+			continue NextClusterSharedVolume
+		}
 
-            # retrieve dedupe status
-            $ReFSDedupStatus = Get-ReFSDedupStatus -Volume $Volume
+		# if ReFS deduplication job is running...
+		if ($ReFSDedupStatus.State -notin 'Idle', 'Cancelled', 'None') {
+			Write-Warning -Message "$([System.Datetime]::Now.ToString('yyyy-MM-dd-HH:mm:ss')): Volume: $Volume; Type: $($ReFSDedupStatus.Type); State: $($ReFSDedupStatus.State); found unexpected state"
+			continue NextClusterSharedVolume
+		}
 
-            # write dedupe status to screen
-            Write-Host "Volume: $Volume; Type: $($ReFSDedupStatus.Type); State: $($ReFSDedupStatus.State)"
-        }
+		# define parameters
+		$StartReFSDedupJob = @{
+			Volume              = $Volume
+			ExcludeFolder       = $ExcludeFolder
+			ConcurrentOpenFiles = 1
+			CpuPercentage       = 25
+			ErrorAction         = [System.Management.Automation.ActionPreference]::Stop
+		}
 
-        # if job failed...
-        if ($Job.State -eq 'Failed') {
-            Write-Host "Volume: $Volume; Type: $($ReFSDedupStatus.Type); State: $($ReFSDedupStatus.State); Error: $($Job.Error.Exception.Message.Replace([System.Environment]::NewLine, ' '))"
-        }
-        else {
-            # write dedupe status to screen
-            Write-Host "Volume: $Volume; Type: $($ReFSDedupStatus.Type); State: $($ReFSDedupStatus.State); Duration: $($ReFSDedupStatus.LastRunDuration.ToString()); Size: $(Format-Bytes -Size $ReFSDedupStatus.Size); TotalSavings: $(Format-Bytes -Size $ReFSDedupStatus.TotalSavings); SavingsOnLastRun: $(Format-Bytes -Size $ReFSDedupStatus.SavingsOnLastRun)"
-        }
-    }
+		# if fullrun requested...
+		if ($FullRun.IsPresent) {
+			$StartReFSDedupJob['FullRun'] = $true
+		}
+		else {
+			$StartReFSDedupJob['Duration'] = [System.TimeSpan]::FromHours($DurationHours)
+		}
+
+		# start stopwatch
+		$StopWatch = [System.Diagnostics.Stopwatch]::StartNew()
+
+		# start a deduplication job
+		$Job = Start-ReFSDedupJob @StartReFSDedupJob
+
+		# while job exists...
+		while ($Job.State -notin 'Completed', 'Failed', 'Stopped') {
+			# sleep for 1 second
+			Start-Sleep -Seconds $Interval
+
+			# update job object
+			$Job = Get-Job -Id $Job.Id
+
+			# retrieve dedupe status
+			$ReFSDedupStatus = Get-ReFSDedupStatus -Volume $Volume
+
+			# if progress...
+			if ($ReFSDedupStatus.Progress -gt 0 -and $ReFSDedupStatus.Progress -lt 1) {
+				# write dedupe status to screen
+				Write-Host "$([System.Datetime]::Now.ToString('yyyy-MM-dd-HH:mm:ss')): Volume: $Volume; Type: $($ReFSDedupStatus.Type); State: $($ReFSDedupStatus.State); Duration: $($StopWatch.Elapsed.ToString()); Progress: $($ReFSDedupStatus.Progress.ToString('P2').PadLeft(6,'0')); Processed: $(Format-Bytes -Size $ReFSDedupStatus.ProcessedOnLastRun -Decimals 2); Savings: $(Format-Bytes -Size $ReFSDedupStatus.SavingsOnLastRun -Decimals 2)"
+			}
+		}
+
+		# stop stopwatch
+		$StopWatch.Stop()
+
+		# if job failed...
+		if ($Job.State -eq 'Failed') {
+			Write-Host "$([System.Datetime]::Now.ToString('yyyy-MMdd-HH:mm:ss')): Volume: $Volume; Type: $($ReFSDedupStatus.Type); Error: $($Job.Error.Exception.Message.Replace([System.Environment]::NewLine, ' '))"
+		}
+		else {
+			# write dedupe status to screen
+			Write-Host "$([System.Datetime]::Now.ToString('yyyy-MM-dd-HH:mm:ss')): Volume: $Volume; Type: $($ReFSDedupStatus.Type); Duration: $($ReFSDedupStatus.LastRunDuration.ToString()); Size: $(Format-Bytes -Size $ReFSDedupStatus.Size); TotalSavings: $(Format-Bytes -Size $ReFSDedupStatus.TotalSavings); ProcessedOnLastRun: $(Format-Bytes -Size $ReFSDedupStatus.ProcessedOnLastRun); SavingsOnLastRun: $(Format-Bytes -Size $ReFSDedupStatus.SavingsOnLastRun)"
+		}
+	}
 }
